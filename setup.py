51aab695b61234fd97c40b42b7350b9a1a536f1c
Nick Rubin <rubinnc0@gmail.com>
2022-07-06 15:31:29 -0700

    Set up file and requirements
    Add setup file to make a python package and preliminary requirements.txt

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""
+CIRQ-QUBITIZATION installation script
+"""
+
+import io
+import re
+
+from setuptools import setup, find_packages
+
+
+def version_number(path: str) -> str:
+    """Get cirq-qubitization's version number from the src directory
+    """
+    exp = r'__version__[ ]*=[ ]*["|\']([\d]+\.[\d]+\.[\d]+[\.dev[\d]*]?)["|\']'
+    version_re = re.compile(exp)
+
+    with open(path, 'r') as fqe_version:
+        version = version_re.search(fqe_version.read()).group(1)
+
+    return version
+
+
+def main() -> None:
+    """
+    """
+    version_path = 'cirq-qubitization/_version.py'
+
+    __version__ = version_number(version_path)
+
+    if __version__ is None:
+        raise ValueError('Version information not found in ' + version_path)
+
+    long_description = ('=================\n' +
+                        'CIRQ-QUBITIZATION\n' +
+                        '=================\n')
+    stream = io.open('README.md', encoding='utf-8')
+    stream.readline()
+    long_description += stream.read()
+
+    requirements_buffer = open('requirements.txt').readlines()
+    requirements = [r.strip() for r in requirements_buffer]
+
+    setup(
+        name='cirq-qubitization',
+        version=__version__,
+        author='Nicholas C. Rubin and Tanuj Khattar',
+        author_email='rubinnc0@gmail.com',
+        description='Learning tools and basics for quantum chemistry',
+        long_description=long_description,
+        install_requires=None,
+        license='Apache 2',
+        packages=find_packages(),
+        )
+
+
+main()
