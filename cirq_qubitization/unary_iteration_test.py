a2d57bf5fd6634aeff5507101560511c8f9f85f7
Matthew Harrigan <mpharrigan@google.com>
2022-07-18 16:53:43 -0700

    tests too

--- cirq_qubitization/unary_iteration_test.py
+++ cirq_qubitization/unary_iteration_test.py
@@ -45,13 +45,17 @@ def test_unary_iteration():
         ApplyXToLthQubit(3, 5).on(control, *selection, *ancilla, *target)
     )
     sim = cirq.Simulator()
-    for selection_integer in range(target_length):
-        svals = [int(x) for x in format(selection_integer, f"0{selection_length}b")]
-        qubit_vals = {x: int(x == control) for x in all_qubits}  # turn on control bit to activate circuit
-        qubit_vals.update({s: sval for s, sval in zip(selection, svals)})  # Initialize selection bits appropriately
+    for n in range(len(target)):
+        svals = [int(x) for x in format(n, f"0{len(selection)}b")]
+        # turn on control bit to activate circuit:
+        qubit_vals = {x: int(x == control) for x in all_qubits}
+        # Initialize selection bits appropriately:
+
+        qubit_vals.update({s: sval for s, sval in zip(selection, svals)})
 
         initial_state = [qubit_vals[x] for x in all_qubits]
         result = sim.simulate(circuit, initial_state=initial_state)
-        initial_state[-(selection_integer + 1)] = 1  # Build correct statevector with selection_integer bit flipped in the target register
+        # Build correct statevector with selection_integer bit flipped in the target register:
+        initial_state[-(n + 1)] = 1
         expected_output = "".join(str(x) for x in initial_state)
         assert result.dirac_notation()[1:-1] == expected_output
