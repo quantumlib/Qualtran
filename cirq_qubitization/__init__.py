78c6f7c6deb62092c6dd76b7b879223cbb24e856
Tanuj Khattar <tanujkhattar@google.com>
2022-08-01 14:13:43 -0700

    Add MultiTargetCSwap gates

--- cirq_qubitization/__init__.py
+++ cirq_qubitization/__init__.py
@@ -7,5 +7,9 @@ from cirq_qubitization.unary_iteration import UnaryIterationGate
 from cirq_qubitization.generic_select import GenericSelect
 from cirq_qubitization.generic_subprepare import GenericSubPrepare
 from cirq_qubitization.selected_majorana_fermion import SelectedMajoranaFermionGate
-from cirq_qubitization.swap_network import SwapWithZeroGate
+from cirq_qubitization.swap_network import (
+    MultiTargetCSwap,
+    MultiTargetCSwapApprox,
+    SwapWithZeroGate,
+)
 from cirq_qubitization.qrom import QROM
