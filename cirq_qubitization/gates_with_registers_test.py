18885dafb0a0be0c3235345bb7f971f17e018a35
Tanuj Khattar <tanujkhattar@google.com>
2022-08-01 13:33:46 -0700

    Add on_registers to GateWithRegisters base class

--- cirq_qubitization/gates_with_registers_test.py
+++ cirq_qubitization/gates_with_registers_test.py
@@ -13,20 +13,28 @@ def test_register():
 def test_registers():
     r1 = Register("r1", 5)
     r2 = Register("r2", 2)
-    regs = Registers([r1, r2])
+    r3 = Register("r3", 1)
+    regs = Registers([r1, r2, r3])
 
     assert regs.at(0) == r1
     assert regs.at(1) == r2
+    assert regs.at(2) == r3
 
     assert regs["r1"] == r1
     assert regs["r2"] == r2
+    assert regs["r3"] == r3
 
-    assert list(regs) == [r1, r2]
+    assert list(regs) == [r1, r2, r3]
 
-    qubits = cirq.LineQubit.range(7)
+    qubits = cirq.LineQubit.range(8)
     qregs = regs.split_qubits(qubits)
     assert qregs["r1"] == cirq.LineQubit.range(5)
     assert qregs["r2"] == cirq.LineQubit.range(5, 5 + 2)
+    assert qregs["r3"] == [cirq.LineQubit(7)]
+
+    qubits = qubits[::-1]
+    merged_qregs = regs.merge_qubits(r1=qubits[:5], r2=qubits[5:7], r3=qubits[-1])
+    assert merged_qregs == qubits
 
 
 def test_registers_build():
@@ -40,19 +48,25 @@ class _TestGate(GateWithRegisters):
     def registers(self) -> Registers:
         r1 = Register("r1", 5)
         r2 = Register("r2", 2)
-        regs = Registers([r1, r2])
+        r3 = Register("r3", 1)
+        regs = Registers([r1, r2, r3])
         return regs
 
     def decompose_from_registers(
-        self, r1: Sequence[cirq.Qid], r2: Sequence[cirq.Qid]
+        self, r1: Sequence[cirq.Qid], r2: Sequence[cirq.Qid], r3: Sequence[cirq.Qid]
     ) -> cirq.OP_TREE:
         yield cirq.H.on_each(r1)
         yield cirq.X.on_each(r2)
+        yield cirq.X.on_each(r3)
 
 
 def test_gate_with_registers():
     tg = _TestGate()
-    assert tg._num_qubits_() == 7
-    qubits = cirq.LineQubit.range(7)
+    assert tg._num_qubits_() == 8
+    qubits = cirq.LineQubit.range(8)
     circ = cirq.Circuit(tg._decompose_(qubits))
     assert circ.operation_at(cirq.LineQubit(3), 0).gate == cirq.H
+
+    op1 = tg.on_registers(r1=qubits[:5], r2=qubits[6:], r3=qubits[5])
+    op2 = tg.on(*qubits[:5], *qubits[6:], qubits[5])
+    assert op1 == op2
