f8a6f19614610b06b145cf65dd0916daa84ceea2
Tanuj Khattar <tanujkhattar@google.com>
2022-08-02 14:19:39 -0700

    Add support for extra registers to UnaryIteration and update SelectedMajoranaFermionGate to support any target_gate

--- cirq_qubitization/selected_majorana_fermion_test.py
+++ cirq_qubitization/selected_majorana_fermion_test.py
@@ -7,7 +7,8 @@ import cirq
     "selection_bitsize, target_bitsize",
     [(2, 4), (3, 8), (4, 9)],
 )
-def test_selected_majorana_fermion_gate(selection_bitsize, target_bitsize):
+@pytest.mark.parametrize("target_gate", [cirq.X, cirq.Y])
+def test_selected_majorana_fermion_gate(selection_bitsize, target_bitsize, target_gate):
     all_qubits = cirq.LineQubit.range(2 * selection_bitsize + target_bitsize + 2)
     control, selection, ancilla, accumulator, target = (
         all_qubits[0],
@@ -17,15 +18,15 @@ def test_selected_majorana_fermion_gate(selection_bitsize, target_bitsize):
         all_qubits[2 * selection_bitsize + 2 :],
     )
     gate = cirq_qubitization.SelectedMajoranaFermionGate(
-        selection_bitsize, target_bitsize
+        selection_bitsize, target_bitsize, target_gate=target_gate
     )
     circuit = cirq.Circuit(
         gate.on_registers(
-            control_register=control,
-            selection_register=selection,
-            selection_ancilla=ancilla,
+            control=control,
+            selection=selection,
+            ancilla=ancilla,
             accumulator=accumulator,
-            target_register=target,
+            target=target,
         )
     )
 
@@ -49,10 +50,50 @@ def test_selected_majorana_fermion_gate(selection_bitsize, target_bitsize):
 
         expected_target_state = cirq.Circuit(
             [cirq.Z(q) for q in target[: selection_integer - 1]],
-            cirq.Y(target[selection_integer]),
+            target_gate(target[selection_integer]),
             [cirq.I(q) for q in target[selection_integer + 1 :]],
         ).final_state_vector(qubit_order=target)
 
         cirq.testing.assert_allclose_up_to_global_phase(
             expected_target_state, final_target_state, atol=1e-6
         )
+
+
+def test_selected_majorana_fermion_gate_diagram():
+    selection_bitsize, target_bitsize = 3, 5
+    gate = cirq_qubitization.SelectedMajoranaFermionGate(
+        selection_bitsize, target_bitsize, target_gate=cirq.X
+    )
+    circuit = cirq.Circuit(gate.on_registers(**gate.registers.get_named_qubits()))
+    qubits = list(q for v in gate.registers.get_named_qubits().values() for q in v)
+    cirq.testing.assert_has_diagram(
+        circuit,
+        """
+control: ───────@─────
+                │
+selection0: ────In────
+                │
+selection1: ────In────
+                │
+selection2: ────In────
+                │
+ancilla0: ──────Anc───
+                │
+ancilla1: ──────Anc───
+                │
+ancilla2: ──────Anc───
+                │
+target0: ───────ZX────
+                │
+target1: ───────ZX────
+                │
+target2: ───────ZX────
+                │
+target3: ───────ZX────
+                │
+target4: ───────ZX────
+                │
+accumulator: ───Acc───
+    """,
+        qubit_order=qubits,
+    )
