48ab2d4dc3581cd06c09beaeade227bd0b315eef
Tanuj Khattar <tanujkhattar@google.com>
2022-08-10 16:55:42 -0700

    Add docstrings, address comments

--- cirq_qubitization/and_gate.py
+++ cirq_qubitization/and_gate.py
@@ -55,7 +55,15 @@ class And(GateWithRegisters):
     def _has_unitary_(self) -> bool:
         return not self.adjoint
 
-    def _decompose_single_and(self, cv1, cv2, c1, c2, target) -> cirq.OP_TREE:
+    def _decompose_single_and(
+        self, cv1: int, cv2: int, c1: cirq.Qid, c2: cirq.Qid, target: cirq.Qid
+    ) -> cirq.OP_TREE:
+        """Decomposes a single `And` gate on 2 controls and 1 target in terms of Clifford+T gates.
+
+        * And(cv).on(c1, c2, target) uses 4 T-gates and assumes target is in |0> state.
+        * And(cv, adjoint=True).on(c1, c2, target) uses measurement based un-computation
+            (0 T-gates) and will always leave the target in |0> state.
+        """
         pre_post_ops = [cirq.X(q) for (q, v) in zip([c1, c2], [cv1, cv2]) if v == 0]
         yield pre_post_ops
         if self.adjoint:
@@ -79,22 +87,23 @@ class And(GateWithRegisters):
         ancillas: Sequence[cirq.Qid],
         target: cirq.Qid,
     ) -> cirq.OP_TREE:
+        """Decomposes multi-controlled `And` in-terms of an `And` ladder of size #controls- 2."""
         if len(controls) == 2:
             yield And(control_values, adjoint=self.adjoint).on(*controls, target)
+            return
+        new_controls = (ancillas[0], *controls[2:])
+        new_control_values = (1, *control_values[2:])
+        and_op = And(control_values[:2], adjoint=self.adjoint).on(*controls[:2], ancillas[0])
+        if self.adjoint:
+            yield from self._decompose_via_tree(
+                new_controls, new_control_values, ancillas[1:], target
+            )
+            yield and_op
         else:
-            new_controls = (ancillas[0], *controls[2:])
-            new_control_values = (1, *control_values[2:])
-            and_op = And(control_values[:2], adjoint=self.adjoint).on(*controls[:2], ancillas[0])
-            if self.adjoint:
-                yield from self._decompose_via_tree(
-                    new_controls, new_control_values, ancillas[1:], target
-                )
-                yield and_op
-            else:
-                yield and_op
-                yield from self._decompose_via_tree(
-                    new_controls, new_control_values, ancillas[1:], target
-                )
+            yield and_op
+            yield from self._decompose_via_tree(
+                new_controls, new_control_values, ancillas[1:], target
+            )
 
     def decompose_from_registers(
         self, control: Sequence[cirq.Qid], ancilla: Sequence[cirq.Qid], target: Sequence[cirq.Qid]
