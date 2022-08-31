f24814d074d74bacab012b5f92229386f5be2f72
Noureldin <noureldinyosri@gmail.com>
2022-08-31 18:22:14 +0100

    added reuse option to qubit manager

--- cirq_qubitization/qubit_manager.py
+++ cirq_qubitization/qubit_manager.py
@@ -1,40 +1,45 @@
 import cirq
-from typing import List
+from typing import List, Optional
 
-class QubitManager:
-    _free_qubits = []
-    _total_allocated = 0
+_free_qubits = []
+_total_allocated = 0
 
-    @classmethod
-    def _allocate(cls):
-        if not len(cls._free_qubits):
-            cls._total_allocated += 1
-            cls._free_qubits.append(cirq.NamedQubit(''))
-        return cls._free_qubits.pop()
+def _allocate(name: Optional[str] = None, reuse: bool = True):
+    global _free_qubits, _total_allocated
+    if not (reuse and len(_free_qubits)):
+        name = name or f'q{_total_allocated}'
+        _free_qubits.append(cirq.NamedQubit(name))
+        _total_allocated += 1
+    return _free_qubits.pop()
 
-    @classmethod
-    def _free(cls, q: cirq.NamedQubit):
-        cls._free_qubits.append(q)
+def _free(q: cirq.NamedQubit):
+    global _free_qubits
+    _free_qubits.append(q)
 
-def qalloc(n: int = 1, prefix: str = "") -> List[cirq.NamedQubit]:
+def qalloc(n: int = 1, prefix: str = "", reuse: bool = True) -> List[cirq.NamedQubit]:
     name = lambda i: prefix + str(i)
     if n == 1: name = lambda i: prefix
-    qubits = [QubitManager._allocate() for i in range(n)]
-    for i in range(n):
-        qubits[i]._name = name(i)
+    qubits = [_allocate(name(i), reuse=reuse) for i in range(n)]
+    if reuse:
+        for i in range(n):
+            qubits[i]._name = name(i)
     return qubits
 
 def qfree(q: cirq.NamedQubit) -> None:
-    QubitManager._free(q)
+    _free(q)
 
 
 def qtot() -> int:
-    return QubitManager._total_allocated
+    global _total_allocated
+    return _total_allocated
 
 if __name__ == "__main__":
     qs = qalloc(2, "test")
     print(qs)
     qfree(qs.pop(0))
     qs.extend(qalloc(2, "x"))
+    print(qs)
+    print(qtot()) 
+    qs.extend(qalloc(2, prefix='noreuse', reuse=False))
     print(qs)
     print(qtot()) 
