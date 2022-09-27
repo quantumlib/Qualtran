d3cadc9ec9479b9210eef8d2f7ff56871534c0f9
Matthew Harrigan <matthew.harrigan@outlook.com>
2022-09-27 09:43:59 -0700

    Jupyter tools (#71)
    Initial commit of tools to easily diagram gates.

---
+++ cirq_qubitization/jupyter_tools_test.py
@@ -0,0 +1,30 @@
+import IPython.display
+import ipywidgets
+
+import cirq_qubitization.testing as cq_testing
+from cirq_qubitization import And
+from cirq_qubitization.jupyter_tools import svg_circuit, display_gate_and_compilation
+
+
+def test_svg_circuit():
+    g = cq_testing.GateHelper(And(cv=(1, 1, 1)))
+    svg = svg_circuit(g.circuit, g.r)
+    svg_str: str = svg.data
+
+    # check that the order is respected in the svg data.
+    assert svg_str.find('control') < svg_str.find('ancilla') < svg_str.find('target')
+
+
+def test_display_gate_and_compilation(monkeypatch):
+    call_args = []
+
+    def _dummy_display(stuff):
+        call_args.append(stuff)
+
+    monkeypatch.setattr(IPython.display, "display", _dummy_display)
+    g = cq_testing.GateHelper(And(cv=(1, 1, 1)))
+    display_gate_and_compilation(g)
+
+    (display_arg,) = call_args
+    assert isinstance(display_arg, ipywidgets.HBox)
+    assert len(display_arg.children) == 2
