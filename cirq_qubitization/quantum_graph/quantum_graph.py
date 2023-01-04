8377056e875680c91452f2bcd496f80d5827b541
Matthew Harrigan <mpharrigan@google.com>
2023-01-04 15:55:14 -0800

    Connection helper method

--- cirq_qubitization/quantum_graph/quantum_graph.py
+++ cirq_qubitization/quantum_graph/quantum_graph.py
@@ -1,3 +1,4 @@
+from functools import cached_property
 from typing import Tuple, Union
 
 from attrs import field, frozen
@@ -91,8 +92,8 @@ def _singleton_error(self, x):
 
 DanglingT.__init__ = _singleton_error
 
-
-@frozen
+# attrs note: Slots come into play because of our use of cached_property??? TODO: figure out
+@frozen(slots=False)
 class Connection:
     """A connection between two `Soquet`s.
 
@@ -102,3 +103,13 @@ class Connection:
 
     left: Soquet
     right: Soquet
+
+    @cached_property
+    def shape(self) -> int:
+        ls = self.left.reg.bitsize
+        rs = self.right.reg.bitsize
+
+        if ls != rs:
+            # raise ValueError(f"Invalid Connection {self}: shape mismatch: {ls} != {rs}")
+            print(f"Invalid Connection {self}: shape mismatch: {ls} != {rs}")
+        return ls
