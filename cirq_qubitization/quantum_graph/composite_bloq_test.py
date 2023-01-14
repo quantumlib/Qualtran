3af4cbb3b39844e22cb524101a1e97a1472e3e66
Matthew Harrigan <mpharrigan@google.com>
2023-01-13 16:26:08 -0800

    [quantum_graph] Rename to "connection" (#120)
    Rename to "connection"

--- cirq_qubitization/quantum_graph/composite_bloq_test.py
+++ cirq_qubitization/quantum_graph/composite_bloq_test.py
@@ -17,25 +17,25 @@ from cirq_qubitization.quantum_graph.composite_bloq import (
 )
 from cirq_qubitization.quantum_graph.quantum_graph import (
     BloqInstance,
+    Connection,
     LeftDangle,
     RightDangle,
     Soquet,
-    Wire,
 )
 
 
-def _manually_make_test_cbloq_wires():
+def _manually_make_test_cbloq_cxns():
     tb = TestBloq()
     binst1 = BloqInstance(tb, 1)
     binst2 = BloqInstance(tb, 2)
     assert binst1 != binst2
     return [
-        Wire(Soquet(LeftDangle, 'q1'), Soquet(binst1, 'control')),
-        Wire(Soquet(LeftDangle, 'q2'), Soquet(binst1, 'target')),
-        Wire(Soquet(binst1, 'control'), Soquet(binst2, 'target')),
-        Wire(Soquet(binst1, 'target'), Soquet(binst2, 'control')),
-        Wire(Soquet(binst2, 'control'), Soquet(RightDangle, 'q1')),
-        Wire(Soquet(binst2, 'target'), Soquet(RightDangle, 'q2')),
+        Connection(Soquet(LeftDangle, 'q1'), Soquet(binst1, 'control')),
+        Connection(Soquet(LeftDangle, 'q2'), Soquet(binst1, 'target')),
+        Connection(Soquet(binst1, 'control'), Soquet(binst2, 'target')),
+        Connection(Soquet(binst1, 'target'), Soquet(binst2, 'control')),
+        Connection(Soquet(binst2, 'control'), Soquet(RightDangle, 'q1')),
+        Connection(Soquet(binst2, 'target'), Soquet(RightDangle, 'q2')),
     ]
 
 
@@ -53,18 +53,18 @@ class TestComposite(Bloq):
 
 
 def test_create_binst_graph():
-    wires = _manually_make_test_cbloq_wires()
-    binst1 = wires[2].left.binst
-    binst2 = wires[2].right.binst
-    binst_graph = _create_binst_graph(wires)
+    cxns = _manually_make_test_cbloq_cxns()
+    binst1 = cxns[2].left.binst
+    binst2 = cxns[2].right.binst
+    binst_graph = _create_binst_graph(cxns)
 
     binst_generations = list(nx.topological_generations(binst_graph))
     assert binst_generations == [[LeftDangle], [binst1], [binst2], [RightDangle]]
 
 
 def test_composite_bloq():
-    wires = _manually_make_test_cbloq_wires()
-    cbloq = CompositeBloq(wires=wires, registers=Registers.build(q1=1, q2=1))
+    cxns = _manually_make_test_cbloq_cxns()
+    cbloq = CompositeBloq(cxns=cxns, registers=Registers.build(q1=1, q2=1))
     circuit = cbloq.to_cirq_circuit(q1=[cirq.LineQubit(1)], q2=[cirq.LineQubit(2)])
     cirq.testing.assert_has_diagram(
         circuit,
