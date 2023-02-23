3c3521f55bdd9525f6eda7777bc1965067015a19
Matthew Harrigan <mpharrigan@google.com>
2023-02-23 12:03:12 -0800

    remove partition

--- cirq_qubitization/quantum_graph/util_bloqs.py
+++ cirq_qubitization/quantum_graph/util_bloqs.py
@@ -1,7 +1,7 @@
 from functools import cached_property
-from typing import Sequence, Tuple, TYPE_CHECKING, Union
+from typing import Sequence, TYPE_CHECKING, Union
 
-from attrs import field, frozen
+from attrs import frozen
 
 from cirq_qubitization.quantum_graph.bloq import Bloq
 from cirq_qubitization.quantum_graph.fancy_registers import FancyRegister, FancyRegisters, Side
@@ -88,58 +88,3 @@ class Free(Bloq):
     @cached_property
     def registers(self) -> FancyRegisters:
         return FancyRegisters([FancyRegister('free', bitsize=self.n, side=Side.LEFT)])
-
-
-@frozen
-class Partition(Bloq):
-    """Partition one register into a number of new registers of uneven size.
-
-    Args:
-        sizes: Create one right register per size of the given bitsize.
-    """
-
-    sizes: Tuple[int, ...] = field(converter=tuple)
-
-    @cached_property
-    def registers(self) -> FancyRegisters:
-        tot = sum(self.sizes)
-        return FancyRegisters(
-            [FancyRegister(name='x', bitsize=tot, wireshape=tuple(), side=Side.LEFT)]
-            + [
-                FancyRegister(name=f'y{i}', bitsize=n, wireshape=tuple(), side=Side.RIGHT)
-                for i, n in enumerate(self.sizes)
-            ]
-        )
-
-    def on_registers(
-        self, **qubit_regs: Union['cirq.Qid', Sequence['cirq.Qid']]
-    ) -> 'cirq.GateOperation':
-        return None
-
-
-@frozen
-class Unpartition(Bloq):
-    """Take a number of registers of uneven size and combine into one register.
-
-    Args:
-        sizes: Input one left register per size of the given bitsize. The output register
-            will be of bitsize `sum(sizes)`.
-    """
-
-    sizes: Tuple[int, ...]
-
-    @cached_property
-    def registers(self) -> FancyRegisters:
-        tot = sum(self.sizes)
-        return FancyRegisters(
-            [
-                FancyRegister(name=f'y{i}', bitsize=n, wireshape=tuple(), side=Side.LEFT)
-                for i, n in enumerate(self.sizes)
-            ]
-            + [FancyRegister(name='x', bitsize=tot, wireshape=tuple(), side=Side.RIGHT)]
-        )
-
-    def on_registers(
-        self, **qubit_regs: Union['cirq.Qid', Sequence['cirq.Qid']]
-    ) -> 'cirq.GateOperation':
-        return None
