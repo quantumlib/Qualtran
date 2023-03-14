8bbd33841a4779c0c8107d63a3d85e676502f3b5
Matthew Harrigan <mpharrigan@google.com>
2023-03-14 14:04:29 -0700

    Quimb and Gates -- mergefix (#151)
    see #140 for review history

---
+++ cirq_qubitization/bloq_algos/basic_gates/cnot_test.py
@@ -0,0 +1,54 @@
+import cirq
+import numpy as np
+
+from cirq_qubitization.bloq_algos.basic_gates import CNOT, PlusState, ZeroState
+from cirq_qubitization.quantum_graph.composite_bloq import CompositeBloqBuilder
+from cirq_qubitization.quantum_graph.fancy_registers import FancyRegisters
+from cirq_qubitization.quantum_graph.quimb_sim import bloq_to_dense, cbloq_to_dense
+
+
+def _make_CNOT():
+    from cirq_qubitization.bloq_algos.basic_gates import CNOT
+
+    return CNOT()
+
+
+def test_cnot():
+    bloq = CNOT()
+    matrix = bloq_to_dense(bloq)
+    # fmt: off
+    should_be = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0]])
+    # fmt: on
+    np.testing.assert_allclose(should_be, matrix)
+
+
+def test_cnot_cbloq():
+    bb, soqs = CompositeBloqBuilder.from_registers(FancyRegisters.build(c=1, t=1))
+    c, t = bb.add(CNOT(), ctrl=soqs['c'], target=soqs['t'])
+    cbloq = bb.finalize(c=c, t=t)
+    matrix = cbloq_to_dense(cbloq)
+
+    c_qs = cirq.LineQubit.range(2)
+    c_circ = cirq.Circuit(cirq.CNOT(c_qs[0], c_qs[1]))
+    c_matrix = c_circ.unitary(qubit_order=c_qs)
+
+    np.testing.assert_allclose(c_matrix, matrix)
+
+
+def test_bell_state():
+    bb = CompositeBloqBuilder()
+
+    (q0,) = bb.add(PlusState())
+    (q1,) = bb.add(ZeroState())
+
+    q0, q1 = bb.add(CNOT(), ctrl=q0, target=q1)
+
+    cbloq = bb.finalize(q0=q0, q1=q1)
+    matrix = cbloq_to_dense(cbloq)
+
+    should_be = np.array([1, 0, 0, 1]) / np.sqrt(2)
+    np.testing.assert_allclose(should_be, matrix)
---
+++ cirq_qubitization/bloq_algos/basic_gates_test.py
@@ -0,0 +1,5 @@
+import cirq_qubitization.testing as cq_testing
+
+
+def test_notebook():
+    cq_testing.execute_notebook('bloq_algos/basic_gates')
