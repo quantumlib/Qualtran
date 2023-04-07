351767513fd9f495e4876ad54d2d8c63e5df6080
Tanuj Khattar <tanujkhattar@google.com>
2023-04-07 12:21:30 -0700

    Add multi controlled not using n dirty ancillas (#184)
    * Fix state preparation using coherent alias sampling
    
    * Regenerate notebooks
    
    * Update tests
    
    * Add multi controlled not using n dirty ancillas
    
    * Add _t_complexity_ protocol formula and tests to MultiControlNOT
    
    * type annotation for rmul

--- cirq_qubitization/cirq_algos/__init__.py
+++ cirq_qubitization/cirq_algos/__init__.py
@@ -1,7 +1,10 @@
 from cirq_qubitization.cirq_algos.and_gate import And
 from cirq_qubitization.cirq_algos.apply_gate_to_lth_target import ApplyGateToLthQubit
 from cirq_qubitization.cirq_algos.arithmetic_gates import LessThanEqualGate, LessThanGate
-from cirq_qubitization.cirq_algos.multi_target_cnot import MultiTargetCNOT
+from cirq_qubitization.cirq_algos.multi_control_multi_target_cnot import (
+    MultiControlNOT,
+    MultiTargetCNOT,
+)
 from cirq_qubitization.cirq_algos.prepare_uniform_superposition import PrepareUniformSuperposition
 from cirq_qubitization.cirq_algos.programmable_rotation_gate_array import (
     ProgrammableRotationGateArray,
