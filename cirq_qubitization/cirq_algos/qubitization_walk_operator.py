0ac738e06d65b04f546961e44e104efd5704f439
Tanuj Khattar <tanujkhattar@google.com>
2023-04-18 06:24:35 +0530

    Add docstrings

--- cirq_qubitization/cirq_algos/qubitization_walk_operator.py
+++ cirq_qubitization/cirq_algos/qubitization_walk_operator.py
@@ -9,6 +9,34 @@ from cirq_qubitization.cirq_algos import reflection_using_prepare, state_prepara
 
 @cirq.value_equality()
 class QubitizationWalkOperator(cirq_infra.GateWithRegisters):
+    """Constructs a Szegedy Quantum Walk operator using LCU oracles SELECT and PREPARE.
+
+    Constructs a Szegedy quantum walk operator $W = R_{L} . SELECT$, which is a product of
+    two reflections $R_{L} = (2|L><L| - I)$ and $SELECT=\sum_{l}|l><l|H_{l}$.
+
+    The action of $W$ partitions the Hilbert space into a direct sum of two-dimensional irreducible
+    vector spaces s.t. $|\ell>|k>$ and an orthogonal state $\phi_{k}$ span the irreducible two-
+    dimensional space that $|\ell>|k>$ is in under the action of $W$ for arbitrary eigenstate $|k>$
+    of $H$ with eigenvalue $E_k$. In this space, $W$ implements a Pauli-Y rotation by an angle of
+    $-2arccos(E_{k} / \lambda)$ s.t. $W = e^{i arccos(E_k / \lambda) Y}$.
+
+    Thus, the walk operator $W$ encodes the spectrum of $H$ as a function of eigen phases of $W$
+    s.t. $spectrum(H) = \lambda cos(arg(spectrum(W)))$ where $arg(e^{i\phi}) = \phi$.
+
+    Args:
+        select: The SELECT lcu gate implementing $SELECT=\sum_{l}|l><l|H_{l}$.
+        prepare: Then PREPARE lcu gate implementing
+            $PREPARE|00...00> = \sum_{l=0}^{L - 1}\sqrt{\frac{w_{l}}{\lambda}} |l> = |\ell>
+        control_val: If 0/1, a controlled version of the walk operator is constructed. Defaults to
+            None, in which case the resulting walk operator is not controlled.
+        power: Constructs $W^{power}$ by repeatedly decomposing into `power` copies of $W$.
+            Defaults to 1.
+
+    References:
+        [Encoding Electronic Spectra in Quantum Circuits with Linear T Complexity](https://arxiv.org/abs/1805.03662).
+        Babbush et. al. (2018). Figure 1.
+    """
+
     def __init__(
         self,
         select: generic_select.GenericSelect,
