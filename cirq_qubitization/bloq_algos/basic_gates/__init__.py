bfdcacbefc18702ca0c8fd3859d0be47adf37c87
Fionn Malone <fmalone@google.com>
2023-05-10 19:15:36 +0000

    Add rotation gates to __init__.

--- cirq_qubitization/bloq_algos/basic_gates/__init__.py
+++ cirq_qubitization/bloq_algos/basic_gates/__init__.py
@@ -1,3 +1,4 @@
 from .cnot import CNOT
+from .rotation import Rx, Ry, Rz
 from .x_basis import MinusEffect, MinusState, PlusEffect, PlusState, XGate
 from .z_basis import OneEffect, OneState, ZeroEffect, ZeroState, ZGate
