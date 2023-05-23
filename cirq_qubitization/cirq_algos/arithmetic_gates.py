81a55bab1df1ee7da1767889bcda2685acf97df0
Noureldin <noureldinyosri@gmail.com>
2023-05-23 17:48:39 +0100

    added citation

--- cirq_qubitization/cirq_algos/arithmetic_gates.py
+++ cirq_qubitization/cirq_algos/arithmetic_gates.py
@@ -109,6 +109,64 @@ class LessThanGate(cirq.ArithmeticGate):
         )
 
 
+@frozen
+class AddMod(cirq.ArithmeticGate):
+    """Applies U_{M}_{add}|x> = |(x + add) % M> if x < M else |x>.
+
+    Applies modular addition to input register `|x>` given parameters `mod` and `add_val` s.t.
+        1) If integer `x` < `mod`: output is `|(x + add) % M>`
+        2) If integer `x` >= `mod`: output is `|x>`.
+
+    This condition is needed to ensure that the mapping of all input basis states (i.e. input
+    states |0>, |1>, ..., |2 ** bitsize - 1) to corresponding output states is bijective and thus
+    the gate is reversible.
+
+    Also supports controlled version of the gate by specifying a per qubit control value as a tuple
+    of integers passed as `cv`.
+    """
+
+    bitsize: int
+    mod: int = field()
+    add_val: int = 1
+    cv: Tuple[int, ...] = field(converter=tuple, default=())
+
+    @mod.validator
+    def _validate_mod(self, attribute, value):
+        if not 1 <= value <= 2**self.bitsize:
+            raise ValueError(f"mod: {value} must be between [1, {2 ** self.bitsize}].")
+
+    def registers(self) -> Sequence[Union[int, Sequence[int]]]:
+        add_reg = (2,) * self.bitsize
+        control_reg = (2,) * len(self.cv)
+        return (control_reg, add_reg) if control_reg else (add_reg,)
+
+    def with_registers(self, *new_registers: Union[int, Sequence[int]]) -> "AddMod":
+        raise NotImplementedError()
+
+    def apply(self, *args) -> Union[int, Iterable[int]]:
+        target_val = args[-1]
+        if target_val < self.mod:
+            new_target_val = (target_val + self.add_val) % self.mod
+        else:
+            new_target_val = target_val
+        if self.cv and args[0] != int(''.join(str(x) for x in self.cv), 2):
+            new_target_val = target_val
+        ret = (args[0], new_target_val) if self.cv else (new_target_val,)
+        return ret
+
+    def _circuit_diagram_info_(self, _) -> cirq.CircuitDiagramInfo:
+        wire_symbols = ['@' if b else '@(0)' for b in self.cv]
+        wire_symbols += [f"Add_{self.add_val}_Mod_{self.mod}"] * self.bitsize
+        return cirq.CircuitDiagramInfo(wire_symbols=wire_symbols)
+
+    def __pow__(self, power: int) -> 'AddMod':
+        return AddMod(self.bitsize, self.mod, add_val=self.add_val * power, cv=self.cv)
+
+    def _t_complexity_(self) -> 't_complexity_protocol.TComplexity':
+        # Rough cost as given in https://arxiv.org/abs/1905.09749
+        return 5 * t_complexity_protocol.t_complexity(AdditionGate(self.bitsize))
+
+
 def mix_double_qubit_registers(
     x: Tuple[cirq.Qid, cirq.Qid], y: Tuple[cirq.Qid, cirq.Qid]
 ) -> cirq.OP_TREE:
@@ -194,6 +252,7 @@ class LessThanEqualGate(cirq.ArithmeticGate):
     def _decompose_via_tree(
         self, X: Tuple[cirq.Qid], Y: Tuple[cirq.Qid]
     ) -> Tuple[Sequence[cirq.Operation], Tuple[cirq.Qid, cirq.Qid]]:
+        """Implements the comparison oracle from https://www.nature.com/articles/s41534-018-0071-5#Sec8."""
         assert len(X) == len(Y), f'{len(X)=} != {len(Y)=}'
         if len(X) == 1:
             return [], (X[0], Y[0])
@@ -347,64 +406,6 @@ class ContiguousRegisterGate(cirq.ArithmeticGate):
         toffoli_complexity = t_complexity_protocol.t_complexity(cirq.CCNOT)
         n = len(self._p_register)
         return (n**2 + n - 1) * toffoli_complexity
-
-
-@frozen
-class AddMod(cirq.ArithmeticGate):
-    """Applies U_{M}_{add}|x> = |(x + add) % M> if x < M else |x>.
-
-    Applies modular addition to input register `|x>` given parameters `mod` and `add_val` s.t.
-        1) If integer `x` < `mod`: output is `|(x + add) % M>`
-        2) If integer `x` >= `mod`: output is `|x>`.
-
-    This condition is needed to ensure that the mapping of all input basis states (i.e. input
-    states |0>, |1>, ..., |2 ** bitsize - 1) to corresponding output states is bijective and thus
-    the gate is reversible.
-
-    Also supports controlled version of the gate by specifying a per qubit control value as a tuple
-    of integers passed as `cv`.
-    """
-
-    bitsize: int
-    mod: int = field()
-    add_val: int = 1
-    cv: Tuple[int, ...] = field(converter=tuple, default=())
-
-    @mod.validator
-    def _validate_mod(self, attribute, value):
-        if not 1 <= value <= 2**self.bitsize:
-            raise ValueError(f"mod: {value} must be between [1, {2 ** self.bitsize}].")
-
-    def registers(self) -> Sequence[Union[int, Sequence[int]]]:
-        add_reg = (2,) * self.bitsize
-        control_reg = (2,) * len(self.cv)
-        return (control_reg, add_reg) if control_reg else (add_reg,)
-
-    def with_registers(self, *new_registers: Union[int, Sequence[int]]) -> "AddMod":
-        raise NotImplementedError()
-
-    def apply(self, *args) -> Union[int, Iterable[int]]:
-        target_val = args[-1]
-        if target_val < self.mod:
-            new_target_val = (target_val + self.add_val) % self.mod
-        else:
-            new_target_val = target_val
-        if self.cv and args[0] != int(''.join(str(x) for x in self.cv), 2):
-            new_target_val = target_val
-        ret = (args[0], new_target_val) if self.cv else (new_target_val,)
-        return ret
-
-    def _circuit_diagram_info_(self, _) -> cirq.CircuitDiagramInfo:
-        wire_symbols = ['@' if b else '@(0)' for b in self.cv]
-        wire_symbols += [f"Add_{self.add_val}_Mod_{self.mod}"] * self.bitsize
-        return cirq.CircuitDiagramInfo(wire_symbols=wire_symbols)
-
-    def __pow__(self, power: int) -> 'AddMod':
-        return AddMod(self.bitsize, self.mod, add_val=self.add_val * power, cv=self.cv)
-
-    def _t_complexity_(self) -> 't_complexity_protocol.TComplexity':
-        # Rough cost as given in https://arxiv.org/abs/1905.09749
-        return 5 * t_complexity_protocol.t_complexity(AdditionGate(self.bitsize))
 
 
 class AdditionGate(cirq.ArithmeticGate):
