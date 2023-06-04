8f6a22db988d9e247ea8af31d2de95ec429269dc
Fionn Malone <fmalone@google.com>
2023-06-04 19:55:25 +0000

    Allow for multi-dimensional data to be loaded into QROM.
    Instead of accepting a list of 1D lists of integer data we now can
    accept a list of numpy.ndarrays from which the dimension of
    each selection register is deduced.

--- cirq_qubitization/cirq_algos/qrom.py
+++ cirq_qubitization/cirq_algos/qrom.py
@@ -2,76 +2,105 @@ from functools import cached_property
 from typing import Callable, Optional, Sequence, Tuple
 
 import cirq
+import numpy as np
+from attrs import frozen
+from numpy.typing import NDArray
 
 from cirq_qubitization import cirq_infra
 from cirq_qubitization.cirq_algos import and_gate, unary_iteration
 
 
-@cirq.value_equality
+@frozen
 class QROM(unary_iteration.UnaryIterationGate):
-    """Gate to load data[l] in the target register when the selection register stores integer l."""
+    """Gate to load n-dimensional data in the target register when the selection
+    register stores the index of the data element to load.
+    """
 
-    def __init__(
-        self,
-        *data: Sequence[int],
-        target_bitsizes: Optional[Sequence[int]] = None,
-        num_controls: int = 0,
-    ):
-        if len(set(len(d) for d in data)) != 1:
-            raise ValueError("All data sequences to load must be of equal length.")
-        self._data = tuple(tuple(d) for d in data)
-        self._selection_bitsize = (len(data[0]) - 1).bit_length()
-        self._num_controls = num_controls
-        if target_bitsizes is None:
-            target_bitsizes = [max(d).bit_length() for d in data]
-        assert len(target_bitsizes) == len(data)
-        assert all(t >= max(d).bit_length() for t, d in zip(target_bitsizes, data))
-        self._target_bitsizes = tuple(target_bitsizes)
+    data: Sequence[NDArray[np.int_]]
+    selection_bitsizes: Sequence[int]
+    target_bitsizes: Sequence[int]
+    num_controls: int = 0
+
+    @classmethod
+    def build(cls, data: Sequence[NDArray[np.int_]], num_controls: int = 0) -> 'QROM':
+        selection_bitsizes = [(s - 1).bit_length() for s in data[0].shape]
+        target_bitsizes = [max(int(np.max(d)).bit_length(), 1) for d in data]
+        return QROM(
+            data=data,
+            selection_bitsizes=selection_bitsizes,
+            target_bitsizes=target_bitsizes,
+            num_controls=num_controls,
+        )
+
+    def __attrs_post_init__(self):
+        sizes = [np.prod(s.shape) for s in self.data]
+        assert all([isinstance(s, int) for s in self.selection_bitsizes])
+        assert all([isinstance(t, int) for t in self.target_bitsizes])
+        assert len(set(sizes)) == 1, f"Data must all have the same size: {sizes}"
+        assert len(self.target_bitsizes) == len(self.data)
+        assert all(
+            t >= int(np.max(d)).bit_length() for t, d in zip(self.target_bitsizes, self.data)
+        )
+
+    def __hash__(self):
+        return hash(tuple(tuple(d.ravel()) for d in self.data))
 
     @cached_property
     def control_registers(self) -> cirq_infra.Registers:
         return (
-            cirq_infra.Registers.build(control=self._num_controls)
-            if self._num_controls
+            cirq_infra.Registers.build(control=self.num_controls)
+            if self.num_controls
             else cirq_infra.Registers([])
         )
 
     @cached_property
     def selection_registers(self) -> cirq_infra.SelectionRegisters:
-        return cirq_infra.SelectionRegisters.build(
-            selection=(self._selection_bitsize, len(self._data[0]))
-        )
+        if len(self.data[0].shape) == 1:
+            return cirq_infra.SelectionRegisters.build(
+                selection=(self.selection_bitsizes[0], self.data[0].shape[0])
+            )
+        else:
+            return cirq_infra.SelectionRegisters.build(
+                **{
+                    f'selection{i}': (sb, len)
+                    for i, (len, sb) in enumerate(zip(self.data[0].shape, self.selection_bitsizes))
+                }
+            )
 
     @cached_property
     def target_registers(self) -> cirq_infra.Registers:
         return cirq_infra.Registers.build(
-            **{f'target{i}': len for i, len in enumerate(self._target_bitsizes)}
+            **{f'target{i}': len for i, len in enumerate(self.target_bitsizes)}
         )
 
-    @property
-    def data(self) -> Tuple[Tuple[int, ...], ...]:
-        return self._data
-
     def __repr__(self) -> str:
-        data_repr = ",".join(repr(d) for d in self.data)
-        return f"cirq_qubitization.QROM({data_repr}, target_bitsizes={self._target_bitsizes})"
+        data_repr = "[" + "".join(f"np.{repr(d)}," for d in self.data) + "]"
+        selection_repr = repr(self.selection_bitsizes)
+        target_repr = repr(self.target_bitsizes)
+        return f"cirq_qubitization.QROM({data_repr}, selection_bitsizes={selection_repr}, target_bitsizes={target_repr}, num_controls={self.num_controls})"
 
     def _load_nth_data(
-        self, n: int, gate: Callable[[cirq.Qid], cirq.Operation], **target_regs: Sequence[cirq.Qid]
+        self,
+        selection_idx: Tuple[int, ...],
+        gate: Callable[[cirq.Qid], cirq.Operation],
+        **target_regs: Sequence[cirq.Qid],
     ) -> cirq.OP_TREE:
-        for i, d in enumerate(self._data):
+        for i, d in enumerate(self.data):
             target = target_regs[f'target{i}']
-            for q, bit in zip(target, f'{d[n]:0{len(target)}b}'):
+            for q, bit in zip(target, f'{int(d[selection_idx]):0{len(target)}b}'):
                 if int(bit):
                     yield gate(q)
 
     def decompose_zero_selection(self, **qubit_regs: Sequence[cirq.Qid]) -> cirq.OP_TREE:
         controls = self.control_registers.merge_qubits(**qubit_regs)
         target_regs = {k: v for k, v in qubit_regs.items() if k in self.target_registers}
-        if self._num_controls == 0:
-            yield from self._load_nth_data(0, cirq.X, **target_regs)
-        elif self._num_controls == 1:
-            yield from self._load_nth_data(0, lambda q: cirq.CNOT(controls[0], q), **target_regs)
+        num_indx = len(self.data[0].shape)
+        if self.num_controls == 0:
+            yield from self._load_nth_data((0,) * num_indx, cirq.X, **target_regs)
+        elif self.num_controls == 1:
+            yield from self._load_nth_data(
+                (0,) * num_indx, lambda q: cirq.CNOT(controls[0], q), **target_regs
+            )
         else:
             and_ancilla = cirq_infra.qalloc(len(controls) - 2)
             and_target = cirq_infra.qalloc(1)[0]
@@ -79,29 +108,38 @@ class QROM(unary_iteration.UnaryIterationGate):
                 control=controls, ancilla=and_ancilla, target=and_target
             )
             yield multi_controlled_and
-            yield from self._load_nth_data(0, lambda q: cirq.CNOT(and_target, q), **target_regs)
+            yield from self._load_nth_data(
+                (0,) * num_indx, lambda q: cirq.CNOT(and_target, q), **target_regs
+            )
             yield multi_controlled_and**-1
             cirq_infra.qfree(and_ancilla + [and_target])
 
-    def nth_operation(
-        self, control: cirq.Qid, selection: int, **target_regs: Sequence[cirq.Qid]
-    ) -> cirq.OP_TREE:
-        yield from self._load_nth_data(selection, lambda q: cirq.CNOT(control, q), **target_regs)
+    def nth_operation(self, control: cirq.Qid, **qubit_regs: Sequence[cirq.Qid]) -> cirq.OP_TREE:
+        selection_idx = tuple(qubit_regs[reg.name] for reg in self.selection_registers)
+        target_regs = {k: v for k, v in qubit_regs.items() if k in self.target_registers}
+        yield from self._load_nth_data(
+            selection_idx, lambda q: cirq.CNOT(control, q), **target_regs
+        )
 
     def _circuit_diagram_info_(self, _) -> cirq.CircuitDiagramInfo:
-        wire_symbols = ["@"] * self._num_controls
-        wire_symbols += ["In"] * self._selection_bitsize
+        wire_symbols = ["@"] * self.num_controls
+        wire_symbols += ["In"] * self.selection_registers.bitsize
         for i, target in enumerate(self.target_registers):
             wire_symbols += [f"QROM_{i}"] * target.bitsize
         return cirq.CircuitDiagramInfo(wire_symbols=wire_symbols)
-
-    def __eq__(self, other: 'QROM'):
-        return self.data == other.data and self._target_bitsizes == other._target_bitsizes
 
     def __pow__(self, power: int):
         if power in [1, -1]:
             return self
         return NotImplemented
 
-    def _value_equality_values_(self):
-        return self._selection_bitsize, self._target_bitsizes, self._num_controls, self.data
+    def __eq__(self, other):
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        else:
+            return (
+                np.array_equal(self.data, other.data)
+                and np.array_equal(self.selection_bitsizes, other.selection_bitsizes)
+                and np.array_equal(self.target_bitsizes, other.target_bitsizes)
+                and np.array_equal(self.num_controls, other.num_controls)
+            )
