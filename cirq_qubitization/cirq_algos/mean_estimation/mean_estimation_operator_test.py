6aef3ad9018a97337165d3bce18bd8425ee84892
Tanuj Khattar <tanujkhattar@google.com>
2023-06-05 13:34:09 -0700

    Fix bug in tests for Robins mean estimation algorithm (#241)

--- cirq_qubitization/cirq_algos/mean_estimation/mean_estimation_operator_test.py
+++ cirq_qubitization/cirq_algos/mean_estimation/mean_estimation_operator_test.py
@@ -120,6 +120,10 @@ class BernoulliEncoder(SelectOracle):
         return self.p * (self.y[1] ** 2) + (1 - self.p) * (self.y[0] ** 2)
 
 
+def overlap(v1: np.ndarray, v2: np.ndarray) -> float:
+    return np.abs(np.vdot(v1, v2)) ** 2
+
+
 def satisfies_theorem_321(
     synthesizer: PrepareOracle,
     encoder: SelectOracle,
@@ -127,7 +131,6 @@ def satisfies_theorem_321(
     s: float,
     mu: float,
     arctan_bitsize: int,
-    debug_print: bool = False,
 ):
     r"""Verifies Theorem 3.21 of https://arxiv.org/abs/2208.07544
 
@@ -144,29 +147,20 @@ def satisfies_theorem_321(
     # Compute the final state vector obtained using the synthesizer `Prep |0>`
     prep_op = synthesizer.on_registers(**synthesizer.registers.get_named_qubits())
     prep_state = cirq.Circuit(prep_op).final_state_vector()
-    if debug_print:
-        print(cirq.dirac_notation(prep_state))
 
     expected_hav = abs(mu) * np.sqrt(1 / (1 + s**2))
     expected_hav_low = expected_hav / (1 + c * s)
     expected_hav_high = expected_hav / (1 - c * s)
 
-    if debug_print:
-        print(f'{expected_hav_low=}, {expected_hav=}, {expected_hav_high=}')
-
-    def is_good_eigvect(eig_val, eig_vect):
+    overlap_sum = 0
+    eigvals, eigvects = cirq.linalg.unitary_eig(u)
+    for (eig_val, eig_vect) in zip(eigvals, eigvects.T):
         theta = np.abs(np.angle(eig_val))
         hav_theta = np.sin(theta / 2)
-        overlap_prob = abs(np.dot(prep_state, eig_vect))
+        overlap_prob = overlap(prep_state, eig_vect)
         if expected_hav_low <= hav_theta <= expected_hav_high:
-            if debug_print:
-                print(f'{hav_theta=}, {overlap_prob=}, {1-2/c**2=}')
-            return overlap_prob >= 1 - 2 / (c**2)
-        return False
-
-    eigvals, eigvects = cirq.linalg.unitary_eig(u)
-
-    return any(is_good_eigvect(eig_val, eig_vect) for (eig_val, eig_vect) in zip(eigvals, eigvects))
+            overlap_sum += overlap_prob
+    return overlap_sum >= 1 - 2 / (c**2) > 0
 
 
 @pytest.mark.parametrize('selection_bitsize', [1, 2])
@@ -257,16 +251,13 @@ class GroverEncoder(SelectOracle):
         return (self.marked_val**2) / 2**self.n
 
 
-@pytest.mark.parametrize('n, marked_val, c', [(5, 1, 1.5), (4, 1, 1.5), (2, 1, np.sqrt(2))])
+@pytest.mark.parametrize('n, marked_val, c', [(5, 1, 4), (4, 1, 2), (2, 1, np.sqrt(2))])
 def test_mean_estimation_grover(
     n: int, marked_val: int, c: float, marked_item: int = 1, arctan_bitsize: int = 5
 ):
     synthesizer = GroverSynthesizer(n)
     encoder = GroverEncoder(n, marked_item=marked_item, marked_val=marked_val)
     s = np.sqrt(encoder.s_square)
-    # TODO(#229): This test currently passes only for small `c` since the overlap prob for these
-    # cases is ~15-30%. When we increase `c`, the expected overlap is much higher (c=1.5 implies an
-    # expected overlap of 0.11 whereas c=2 implies 0.5), which doesn't seem to be the case here.
     assert c * s < 1 and c >= 1 >= s
 
     assert satisfies_theorem_321(
