a90d6f8f603c5267527311c1433a4ee1b3121c4c
Fionn Malone <fmalone@google.com>
2023-06-06 18:56:55 +0000

    Address review comments.

--- cirq_qubitization/cirq_algos/qrom.py
+++ cirq_qubitization/cirq_algos/qrom.py
@@ -1,10 +1,10 @@
 from functools import cached_property
-from typing import Callable, Sequence, Tuple
+from typing import Callable, Sequence, Tuple, Union
 
 import cirq
 import numpy as np
 from attrs import frozen
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
 
 from cirq_qubitization import cirq_infra
 from cirq_qubitization.cirq_algos import and_gate, unary_iteration
@@ -12,31 +12,48 @@ from cirq_qubitization.cirq_algos import and_gate, unary_iteration
 
 @frozen
 class QROM(unary_iteration.UnaryIterationGate):
-    """Gate to load n-dimensional data in the target register when the selection
-    register stores the index of the data element to load.
+    """Gate to load data[l] in the target register when the selection stores an index l.
+
+    In the case of multi-dimensional data[p,q,r,...] we use of multple name
+    selection registers [p, q, r, ...] to index and load the data.
+
+    Args:
+        data: List of numpy ndarrays specifying the data to load. If the length
+            of this list is greater than one then we use the same selection indices
+            to load each dataset (for example, to load alt and keep data for
+            state preparation). Each data set is required to have the same
+            shape and to be of integer type.
+        selection_bitsizes: The number of bits used to represent each selection register
+            corresponding to the size of each dimension of the array. Should be
+            the same length as the shape of each of the datasets.
+        target_bitsizes: The number of bits used to represent the data
+            registers. This can be deduced from the maximum element of each of the
+            datasets. Should be of length len(data), i.e. the number of datasets.
+        num_controls: The number of control registers.
     """
 
-    data: Sequence[NDArray[np.int_]]
-    selection_bitsizes: Sequence[int]
-    target_bitsizes: Sequence[int]
+    data: Sequence[NDArray]
+    selection_bitsizes: Tuple[int, ...]
+    target_bitsizes: Tuple[int, ...]
     num_controls: int = 0
 
     @classmethod
-    def build(cls, data: Sequence[NDArray[np.int_]], num_controls: int = 0) -> 'QROM':
-        selection_bitsizes = [(s - 1).bit_length() for s in data[0].shape]
-        target_bitsizes = [max(int(np.max(d)).bit_length(), 1) for d in data]
+    def build(cls, *data: ArrayLike, num_controls: int = 0) -> 'QROM':
+        _data = [np.array(d, dtype=int) for d in data]
+        selection_bitsizes = tuple((s - 1).bit_length() for s in _data[0].shape)
+        target_bitsizes = tuple(max(int(np.max(d)).bit_length(), 1) for d in data)
         return QROM(
-            data=data,
+            data=_data,
             selection_bitsizes=selection_bitsizes,
             target_bitsizes=target_bitsizes,
             num_controls=num_controls,
         )
 
     def __attrs_post_init__(self):
-        sizes = [np.prod(s.shape) for s in self.data]
+        shapes = [d.shape for d in self.data]
         assert all([isinstance(s, int) for s in self.selection_bitsizes])
         assert all([isinstance(t, int) for t in self.target_bitsizes])
-        assert len(set(sizes)) == 1, f"Data must all have the same size: {sizes}"
+        assert len(set(shapes)) == 1, f"Data must all have the same size: {shapes}"
         assert len(self.target_bitsizes) == len(self.data)
         assert all(
             t >= int(np.max(d)).bit_length() for t, d in zip(self.target_bitsizes, self.data)
@@ -74,7 +91,7 @@ class QROM(unary_iteration.UnaryIterationGate):
         )
 
     def __repr__(self) -> str:
-        data_repr = "[" + "".join(f"np.{repr(d)}," for d in self.data) + "]"
+        data_repr = cirq._compat.proper_repr(self.data)
         selection_repr = repr(self.selection_bitsizes)
         target_repr = repr(self.target_bitsizes)
         return f"cirq_qubitization.QROM({data_repr}, selection_bitsizes={selection_repr}, target_bitsizes={target_repr}, num_controls={self.num_controls})"
@@ -91,15 +108,17 @@ class QROM(unary_iteration.UnaryIterationGate):
                 if int(bit):
                     yield gate(q)
 
-    def decompose_zero_selection(self, **qubit_regs: Sequence[cirq.Qid]) -> cirq.OP_TREE:
+    def decompose_zero_selection(
+        self, **qubit_regs: Union[int, Sequence[cirq.Qid]]
+    ) -> cirq.OP_TREE:
         controls = self.control_registers.merge_qubits(**qubit_regs)
         target_regs = {k: v for k, v in qubit_regs.items() if k in self.target_registers}
-        num_indx = len(self.data[0].shape)
+        zero_indx = (0,) * len(self.data[0].shape)
         if self.num_controls == 0:
-            yield from self._load_nth_data((0,) * num_indx, cirq.X, **target_regs)
+            yield from self._load_nth_data(zero_indx, cirq.X, **target_regs)
         elif self.num_controls == 1:
             yield from self._load_nth_data(
-                (0,) * num_indx, lambda q: cirq.CNOT(controls[0], q), **target_regs
+                zero_indx, lambda q: cirq.CNOT(controls[0], q), **target_regs
             )
         else:
             and_ancilla = cirq_infra.qalloc(len(controls) - 2)
@@ -109,7 +128,7 @@ class QROM(unary_iteration.UnaryIterationGate):
             )
             yield multi_controlled_and
             yield from self._load_nth_data(
-                (0,) * num_indx, lambda q: cirq.CNOT(and_target, q), **target_regs
+                zero_indx, lambda q: cirq.CNOT(and_target, q), **target_regs
             )
             yield multi_controlled_and**-1
             cirq_infra.qfree(and_ancilla + [and_target])
