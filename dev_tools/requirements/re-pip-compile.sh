7dbc51534b785c0e49f67ab3c40ef917232b0bf7
Matthew Harrigan <mpharrigan@google.com>
2023-06-20 12:35:52 -0700

    requirements

--- dev_tools/requirements/re-pip-compile.sh
+++ dev_tools/requirements/re-pip-compile.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-pip-compile --output-file=format.env.txt --resolver=backtracking deps/format.txt deps/runtime.txt
-pip-compile --output-file=pylint.env.txt --resolver=backtracking deps/pylint.txt deps/runtime.txt
-pip-compile --output-file=pytest.env.txt --resolver=backtracking deps/pytest.txt deps/runtime.txt
-pip-compile --output-file=dev.env.txt --resolver=backtracking deps/dev-tools.txt deps/runtime.txt
+# First, compile the runtime requirements
+pip-compile --output-file=runtime.env.txt --resolver=backtracking deps/runtime.txt
+
+# Then, mix in extra requirements for tests
+pip-compile --output-file=format.env.txt --resolver=backtracking deps/format.txt runtime.env.txt
+pip-compile --output-file=pylint.env.txt --resolver=backtracking deps/pylint.txt runtime.env.txt
+pip-compile --output-file=pytest.env.txt --resolver=backtracking deps/pytest.txt runtime.env.txt
+
+# Everything is included in dev env
+pip-compile --output-file=dev.env.txt --resolver=backtracking deps/dev-tools.txt runtime.env.txt
