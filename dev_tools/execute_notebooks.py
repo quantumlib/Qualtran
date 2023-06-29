ca2133d6559153a32e3cc07252b2ba7680172ab1
Matthew Harrigan <mpharrigan@google.com>
2023-06-28 17:47:51 -0700

    Tidy up execute_notebooks

--- dev_tools/execute_notebooks.py
+++ dev_tools/execute_notebooks.py
@@ -32,41 +32,40 @@ def get_nb_rel_paths(rootdir) -> List[Path]:
     return nb_rel_paths
 
 
-def get_html_rel_path(nb_rel_path: Path) -> Path:
-    """For a given input relative notebook path, get the desired output html path."""
-    fn = nb_rel_path.with_name(f'{nb_rel_path.stem}.html')
-    return fn
-
-
-def is_out_of_date(nbpath: Path, htmlpath: Path) -> bool:
-    """Whether the html file needs to be re-rendered according to its mtime."""
-    assert nbpath.exists()
-    if not htmlpath.exists():
+def is_out_of_date(source_path: Path, out_path: Path) -> bool:
+    """Whether the out_path file needs to be re-rendered according to its mtime."""
+    assert source_path.exists()
+    if not out_path.exists():
         print(' -> Out of date: output file does not exist')
         return True
 
-    nbmtime = nbpath.stat().st_mtime
-    htmtime = htmlpath.stat().st_mtime
-    ood = nbmtime > htmtime
+    source_mtime = source_path.stat().st_mtime
+    output_mtime = out_path.stat().st_mtime
+    ood = source_mtime > output_mtime
     if ood:
-        print(f' -> Out of date: {nbmtime} > {htmtime}')
+        print(f' -> Out of date: {source_mtime} > {output_mtime}')
     else:
         print(' -> Up to date.')
     return ood
 
 
-def execute_and_export_nb(nbpath: Path, htmlpath: Path, nboutpath: Path) -> Optional[Exception]:
+def execute_and_export_nb(
+    nbpath: Path, html_outpath: Optional[Path], nb_outpath: Optional[Path]
+) -> Optional[Exception]:
     """Execute the notebook and export it in various forms.
 
-    We execute the notebook at `nbpath` and export it as html to `htmlpath` and as
-    a filled-in notebook to `nboutpath`.
+    We execute the notebook at `nbpath` and export it as html to `html_outpath` and as
+    a filled-in notebook to `nb_outpath` if the paths are not None.
 
     During execution, we catch any exceptions raised by notebook execution
     and return it. Otherwise, we return `None`.
     """
+
+    # Load it in
     with nbpath.open() as f:
         nb = nbformat.read(f, as_version=4)
 
+    # Run it
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
     try:
         nb, resources = ep.preprocess(nb)
@@ -75,37 +74,40 @@ def execute_and_export_nb(nbpath: Path, htmlpath: Path, nboutpath: Path) -> Opti
         print(e)
         return e
 
-    with nboutpath.open('w') as f:
-        nbformat.write(nb, f, version=4)
-    html, resources = nbconvert.export(nbconvert.HTMLExporter(), nb, resources=resources)
-    with htmlpath.open('w') as f:
-        f.write(html)
+    # Optionally save executed notebook as notebook
+    if nb_outpath:
+        nb_outpath.parent.mkdir(parents=True, exist_ok=True)
+        with nb_outpath.open('w') as f:
+            nbformat.write(nb, f, version=4)
+
+    # Optionally save as html
+    if html_outpath:
+        html, resources = nbconvert.export(nbconvert.HTMLExporter(), nb, resources=resources)
+        html_outpath.parent.mkdir(parents=True, exist_ok=True)
+        with html_outpath.open('w') as f:
+            f.write(html)
 
 
-def main(render_all: bool = False):
+def main(output_nbs: bool, output_html: bool):
     """Find, execute, and export all checked-in ipynbs.
 
     Args:
-        render_all: If False (the default) only render out-of-date html outputs.
+        output_nbs: Whether to save the executed notebooks as notebooks
+        output_html: Whether to save the executed notebooks as html
     """
-    print("render_all:", render_all)
     reporoot = get_git_root()
     sourceroot = reporoot / 'cirq_qubitization'
     nb_rel_paths = get_nb_rel_paths(rootdir=sourceroot)
     bad_nbs = []
     for nb_rel_path in nb_rel_paths:
         print(f"Executing {nb_rel_path}")
-        htmlpath = reporoot / 'docs/nbrun' / get_html_rel_path(nb_rel_path)
         nbpath = sourceroot / nb_rel_path
-        nboutpath = reporoot / 'docs' / nb_rel_path
 
-        if not render_all and not is_out_of_date(nbpath, htmlpath):
-            continue
+        html_rel_path = nb_rel_path.with_name(f'{nb_rel_path.stem}.html')
+        html_outpath = reporoot / 'docs/nbrun' / html_rel_path if output_html else None
+        nb_outpath = reporoot / 'docs' / nb_rel_path if output_nbs else None
 
-        htmlpath.parent.mkdir(parents=True, exist_ok=True)
-        nboutpath.parent.mkdir(parents=True, exist_ok=True)
-
-        err = execute_and_export_nb(nbpath=nbpath, htmlpath=htmlpath, nboutpath=nboutpath)
+        err = execute_and_export_nb(nbpath=nbpath, html_outpath=html_outpath, nb_outpath=nb_outpath)
         if err is not None:
             bad_nbs.append(nbpath)
 
@@ -119,9 +121,10 @@ def main(render_all: bool = False):
 
 def parse_args():
     p = ArgumentParser()
-    p.add_argument('--all', action=argparse.BooleanOptionalAction, default=True)
+    p.add_argument('--output-nbs', action=argparse.BooleanOptionalAction, default=True)
+    p.add_argument('--output-html', action=argparse.BooleanOptionalAction, default=False)
     args = p.parse_args()
-    main(render_all=args.all)
+    main(output_nbs=args.output_nbs, output_html=args.output_html)
 
 
 if __name__ == '__main__':
