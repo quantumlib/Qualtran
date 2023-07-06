f6e55ff0fc72e3d73e68c27968ee1a6ef5fac22c
Matthew Harrigan <mpharrigan@google.com>
2023-07-06 10:38:22 -0700

    Delete everything else

--- dev_tools/test_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-
-def only_on_posix(func):
-    """Only run test on posix."""
-    return pytest.mark.skipif(os.name != "posix", reason=f"os {os.name} is not posix")(func)
