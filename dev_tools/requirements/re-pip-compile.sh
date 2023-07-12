77e08facbabd4abc48adc8d383a3a8efb3be6c51
Matthew Harrigan <mpharrigan@google.com>
2023-07-12 15:17:26 -0700

    Use constraints file in `pip-compile` to always ensure consistent dependency environments (#307)
    * Change the way we compile
    
    Now, the first thing we do is compile
    a complete and compatible environment
    which will be used to constrain the
    sub-environments.
    
    This is what I always wanted! The sneaky
    trick is to put the "-c constrain.txt"
    line in an external file instead of
    telling `pip-compile` itself.
    
    * Update deps/
    
     - unpin sphinx version since it has
       interdependencies with some of the
       other dependencies. We just let pip-compile
       handle that.
     - Actually include the docs.txt in the
       full dev env
    
    * Re-render dev.env.txt
    
    Note that `re-pip-compile.sh` now
    includes the `--upgrade` flag, so
    all the versions will be bumped to
    the largest self-consistent values
    
    * Re-render runtime.env.txt
    
    Note that this adds the dev.env.txt
    constraint to all the notes
    
    * Render the rest of the sub environments

--- dev_tools/requirements/re-pip-compile.sh
+++ dev_tools/requirements/re-pip-compile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# First, compile the runtime requirements
-pip-compile --output-file=runtime.env.txt --resolver=backtracking deps/runtime.txt
+set -ex
 
-# Then, mix in extra requirements for tests
-pip-compile --output-file=format.env.txt --resolver=backtracking deps/format.txt runtime.env.txt
-pip-compile --output-file=pylint.env.txt --resolver=backtracking deps/pylint.txt runtime.env.txt
-pip-compile --output-file=pytest.env.txt --resolver=backtracking deps/pytest.txt runtime.env.txt
+# First, compile a complete & consistent environment with all dependencies
+pip-compile --upgrade --output-file=dev.env.txt --resolver=backtracking deps/dev-tools.txt deps/runtime.txt
 
-# Everything is included in dev env
-pip-compile --output-file=dev.env.txt --resolver=backtracking deps/dev-tools.txt runtime.env.txt
+# Compile the various sub-environments.
+# For each of these, we include `constrain.txt` as an input requirements file. This file
+# has only one line: "-c ../dev.env.txt", which will use the above rendered dev.env.txt as a
+# "constraints file", which is exactly what we want.
+pip-compile --output-file=runtime.env.txt --resolver=backtracking constrain.txt deps/runtime.txt
+pip-compile --output-file=format.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/format.txt
+pip-compile --output-file=pylint.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/pylint.txt
+pip-compile --output-file=pytest.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/pytest.txt
+pip-compile --output-file=docs.env.txt    --resolver=backtracking constrain.txt deps/runtime.txt deps/docs.txt
+
