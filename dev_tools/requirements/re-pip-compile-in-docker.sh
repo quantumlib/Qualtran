07685c4940c7b351acbf6e31e27b0e86415250ee
Matthew Harrigan <mpharrigan@google.com>
2023-08-01 14:47:53 -0700

    Make docker do it

---
+++ dev_tools/requirements/re-pip-compile-in-docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+set -ex
+
+# We use docker to run the actual `pip-compile` command because its
+# behavior depends on the platform from which it is run. Please see
+# ./Dockerfile for the list of `pip-compile` commands that are run.
+docker build -t qualtran-pip-compile .
+
+# Create a container from the image so we can copy out the outputs
+id=$(docker create qualtran-pip-compile)
+
+# Copy out the files and organize them
+docker cp $id:/pip-compile/envs ./
+
+# Clean up
+docker rm -v $id
