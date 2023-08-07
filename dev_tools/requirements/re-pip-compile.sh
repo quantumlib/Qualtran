88b6287ca7971995cf27c0e2e7bb89a5637111ae
Matthew Harrigan <mpharrigan@google.com>
2023-08-07 10:36:59 -0700

    Use Docker to pin dependencies (#340)
    * Split pip-tools into its own env
    
    * Make docker do it
    
    * Re-render
    
    * Use new env location
    
    * We need ipykernel to run our notebooks
    
    * script improvements

--- dev_tools/requirements/re-pip-compile.sh
+++ dev_tools/requirements/re-pip-compile.sh
@@ -17,15 +17,16 @@
 set -ex
 
 # First, compile a complete & consistent environment with all dependencies
-pip-compile --upgrade --output-file=dev.env.txt --resolver=backtracking deps/dev-tools.txt deps/runtime.txt
+mkdir -p ./envs/
+pip-compile --output-file=envs/dev.env.txt --resolver=backtracking deps/dev-tools.txt deps/runtime.txt
 
 # Compile the various sub-environments.
 # For each of these, we include `constrain.txt` as an input requirements file. This file
-# has only one line: "-c ../dev.env.txt", which will use the above rendered dev.env.txt as a
+# has only one line: "-c envs/dev.env.txt", which will use the above rendered dev.env.txt as a
 # "constraints file", which is exactly what we want.
-pip-compile --output-file=runtime.env.txt --resolver=backtracking constrain.txt deps/runtime.txt
-pip-compile --output-file=format.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/format.txt
-pip-compile --output-file=pylint.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/pylint.txt
-pip-compile --output-file=pytest.env.txt  --resolver=backtracking constrain.txt deps/runtime.txt deps/pytest.txt
-pip-compile --output-file=docs.env.txt    --resolver=backtracking constrain.txt deps/runtime.txt deps/docs.txt
-
+pip-compile --output-file=envs/runtime.env.txt   --resolver=backtracking constrain.txt deps/runtime.txt
+pip-compile --output-file=envs/format.env.txt    --resolver=backtracking constrain.txt deps/runtime.txt deps/format.txt
+pip-compile --output-file=envs/pylint.env.txt    --resolver=backtracking constrain.txt deps/runtime.txt deps/pylint.txt
+pip-compile --output-file=envs/pytest.env.txt    --resolver=backtracking constrain.txt deps/runtime.txt deps/pytest.txt
+pip-compile --output-file=envs/docs.env.txt      --resolver=backtracking constrain.txt deps/runtime.txt deps/docs.txt
+pip-compile --output-file=envs/pip-tools.env.txt --resolver=backtracking constrain.txt deps/pip-tools.txt
