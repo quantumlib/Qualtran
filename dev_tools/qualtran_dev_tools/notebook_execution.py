2ba40efa870cf9bdf74ba714d321a2dece391d80
Matthew Harrigan <mpharrigan@google.com>
2023-08-14 14:33:25 -0700

    Notebook execution upgrades (#319)
    * Notebook execution upgrades
    
     - Only stale outputs
     - Insert cross-referencing
    
    * weird ci filesystem but I think I wanted this mode anyways
    
    * try this
    
    ---------
    
    Co-authored-by: Fionn Malone <fmalone@google.com>

--- dev_tools/qualtran_dev_tools/notebook_execution.py
+++ dev_tools/qualtran_dev_tools/notebook_execution.py
@@ -15,19 +15,21 @@
 import subprocess
 import sys
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import nbconvert
 import nbformat
+from attrs import frozen
 from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat import NotebookNode
 
 from .git_tools import get_git_root
 
 
-def get_nb_rel_paths(rootdir) -> List[Path]:
-    """List all checked-in *.ipynb files within `rootdir`."""
+def get_nb_rel_paths(sourceroot: Path) -> List[Path]:
+    """List all checked-in *.ipynb files within `sourceroot`."""
     cp = subprocess.run(
-        ['git', 'ls-files', '*.ipynb'], capture_output=True, universal_newlines=True, cwd=rootdir
+        ['git', 'ls-files', '*.ipynb'], capture_output=True, universal_newlines=True, cwd=sourceroot
     )
     outs = cp.stdout.splitlines()
     nb_rel_paths = [Path(out) for out in outs]
@@ -36,85 +38,159 @@ def get_nb_rel_paths(rootdir) -> List[Path]:
 
 
 def is_out_of_date(source_path: Path, out_path: Path) -> bool:
-    """Whether the out_path file needs to be re-rendered according to its mtime."""
+    """Whether the out_path file is stale according to its mtime."""
     assert source_path.exists()
     if not out_path.exists():
-        print(' -> Out of date: output file does not exist')
         return True
 
     source_mtime = source_path.stat().st_mtime
     output_mtime = out_path.stat().st_mtime
-    ood = source_mtime > output_mtime
-    if ood:
-        print(f' -> Out of date: {source_mtime} > {output_mtime}')
-    else:
-        print(' -> Up to date.')
-    return ood
+    return source_mtime > output_mtime
 
 
-def execute_and_export_notebook(
-    nbpath: Path, html_outpath: Optional[Path], nb_outpath: Optional[Path]
-) -> Optional[Exception]:
+@frozen
+class _NBInOutPaths:
+    """A collection of input and output paths.
+
+    If output paths are `None`, the requested file should not be
+    output.
+    """
+
+    nb_in: Path
+    html_out: Optional[Path]
+    nb_out: Optional[Path]
+
+    @classmethod
+    def from_nb_rel_path(
+        cls, nb_rel_path: Path, reporoot: Path, output_html: bool, output_nbs: bool
+    ):
+        sourceroot = reporoot / 'qualtran'
+        nbpath = sourceroot / nb_rel_path
+
+        html_rel_path = nb_rel_path.with_name(f'{nb_rel_path.stem}.html')
+        html_outpath = reporoot / 'docs/nbrun' / html_rel_path if output_html else None
+        nb_outpath = reporoot / 'docs' / nb_rel_path if output_nbs else None
+        return cls(nb_in=nbpath, html_out=html_outpath, nb_out=nb_outpath)
+
+    def html_needs_reexport(self) -> bool:
+        """Whether the html output needs to be re-exported"""
+        if self.html_out is None:
+            return False
+
+        return is_out_of_date(self.nb_in, self.html_out)
+
+    def nb_needs_reexport(self) -> bool:
+        """Whether the notebook output needs to be re-exported"""
+        if self.nb_out is None:
+            return False
+
+        return is_out_of_date(self.nb_in, self.nb_out)
+
+    def needs_reexport(self):
+        """Whether anything needs to be re-exported"""
+        return self.html_needs_reexport() or self.nb_needs_reexport()
+
+
+def _make_link_replacements() -> List[Tuple[str, str]]:
+    """Helper function to make a list of link replacements."""
+    top_level = [
+        'Bloq',
+        'CompositeBloq',
+        'BloqBuilder',
+        'Register',
+        'Signature',
+        'Side',
+        'BloqInstance',
+        'Connection',
+        'Soquet',
+    ]
+    replacements = [
+        (f'`{name}`', f'[`{name}`](/reference/qualtran/{name}.md)') for name in top_level
+    ]
+    return replacements
+
+
+_LINK_REPLACEMENTS = _make_link_replacements()
+
+
+def linkify(nb: NotebookNode):
+    """Replace certain object names with links to their reference docs in markdown cells."""
+    for i, cell in enumerate(nb.cells):
+        if cell.cell_type != 'markdown':
+            continue
+
+        for fr_text, to_text in _LINK_REPLACEMENTS:
+            cell.source = cell.source.replace(fr_text, to_text)
+
+
+def execute_and_export_notebook(paths: _NBInOutPaths) -> Optional[Exception]:
     """Execute the notebook and export it in various forms.
 
-    We execute the notebook at `nbpath` and export it as html to `html_outpath` and as
-    a filled-in notebook to `nb_outpath` if the paths are not None.
+    We execute the notebook at `nb_in` and export it as html to `html_out` and as
+    a filled-in notebook to `nb_out` if the paths are not None.
 
     During execution, we catch any exceptions raised by notebook execution
     and return it. Otherwise, we return `None`.
     """
 
     # Load it in
-    with nbpath.open() as f:
+    with paths.nb_in.open() as f:
         nb = nbformat.read(f, as_version=4)
+
+    # Put in some cross-links. The following mutates `nb`
+    linkify(nb)
 
     # Run it
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
     try:
         nb, resources = ep.preprocess(nb)
     except Exception as e:
-        print(f'{nbpath} failed!')
+        print(f'{paths.nb_in} failed!')
         print(e)
         return e
 
     # Optionally save executed notebook as notebook
-    if nb_outpath:
-        nb_outpath.parent.mkdir(parents=True, exist_ok=True)
-        with nb_outpath.open('w') as f:
+    if paths.nb_out:
+        paths.nb_out.parent.mkdir(parents=True, exist_ok=True)
+        with paths.nb_out.open('w') as f:
             nbformat.write(nb, f, version=4)
 
     # Optionally save as html
-    if html_outpath:
+    if paths.html_out:
         html, resources = nbconvert.export(nbconvert.HTMLExporter(), nb, resources=resources)
-        html_outpath.parent.mkdir(parents=True, exist_ok=True)
-        with html_outpath.open('w') as f:
+        paths.html_out.parent.mkdir(parents=True, exist_ok=True)
+        with paths.html_out.open('w') as f:
             f.write(html)
 
 
-def execute_and_export_notebooks(output_nbs: bool, output_html: bool):
+def execute_and_export_notebooks(
+    output_nbs: bool, output_html: bool, only_out_of_date: bool = True
+):
     """Find, execute, and export all checked-in ipynbs.
 
     Args:
         output_nbs: Whether to save the executed notebooks as notebooks
         output_html: Whether to save the executed notebooks as html
+        only_out_of_date: Only re-execute and re-export notebooks whose output files
+            are out of date.
     """
     reporoot = get_git_root()
     sourceroot = reporoot / 'qualtran'
-    nb_rel_paths = get_nb_rel_paths(rootdir=sourceroot)
+    nb_rel_paths = get_nb_rel_paths(sourceroot=sourceroot)
     bad_nbs = []
     for nb_rel_path in nb_rel_paths:
-        print(f"Executing {nb_rel_path}")
-        nbpath = sourceroot / nb_rel_path
-
-        html_rel_path = nb_rel_path.with_name(f'{nb_rel_path.stem}.html')
-        html_outpath = reporoot / 'docs/nbrun' / html_rel_path if output_html else None
-        nb_outpath = reporoot / 'docs' / nb_rel_path if output_nbs else None
-
-        err = execute_and_export_notebook(
-            nbpath=nbpath, html_outpath=html_outpath, nb_outpath=nb_outpath
+        paths = _NBInOutPaths.from_nb_rel_path(
+            nb_rel_path, reporoot, output_html=output_html, output_nbs=output_nbs
         )
+
+        if only_out_of_date and not paths.needs_reexport():
+            print(f'{nb_rel_path} up to date')
+            continue
+
+        print(f"Exporting {nb_rel_path}")
+        err = execute_and_export_notebook(paths)
         if err is not None:
-            bad_nbs.append(nbpath)
+            bad_nbs.append(paths.nb_in)
 
     if len(bad_nbs) > 0:
         print()
