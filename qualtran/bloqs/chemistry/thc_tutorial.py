28c9db39bfcef48dceccd7973f78ad66ba6aa8a6
Fionn Malone <fionn.malone@gmail.com>
2023-09-19 11:01:14 -0700

    Add cheaper signed state preparation.

--- qualtran/bloqs/chemistry/thc_tutorial.py
+++ qualtran/bloqs/chemistry/thc_tutorial.py
@@ -434,7 +434,7 @@ class SignedStatePreparationAliasSampling(select_and_prepare.PrepareOracle):
     In particular, we take the zero state to:
 
     $$
-    \sum_{\ell=0}^{L-1} \sqrt{p_\ell} |\ell\rangle |\mathrm{temp}_\ell\rangle
+    \sum_{\ell=0}^{L-1} \sqrt{p_\ell} |\theta \ell\rangle |\mathrm{temp}_\ell\rangle
     $$
 
     where the probabilities $p_\ell$ are $\mu$-bit binary approximations to the true values and
@@ -588,3 +588,169 @@ class SignedStatePreparationAliasSampling(select_and_prepare.PrepareOracle):
         yield swap_network.MultiTargetCSwap.make_on(
             control=less_than_equal, target_x=alt_theta, target_y=theta
         )
+
+
+@cirq.value_equality()
+@attr.frozen
+class SignedStatePreparationAliasSamplingLowerCost(select_and_prepare.PrepareOracle):
+    r"""Initialize a state with $L$ unique coefficients using coherent alias sampling.
+
+    In particular, we take the zero state to:
+
+    $$
+    \sum_{\ell=0}^{L-1} \sqrt{p_\ell} |\theta \ell\rangle |\mathrm{temp}_\ell\rangle
+    $$
+
+    where the probabilities $p_\ell$ are $\mu$-bit binary approximations to the true values and
+    where the temporary register must be treated with care, see the details in Section III.D. of
+    the reference.
+
+    The preparation is equivalent to [classical alias sampling]
+    (https://en.wikipedia.org/wiki/Alias_method): we sample `l` with probability `p[l]` by first
+    selecting `l` uniformly at random and then returning it with probability `keep[l] / 2**mu`;
+    otherwise returning `alt[l]`.
+
+    Registers:
+        selection: The input/output register $|\ell\rangle$ of size lg(L) where the desired
+            coefficient state is prepared.
+        temp: Work space comprised of sub registers:
+            - sigma: A mu-sized register containing uniform probabilities for comparison against
+                `keep`.
+            - alt: A lg(L)-sized register of alternate indices
+            - keep: a mu-sized register of probabilities of keeping the initially sampled index.
+            - one bit for the result of the comparison.
+
+    This gate corresponds to the following operations:
+     - UNIFORM_L on the selection register
+     - H^mu on the sigma register
+     - QROM addressed by the selection register into the alt and keep registers.
+     - LessThanEqualGate comparing the keep and sigma registers.
+     - Coherent swap between the selection register and alt register if the comparison
+       returns True.
+
+    Total space will be (2 * log(L) + 2 mu + 1) work qubits + log(L) ancillas for QROM.
+    The 1 ancilla in work qubits is for the `LessThanEqualGate` followed by coherent swap.
+
+    References:
+            [Encoding Electronic Spectra in Quantum Circuits with Linear T Complexity]
+        (https://arxiv.org/abs/1805.03662).
+        Babbush et. al. (2018). Section III.D. and Figure 11.
+    """
+    selection_registers: Tuple[infra.SelectionRegister, ...] = attr.field(
+        converter=lambda v: (v,) if isinstance(v, infra.SelectionRegister) else tuple(v)
+    )
+    alt: NDArray[np.int_]
+    alt_theta: NDArray[np.int_]
+    theta: NDArray[np.int_]
+    keep: NDArray[np.int_]
+    mu: int
+
+    @classmethod
+    def from_lcu_probs(
+        cls, lcu_probabilities: List[float], *, probability_epsilon: float = 1.0e-5
+    ) -> 'SignedStatePreparationAliasSamplingLowerCost':
+        """Factory to construct the state preparation gate for a given set of LCU coefficients.
+
+        Args:
+            lcu_probabilities: The LCU coefficients.
+            probability_epsilon: The desired accuracy to represent each probability
+                (which sets mu size and keep/alt integers).
+                See `cirq_ft.linalg.lcu_util.preprocess_lcu_coefficients_for_reversible_sampling`
+                for more information.
+        """
+        alt, keep, mu = linalg.preprocess_lcu_coefficients_for_reversible_sampling(
+            lcu_coefficients=np.abs(lcu_probabilities), epsilon=probability_epsilon
+        )
+        thetas = [int(t) for t in (1 - np.sign(lcu_probabilities)) // 2]
+        N = len(lcu_probabilities)
+        return SignedStatePreparationAliasSamplingLowerCost(
+            selection_registers=infra.SelectionRegister('selection', (N - 1).bit_length(), N),
+            alt=np.array(alt),
+            alt_theta=np.array(thetas)[alt],
+            theta=np.array(thetas),
+            keep=np.array(keep),
+            mu=mu,
+        )
+
+    @cached_property
+    def sigma_mu_bitsize(self) -> int:
+        return self.mu
+
+    @cached_property
+    def alternates_bitsize(self) -> int:
+        return infra.total_bits(self.selection_registers)
+
+    @cached_property
+    def keep_bitsize(self) -> int:
+        return self.mu
+
+    @cached_property
+    def selection_bitsize(self) -> int:
+        return infra.total_bits(self.selection_registers)
+
+    @cached_property
+    def junk_registers(self) -> Tuple[infra.Register, ...]:
+        return tuple(
+            infra.Registers.build(
+                sigma_mu=self.sigma_mu_bitsize,
+                alt=self.alternates_bitsize,
+                alt_theta=1,
+                theta=1,
+                keep=self.keep_bitsize,
+                less_than_equal=1,
+            )
+        )
+
+    def _value_equality_values_(self):
+        return (
+            self.selection_registers,
+            tuple(self.alt.ravel()),
+            tuple(self.alt_theta.ravel()),
+            tuple(self.theta.ravel()),
+            tuple(self.keep.ravel()),
+            self.mu,
+        )
+
+    def __repr__(self) -> str:
+        alt_repr = cirq._compat.proper_repr(self.alt)
+        keep_repr = cirq._compat.proper_repr(self.keep)
+        return (
+            f'cirq_ft.StatePreparationAliasSampling('
+            f'{self.selection_registers}, '
+            f'{alt_repr}, '
+            f'{theta}, '
+            f'{keep_repr}, '
+            f'{self.mu})'
+        )
+
+    def decompose_from_registers(
+        self,
+        *,
+        context: cirq.DecompositionContext,
+        **quregs: NDArray[cirq.Qid],  # type:ignore[type-var]
+    ) -> cirq.OP_TREE:
+        selection, less_than_equal = quregs['selection'], quregs['less_than_equal']
+        sigma_mu, alt, keep = quregs['sigma_mu'], quregs['alt'], quregs['keep']
+        alt_theta, theta = quregs['alt_theta'], quregs['theta']
+        N = self.selection_registers[0].iteration_length
+        yield prepare_uniform_superposition.PrepareUniformSuperposition(N).on(*selection)
+        yield cirq.H.on_each(*sigma_mu)
+        qrom_gate = qrom.QROM(
+            [self.theta, self.alt, self.alt_theta, self.keep],
+            (self.selection_bitsize,),
+            (1, self.alternates_bitsize, 1, self.keep_bitsize),
+        )
+        # print(self.theta)
+        # print(self.alt_theta)
+        yield qrom_gate.on_registers(
+            selection=selection, target0=theta, target1=alt, target2=alt_theta, target3=keep
+        )
+        yield arithmetic_gates.LessThanEqualGate(self.mu, self.mu).on(
+            *keep, *sigma_mu, *less_than_equal
+        )
+        yield swap_network.MultiTargetCSwap.make_on(
+            control=less_than_equal, target_x=alt, target_y=selection
+        )
+        yield cirq.ControlledGate(cirq.Z, control_values=(1,)).on(*alt_theta, *less_than_equal)
+        # Not sure why controls are off here, should be 0 from Fig 4.
+        yield cirq.ControlledGate(cirq.Z, control_values=(1,)).on(*less_than_equal, *theta)
--- qualtran/bloqs/chemistry/thc_tutorial_test.py
+++ qualtran/bloqs/chemistry/thc_tutorial_test.py
@@ -17,7 +17,10 @@ import cirq_ft
 import numpy as np
 import pytest
 
-from qualtran.bloqs.chemistry.thc_tutorial import SignedStatePreparationAliasSampling
+from qualtran.bloqs.chemistry.thc_tutorial import (
+    SignedStatePreparationAliasSampling,
+    SignedStatePreparationAliasSamplingLowerCost,
+)
 
 
 @pytest.mark.parametrize("num_states, epsilon", [[2, 3e-3], [3, 3.0e-3], [4, 5.0e-3], [7, 8.0e-3]])
@@ -34,7 +37,7 @@ def test_signed_state_preparation(num_states, epsilon):
     qubit_order = g.operation.qubits
     zs = cirq.Circuit([cirq.Moment(cirq.Z(g.quregs['theta'][0]))])
     # Add a layer of Zs to pull out the sign
-    sp_with_zs = g.circuit + zs
+    sp_with_zs = cirq.Circuit(cirq.decompose_once(g.operation)) + zs
     # assertion to ensure that simulating the `decomposed_circuit` doesn't run out of memory.
     assert len(g.circuit.all_qubits()) < 22
     result = cirq.Simulator(dtype=np.complex128).simulate(sp_with_zs, qubit_order=qubit_order)
@@ -52,4 +55,43 @@ def test_signed_state_preparation(num_states, epsilon):
     # of amplitudes) is same as `lcu_coefficients` upto `epsilon`.
     np.testing.assert_allclose(probs, abs(prepared_state) ** 2, atol=epsilon)
     state_signs = np.real(np.sign(prepared_state))
+    np.testing.assert_equal(state_signs, (-1) ** signs)
+
+
+@pytest.mark.parametrize("num_states, epsilon", [[2, 3e-3], [3, 3.0e-3], [4, 5.0e-3], [7, 8.0e-3]])
+# @pytest.mark.parametrize("num_states, epsilon", [[2, 3e-3]])
+def test_signed_state_preparation_lower_non_clifford(num_states, epsilon):
+    np.random.seed(11)
+    lcu_coefficients = np.random.randint(1, 10, num_states)
+    # np.random.seed(7)
+    signs = np.random.randint(0, 2, num_states)
+    probs = lcu_coefficients / np.sum(lcu_coefficients)
+    gate = SignedStatePreparationAliasSamplingLowerCost.from_lcu_probs(
+        lcu_probabilities=(-1) ** signs * probs, probability_epsilon=epsilon
+    )
+    g = cirq_ft.testing.GateHelper(gate)
+    qubit_order = g.operation.qubits
+    zs = cirq.Circuit([cirq.Moment(cirq.Z(g.quregs['theta'][0]))])
+    # Add a layer of Zs to pull out the sign
+    sp_with_zs = cirq.Circuit(cirq.decompose_once(g.operation)) + zs
+    # assertion to ensure that simulating the `decomposed_circuit` doesn't run out of memory.
+    assert len(g.circuit.all_qubits()) < 22
+    result = cirq.Simulator(dtype=np.complex128).simulate(sp_with_zs, qubit_order=qubit_order)
+    state_vector = result.final_state_vector
+    # State vector is of the form |l>|temp_{l}>. We trace out the |temp_{l}> part to
+    # get the coefficients corresponding to |l>.
+    L, logL = len(lcu_coefficients), len(g.quregs['selection'])
+    state_vector = state_vector.reshape(2**logL, len(state_vector) // 2**logL)
+    num_non_zero = (abs(state_vector) > 1e-6).sum(axis=1)
+    prepared_state = state_vector.sum(axis=1)
+    assert all(num_non_zero[:L] > 0) and all(num_non_zero[L:] == 0)
+    assert all(np.abs(prepared_state[:L]) > 1e-6) and all(np.abs(prepared_state[L:]) <= 1e-6)
+    prepared_state = prepared_state[:L] / np.sqrt(num_non_zero[:L])
+    # Assert that the absolute square of prepared state (probabilities instead
+    # of amplitudes) is same as `lcu_coefficients` upto `epsilon`.
+    np.testing.assert_allclose(probs, abs(prepared_state) ** 2, atol=epsilon)
+    state_signs = np.real(np.sign(prepared_state))
+    # print((-1) ** signs)
+    # print(state_signs)
+    # cirq.Circuit(cirq.decompose_once(g.operation))
     np.testing.assert_equal(state_signs, (-1) ** signs)
