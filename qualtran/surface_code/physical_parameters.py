b3bfb88844bd7b78e03a821787214e57d5eafa70
Noureldin <noureldinyosri@google.com>
2023-10-02 11:11:11 -0700

    Add AlgorithmSummary and PhysicalParameters dataclasses (#357)
    child PR from #356

---
+++ qualtran/surface_code/physical_parameters.py
@@ -0,0 +1,42 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from attrs import field, frozen
+
+
+@frozen
+class PhysicalParameters:
+    """The physical properties of a quantum computer.
+
+    Attributes:
+        t_gate_ns: Clifford gate physical time.
+        t_meas_ns: Measurement physical time.
+        physical_error_rate: Physical error rate.
+        reference: Source of these estimates.
+    """
+
+    t_gate_ns: float = field(repr=lambda x: f'{x:g}')
+    t_meas_ns: float = field(repr=lambda x: f'{x:g}')
+
+    physical_error_rate: float = field(default=1e-3, repr=lambda x: f'{x:g}')
+
+    reference: str | None = None
+
+
+BeverlandEtAl = PhysicalParameters(
+    t_gate_ns=50,  # 50ns
+    t_meas_ns=100,  # 100ns
+    physical_error_rate=1e-4,
+    reference='https://arxiv.org/abs/2211.07629',
+)
