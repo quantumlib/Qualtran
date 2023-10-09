aef1520eb055b51ba3aae169bb4235b145b31eef
Fionn Malone <fmalone@google.com>
2023-10-09 21:59:47 +0000

    Fix unit test.

--- qualtran/bloqs/chemistry/thc_select_test.py
+++ qualtran/bloqs/chemistry/thc_select_test.py
@@ -27,7 +27,7 @@ def _make_select():
 def test_select_thc():
     num_mu = 10
     num_spin_orb = 2 * 4
-    angles = ((0.5,) * num_spin_orb // 2,) * num_mu
+    angles = ((0.5,) * (num_spin_orb // 2),) * num_mu
     select = SelectTHC(
         num_mu=num_mu, num_spin_orb=num_spin_orb, rotation_angles=angles, num_bits_theta=12
     )
