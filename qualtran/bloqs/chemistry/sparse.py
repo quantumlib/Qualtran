ff9586cff71ce8a19262ddf6216d5cfd8fde737a
Fionn Malone <fmalone@google.com>
2023-10-14 00:30:09 +0000

    Add sparse costs.

--- qualtran/bloqs/chemistry/sparse.py
+++ qualtran/bloqs/chemistry/sparse.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""SELECT and PREPARE for the sparse chemistry Hamtilonian in second quantization."""
 
 from functools import cached_property
 from typing import Optional, Set, Tuple, TYPE_CHECKING
