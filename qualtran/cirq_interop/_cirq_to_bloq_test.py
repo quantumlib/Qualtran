8f3cc109260aaed8d17eeb443681988807659baa
Tanuj Khattar <tanujkhattar@google.com>
2023-10-17 15:42:50 -0700

    CirqGateAsBloq should raise DecomposeNotImplementedError if underlying gate does not have a decomposition

--- qualtran/cirq_interop/_cirq_to_bloq_test.py
+++ qualtran/cirq_interop/_cirq_to_bloq_test.py
@@ -22,7 +22,15 @@ import sympy
 from attrs import frozen
 
 import qualtran
-from qualtran import Bloq, BloqBuilder, CompositeBloq, DecomposeTypeError, Side, Signature
+from qualtran import (
+    Bloq,
+    BloqBuilder,
+    CompositeBloq,
+    DecomposeNotImplementedError,
+    DecomposeTypeError,
+    Side,
+    Signature,
+)
 from qualtran.bloqs.basic_gates import OneState
 from qualtran.bloqs.util_bloqs import Allocate, Free, Join, Split
 from qualtran.cirq_interop import (
@@ -221,3 +229,9 @@ def test_cirq_gate_as_bloq_for_left_only_gates():
     assert bloqs_list.count(Free(1)) == 2
     assert bloqs_list.count(CirqGateAsBloq(cirq.CNOT)) == 1
     assert bloqs_list.count(CirqGateAsBloq(cirq.ResetChannel())) == 2
+
+
+def test_cirq_gate_as_bloq_decompose_raises():
+    bloq = CirqGateAsBloq(cirq.X)
+    with pytest.raises(DecomposeNotImplementedError, match="does not have a decomposition"):
+        _ = bloq.decompose_bloq()
