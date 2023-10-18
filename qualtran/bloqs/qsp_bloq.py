8a46ea026787b37a92b06e6667db94a22865576c
Anurudh Peduri <anurudhp97@gmail.com>
2023-10-18 23:14:54 +0200

    update notebook, some more documentation

--- qualtran/bloqs/qsp_bloq.py
+++ qualtran/bloqs/qsp_bloq.py
@@ -1,11 +1,26 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from functools import cached_property
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Sequence
 
 import cirq
 import numpy as np
 from attrs import frozen
-from cirq_ft.infra import GateWithRegisters, Register, Signature
 from numpy.typing import NDArray
+
+from cirq_ft import GateWithRegisters, Register, Signature
 
 
 def _arbitrary_SU2_rotation(theta: float, phi: float, lambd: float):
@@ -33,7 +48,7 @@ def _arbitrary_SU2_rotation(theta: float, phi: float, lambd: float):
     )
 
 
-def qsp_phase_factors(P: np.ndarray, Q: np.ndarray) -> Dict[str, Any]:
+def qsp_phase_factors(P: Sequence[float], Q: Sequence[float]) -> Dict[str, Any]:
     """Computes the QSP signal rotations for a given pair of polynomials.
     The QSP transformation is described in Theorem 3, and the algorithm for computing co-efficients is described in Algorithm 1.
 
@@ -45,10 +60,8 @@ def qsp_phase_factors(P: np.ndarray, Q: np.ndarray) -> Dict[str, Any]:
         [Generalized Quantum Signal Processing](https://arxiv.org/abs/2308.01501)
             Motlagh and Wiebe. (2023). Theorem 3; Algorithm 1.
     """
-    P = np.array(P)
-    Q = np.array(Q)
-    if P.ndim != 1 or Q.ndim != 1 or P.shape != Q.shape:
-        raise ValueError("Polynomials P and Q must be arrays of the same length.")
+    if len(P) != len(Q):
+        raise ValueError("Polynomials P and Q must have the same degree.")
 
     S = np.array([P, Q])
     n = S.shape[1]
@@ -75,8 +88,14 @@ def qsp_phase_factors(P: np.ndarray, Q: np.ndarray) -> Dict[str, Any]:
 
 @frozen
 class QEVTCircuit(GateWithRegisters):
-    """Applies a QSP sequence described by a pair of polynomials $P, Q$, to a unitary $U$ to obtain a block-encoding of $P(U)$.
+    r"""Applies a QSP sequence described by a pair of polynomials $P, Q$, to a unitary $U$ to obtain a block-encoding of $P(U)$.
     The exact circuit is described in Figure 2.
+
+    When $U$ encodes a Hermitian matrix $H$ as $U = e^{iH}$, then this bloq encodes the following unitary:
+        $\begin{matrix} P(U) & \cdot \\ Q(U) & \cdot \end{matrix}$.
+
+    The polynomials $P, Q$ must satisfy:
+        $\abs{P(x)}^2 + \abs{Q(x)}^2 = 1$ for every $x \in \mathbb{C}$ such that $\abs{x} = 1$
 
     Args:
         U: Unitary operation (encoding some Hermitian matrix $H$ as $e^{iH}$).
@@ -89,8 +108,8 @@ class QEVTCircuit(GateWithRegisters):
     """
 
     U: GateWithRegisters
-    P: Tuple[float]
-    Q: Tuple[float]
+    P: Sequence[float]
+    Q: Sequence[float]
 
     @property
     def signature(self) -> Signature:
@@ -112,7 +131,9 @@ class QEVTCircuit(GateWithRegisters):
     def _lambda(self):
         return self._qsp_phases['lambda']
 
-    def _signal_gate(self, signal_qubit, theta, phi, lambd) -> cirq.OP_TREE:
+    def _signal_gate(
+        self, signal_qubit: cirq.Qid, theta: float, phi: float, lambd: float
+    ) -> cirq.OP_TREE:
         gates = cirq.single_qubit_matrix_to_gates(_arbitrary_SU2_rotation(theta, phi, lambd))
         for gate in gates:
             yield gate.on(signal_qubit)
--- qualtran/bloqs/qsp_bloq_test.py
+++ qualtran/bloqs/qsp_bloq_test.py
@@ -1,21 +1,24 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import numpy as np
-from cirq_ft.algos.qubitization_walk_operator_test import get_walk_operator_for_1d_Ising_model
 
 from qualtran.bloqs.qsp_bloq import QEVTCircuit, qsp_phase_factors
-from qualtran.cirq_interop._cirq_to_bloq import CirqGateAsBloq
 
 
 def test_compute_qsp_phase_factors():
     phases = qsp_phase_factors([0.5, 0.5], [0.5, -0.5])
-    theta = phases['theta']
-    phi = phases['phi']
-    lambd = phases['lambda']
-    assert (theta == [0, np.pi / 4]).all()
-    assert (phi == [0, np.pi]).all()
-    assert lambd == 0
-
-
-def test_QEVT_circuit():
-    U = get_walk_operator_for_1d_Ising_model(4, 2e-1)
-    pU = QEVTCircuit(U, (0.5, 0.5), (0.5, -0.5))
-    bloq = CirqGateAsBloq(pU)
+    assert (phases['theta'] == [0, np.pi / 4]).all()
+    assert (phases['phi'] == [0, np.pi]).all()
+    assert phases['lambda'] == 0
