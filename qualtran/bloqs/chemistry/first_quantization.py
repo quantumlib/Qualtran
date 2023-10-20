a7ee86fea0c3e2c7922407f6ba525d52ea044cb5
Fionn Malone <fmalone@google.com>
2023-10-20 20:00:45 +0000

    Updates for first quantization.

--- qualtran/bloqs/chemistry/first_quantization.py
+++ qualtran/bloqs/chemistry/first_quantization.py
@@ -15,10 +15,12 @@
 from functools import cached_property
 from typing import Optional, Set, Tuple, TYPE_CHECKING
 
+import numpy as np
 from attrs import frozen
 
 from qualtran import Bloq, Register, Signature
-from qualtran.bloqs.basic_gates import TGate
+from qualtran.bloqs.arithmetic import GreaterThan, Product, SumOfSquares
+from qualtran.bloqs.basic_gates import TGate, Toffoli
 
 if TYPE_CHECKING:
     from qualtran.resource_counting import SympySymbolAllocator
@@ -129,7 +131,8 @@ class PrepareTFirstQuantization(Bloq):
     def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
         # there is a cost for the uniform state preparation for the $w$
         # register. Adding a bloq is sort of overkill, should just tag the
-        # correct cost on cirq_ft bloq, 13 is from assuming 8 bits for the rotation, and n = 2.
+        # correct cost on UniformSuperPosition bloq
+        # 13 is from assuming 8 bits for the rotation, and n = 2.
         uni_prep_w = (4 * 13, TGate())
         # Factor of two for r and s registers.
         return {uni_prep_w, (2, PreparePowerTwoState(bitsize=self.num_bits_p))}
@@ -175,7 +178,7 @@ class SelectTFirstQuantization(Bloq):
 
 
 @frozen
-class PrepareUnaryEncodedOneHot(Bloq):
+class PrepareMuUnaryEncodedOneHot(Bloq):
     r"""Prepare a unary encoded one-hot superposition state.
 
     Prepares the state in Eq. 77
@@ -184,6 +187,7 @@ class PrepareUnaryEncodedOneHot(Bloq):
         \frac{1}{\sqrt{2^{n_p + 2}}} \sum_{\mu=2}^{n_p+1} \sqrt{2^\mu}
         |0\dots0\underbrace{1\dots1}{\mu}\rangle
     $$
+
     Args:
         num_bits_p: The number of bits to represent each dimension of the momentum register.
 
@@ -201,6 +205,7 @@ class PrepareUnaryEncodedOneHot(Bloq):
         return Signature.build(mu=self.num_bits_p + 1)
 
     def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        # controlled hadamards which cannot be inverted at zero Toffoli cost.
         return {(4 * (self.num_bits_p - 1), TGate())}
 
 
@@ -237,6 +242,7 @@ class PrepareNuSuperPositionState(Bloq):
         )
 
     def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        # controlled hadamards which cannot be inverted at zero Toffoli cost.
         return {(4 * (3 * (self.num_bits_p - 1)), TGate())}
 
 
@@ -256,7 +262,7 @@ class FlagZeroAsFailure(Bloq):
         (https://arxiv.org/abs/2105.12767) page 21, Eq 80.
     """
     num_bits_p: int
-    adjoint: False
+    adjoint: bool = False
 
     @cached_property
     def signature(self) -> Signature:
@@ -269,11 +275,109 @@ class FlagZeroAsFailure(Bloq):
             # This can be inverted with cliffords.
             return {(0, TGate())}
         else:
-            return {(4 * 3 * self.num_bits_p, TGate())}
+            # Controlled Toffoli each having n_p + 1 controls and 2 Toffolis to
+            # check the result of the Toffolis.
+            return {(4 * (3 * self.num_bits_p + 2), TGate())}
+
 
 @frozen
-class PrepareUVFistQuantization(Bloq):
-    r"""PREPARE for the kinetic energy operator for the first quantized chemistry Hamiltonian.
+class TestNuLessThanMu(Bloq):
+    r"""Bloq to flag if all components of $\nu$ are smaller in absolute value than $2^{\mu-2}$.
+
+    Args:
+        num_bits_p: The number of bits to represent each dimension of the momentum register.
+
+    Registers:
+        nu: the momentum transfer register.
+        flag_nu_lt_mu: a flag bit for failure of the state preparation.
+
+    References:
+        [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
+        (https://arxiv.org/abs/2105.12767) page 21, Eq 80.
+    """
+    num_bits_p: int
+    adjoint: bool = False
+
+    @cached_property
+    def signature(self) -> Signature:
+        return Signature(
+            [Register("mu", self.num_bits_p + 1), Register("nu", self.num_bits_p + 1, shape=(3,))]
+        )
+
+    def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        if self.adjoint:
+            # This can be inverted with cliffords.
+            return {(0, TGate())}
+        else:
+            # n_p controlled Toffolis with four controls.
+            return {(3 * self.num_bits_p, Toffoli())}
+
+
+@frozen
+class TestNuInequality(Bloq):
+    r"""Bloq to flag if all components of $\nu$ are smaller in absolute value than $2^{\mu-2}$.
+
+    Test
+
+    $$
+        (2^{\mu-2})^2 \mathcal{M} > m (\nu_x^2 + \nu_y^2 + \nu_z^2)
+    $$
+
+    where $m \in [0, \mathcal{M}-1]$ and is store in an ancilla register.
+
+    Args:
+        num_bits_p: The number of bits to represent each dimension of the momentum register.
+        num_bits_m: The number of bits for $\mathcal{M}$. Eq 86.
+        adjoint: Whether to dagger the bloq or not.
+
+    Registers:
+        mu: the one-hot unary superposition register.
+        nu: the momentum transfer register.
+        m: the ancilla register in unfiform superposition.
+        flag_minus_zero: A flag from checking for negative zero.
+        flag_nu_lt_mu: A flag from checking $\nu \lt 2^{\mu -2}$.
+        flag_ineq: A flag qubit from the inequality test.
+        succ: a flag bit for failure of the state preparation.
+
+    References:
+        [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
+        (https://arxiv.org/abs/2105.12767) page 21, Eq 80.
+    """
+    num_bits_p: int
+    num_bits_m: int
+    adjoint: bool = False
+
+    @cached_property
+    def signature(self) -> Signature:
+        return Signature(
+            [
+                Register("mu", self.num_bits_p + 1),
+                Register("nu", self.num_bits_p + 1, shape=(3,)),
+                Register("m", self.num_bits_m, shape=(3,)),
+                Register("flag_minus_zero", 1),
+                Register("flag_nu_lt_mu", 1),
+                Register("flag_ineq", 1),
+                Register("succ", 1),
+            ]
+        )
+
+    def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        if self.adjoint:
+            return {(0, TGate())}
+        else:
+            # 1. Compute $\nu_x^2 + \nu_y^2 + \nu_z^2$
+            cost_1 = (1, SumOfSquares(self.num_bits_p, k=3))
+            # 2. Compute $m (\nu_x^2 + \nu_y^2 + \nu_z^2)$
+            cost_2 = (1, Product(self.num_bits_p, self.num_bits_m))
+            # 3. Inequality test
+            cost_3 = (1, GreaterThan(2 * self.num_bits_p + self.num_bits_m + 2))
+            # 4. 3 Toffoli for overall success
+            cost_4 = (3, Toffoli())
+
+
+@frozen
+class PrepareNuState(Bloq):
+    r"""PREPARE for the $\nu$ state for the $U$ and $V$ components of the Hamiltonian.
 
     Prepares a state of the form
 
@@ -292,6 +396,10 @@ class PrepareUVFistQuantization(Bloq):
         er_lambda_zeta: eq 91 of the referce. Cost of erasing qrom.
 
     Registers:
+        mu: The state controlling the nested boxes procedure.
+        nu: The momentum transfer register.
+        m: an ancilla register in a uniform superposition.
+        flag_nu: Flag for success of the state preparation.
 
     References:
         [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
@@ -299,6 +407,106 @@ class PrepareUVFistQuantization(Bloq):
     """
     num_bits_p: int
     eta: int
+    m_param: int
+    adjoint: bool = False
+
+    @cached_property
+    def signature(self) -> Signature:
+        # this is for the nu register which lives on a grid of twice the size
+        # the nu grid is twice as large, so one more bit is needed
+        n_nu = (self.num_bits_p - 1).bit_length() + 2
+        n_mu = self.num_bits_p.bit_length()  # is this correct?
+        n_m = (self.m_param - 1).bit_length()
+        return Signature(
+            [
+                Register("mu", bitsize=n_mu),
+                Register("nu", bitsize=n_nu, shape=(3,)),
+                Register("m", bitsize=n_m),
+                Register("l", bitsize=self.num_bits_nuc_pos),
+                Register("flag_nu", bitsize=1),
+            ]
+        )
+
+    def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        # 1. Prepare unary encoded superposition state (Eq 77)
+        cost_1 = (1, PrepareMuUnaryEncodedOneHot(self.num_bits_p))
+        n_m = (self.m_param - 1).bit_length()
+        # 2. Prepare mu-nu superposition (Eq 78)
+        cost_2 = (1, PrepareNuSuperPositionState(self.num_bits_p))
+        # 3. Remove minus zero
+        cost_3 = (1, FlagZeroAsFailure(self.num_bits_p, adjoint=self.adjoint))
+        # 4. Test $\nu < 2^{\mu-2}$
+        cost_4 = (1, TestNuLessThanMu(self.num_bits_p, adjoint=self.adjoint))
+        # 5. Prepare superposition over $m$ which is a power of two so only clifford.
+        # 6. Test that $(2^{\mu-2})^2\mathcal{M} > m (\nu_x^2 + \nu_y^2 + \nu_z^2)$
+        cost_6 = (1, TestNuInequality(self.num_bits_p, n_m, adjoint=self.adjoint))
+        return {cost_1, cost_2, cost_3, cost_4, cost_6}
+
+
+@frozen
+class PrepareZetaState(Bloq):
+    r"""PREPARE the superpostion over $l$ weighted by $\zeta_l$.
+
+    This is apparently NOT just generic state preparation and there are some
+    tricks I don't understand.
+
+    Args:
+        num_bits_p: The number of bits to represent each dimension of the momentum register.
+        eta: The number of electrons.
+        m_param: $\mathcal{M}$ in the reference.
+        lambda_zeta: sum of nuclear charges.
+        er_lambda_zeta: eq 91 of the referce. Cost of erasing qrom.
+
+    Registers:
+
+    References:
+        [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
+        (https://arxiv.org/abs/2105.12767) page 23-24, last 3 paragraphs.
+    """
+    num_atoms: int
+    lambda_zeta: int
+    num_bits_nuc_pos: int
+    adjoint: bool = False
+
+    @cached_property
+    def signature(self) -> Signature:
+        return Signature([Register("l", bitsize=(self.num_atoms - 1).bitsize())])
+
+    def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
+        if self.adjoint:
+            # Really Er(x), eq 91. In practice we will reaplce this with the
+            # appropriate qrom call down the line.
+            return np.int(np.ceil(self.lambda_zeta**0.5))
+        else:
+            return self.lambda_zeta
+
+
+@frozen
+class PepareUVFirstQuantization(Bloq):
+    r"""PREPARE the U and V parts of the Hamiltonian.
+
+    Args:
+        num_bits_p: The number of bits to represent each dimension of the momentum register.
+        eta: The number of electrons.
+        num_atoms: The number of atoms. $L$ in the reference.
+        m_param: $\mathcal{M}$ in the reference.
+        lambda_zeta: sum of nuclear charges.
+        num_bits_nuc_pos: The number of bits of precision for representing the nuclear coordinates.
+
+    Registers:
+        mu: The state controlling the nested boxes procedure.
+        nu: The momentum transfer register.
+        m: an ancilla register in a uniform superposition.
+        l: The register for atomic species.
+        flag_nu: Flag for success of the state preparation.
+
+    References:
+        [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
+        (https://arxiv.org/abs/2105.12767) page 19, section B
+    """
+    num_bits_p: int
+    eta: int
+    num_atoms: int
     m_param: int
     lambda_zeta: int
     num_bits_nuc_pos: int
@@ -323,16 +531,17 @@ class PrepareUVFistQuantization(Bloq):
         )
 
     def bloq_counts(self, ssa: Optional['SympySymbolAllocator'] = None) -> Set[Tuple[int, Bloq]]:
-        # 1. Prepare unary encoded superposition state
-        n_m = (self.m_param - 1).bit_length()
-        if self.adjoint:
-            cost = 14 * self.num_bits_p - 8  # eq 90 - 89 page 23
-            cost += cost_qrom_erasure(self.lambda_zeta)
-        else:
-            cost = (
-                3 * self.num_bits_p**2 + self.num_bits_p + 1 + 4 * n_m * (self.num_bits_p + 1)
-            )  # eq 89, page 23
-            cost += self.lambda_zeta  # Eq 92.
+        # 1. Prepare the nu state
+        # 2. Prepare the zeta_l state
+        return {
+            (1, PrepareNuState(self.num_bits_p, self.eta, self.m_param, self.adjoint)),
+            (
+                1,
+                PrepareZetaState(
+                    self.num_atoms, self.lambda_zeta, self.num_bits_nuc_pos, self.adjoint
+                ),
+            ),
+        }
 
 
 @frozen
@@ -349,7 +558,7 @@ class SelectUVFirstQuantization(Bloq):
 
     References:
         [Fault-Tolerant Quantum Simulations of Chemistry in First Quantization]
-        (https://arxiv.org/abs/2105.12767) page 19, section B
+        (https://arxiv.org/abs/2105.12767)
     """
 
     num_bits_p: int
