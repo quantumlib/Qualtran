e08e6a052e4235e622d087469861233db0a70251
Fionn Malone <fmalone@google.com>
2023-10-26 20:25:29 +0000

    Add test.

---
+++ qualtran/bloqs/chemistry/pbc/first_quantization/select_t_test.py
@@ -0,0 +1,23 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from qualtran.bloqs.basic_gates import TGate
+from qualtran.bloqs.chemistry.pbc.first_quantization.select_t import SelectTFirstQuantization
+from qualtran.resource_counting import get_bloq_counts_graph
+
+
+def test_select_t_bloq_counts():
+    num_bits_p = 6
+    sel = SelectTFirstQuantization(num_bits_p, 10)
+    _, counts = get_bloq_counts_graph(sel)
+    assert counts[TGate()] == 4 * 5 * (num_bits_p - 1) + 4 * 2
