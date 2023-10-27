d1043f3a1f1b3499072241056ab0ee11657b78c6
Matthew Harrigan <mpharrigan@google.com>
2023-10-26 17:17:57 -0700

    Can't assert equivalent if there aren't two things

--- qualtran/cirq_interop/testing.py
+++ qualtran/cirq_interop/testing.py
@@ -129,9 +129,9 @@ def assert_decompose_is_consistent_with_t_complexity(val: Any):
     t_complexity_method = getattr(val, '_t_complexity_', None)
     expected = NotImplemented if t_complexity_method is None else t_complexity_method()
     if expected is NotImplemented or expected is None:
-        return
+        raise AssertionError("No consistent t_complexity: no _t_complexity_.")
     decomposition = _decompose_once_considering_known_decomposition(val)
     if decomposition is None:
-        return
+        raise AssertionError("No consistent t_complexity: no decomposition.")
     from_decomposition = t_complexity(decomposition, fail_quietly=False)
     assert expected == from_decomposition, f'{expected} != {from_decomposition}'
