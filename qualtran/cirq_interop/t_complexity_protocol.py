527c02df1c34f1b008cfc008c0c657e2d02f4dc5
Matthew Harrigan <mpharrigan@google.com>
2023-11-17 13:43:49 -0800

    Format imports after removing typing_extensions

--- qualtran/cirq_interop/t_complexity_protocol.py
+++ qualtran/cirq_interop/t_complexity_protocol.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Any, Callable, Hashable, Iterable, Optional, overload, Union, Literal, Protocol
+from typing import Any, Callable, Hashable, Iterable, Literal, Optional, overload, Protocol, Union
 
 import attrs
 import cachetools
