ac1ee47fbbf83c067e91c13cb4eeb9e9b05d9652
Nour Yosri <noureldinyosri@gmail.com>
2023-11-30 15:07:39 -0800

    remove formulae.py

--- qualtran/surface_code/quantum_error_correction_scheme_summary_test.py
+++ qualtran/surface_code/quantum_error_correction_scheme_summary_test.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import numpy as np
 import pytest
 
 from qualtran.surface_code import quantum_error_correction_scheme_summary as qecs
@@ -30,3 +31,26 @@ def test_physical_qubits(qec: qecs.QuantumErrorCorrectionSchemeSummary, want: in
 @pytest.mark.parametrize('qec,want', [[qecs.BeverlandSuperConductingQubits, 4.8]])
 def test_error_detection_cycle_time(qec: qecs.QuantumErrorCorrectionSchemeSummary, want: float):
     assert qec.error_detection_circuit_time_us(12) == pytest.approx(want)
+
+
+def test_invert_error_at():
+    phys_err = 1e-3
+    budgets = np.logspace(-1, -18)
+    for budget in budgets:
+        d = qecs.FowlerSuperConductingQubits.code_distance_from_budget(
+            physical_error_rate=phys_err, budget=budget
+        )
+        assert d % 2 == 1
+        assert (
+            qecs.FowlerSuperConductingQubits.logical_error_rate(
+                physical_error_rate=phys_err, code_distance=d
+            )
+            <= budget
+        )
+        if d > 3:
+            assert (
+                qecs.FowlerSuperConductingQubits.logical_error_rate(
+                    physical_error_rate=phys_err, code_distance=d - 2
+                )
+                > budget
+            )
