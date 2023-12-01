b8f560a08f1098cf91cf82c4c5fa5268534530c5
Matthew Harrigan <mpharrigan@google.com>
2023-12-01 10:54:43 -0800

    grid search best params

--- qualtran/surface_code/ccz2t_cost_model_test.py
+++ qualtran/surface_code/ccz2t_cost_model_test.py
@@ -15,11 +15,15 @@
 import numpy as np
 
 from qualtran.surface_code.algorithm_summary import AlgorithmSummary
-from qualtran.surface_code.ccz2t_cost_model import get_ccz2t_costs
+from qualtran.surface_code.ccz2t_cost_model import (
+    get_ccz2t_costs,
+    get_ccz2t_costs_from_error_budget,
+    get_ccz2t_costs_from_grid_search,
+)
 
 
 def test_vs_spreadsheet():
-    re = get_ccz2t_costs(
+    re = get_ccz2t_costs_from_error_budget(
         n_magic=AlgorithmSummary(t_gates=10**8, toffoli_gates=10**8),
         n_algo_qubits=100,
         error_budget=0.01,
@@ -30,3 +34,16 @@ def test_vs_spreadsheet():
     np.testing.assert_allclose(re.failure_prob, 0.0084, rtol=1e-3)
     np.testing.assert_allclose(re.footprint, 4.00e5, rtol=1e-3)
     np.testing.assert_allclose(re.duration_hr, 7.53, rtol=1e-3)
+
+
+def test_grid_search_runs():
+    # TODO: a better test vs some published reference
+    cost, factory, db = get_ccz2t_costs_from_grid_search(
+        n_magic=AlgorithmSummary(t_gates=10**8, toffoli_gates=10**8),
+        n_algo_qubits=100,
+        phys_err=1e-3,
+        cycle_time_us=1,
+    )
+    assert factory.distillation_l1_d == 15
+    assert factory.distillation_l2_d == 23
+    assert db.data_d == 25
