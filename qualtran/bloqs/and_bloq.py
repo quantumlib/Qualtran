8ba16eb73b204b01915b3549282bd055b6140e6a
Matthew Harrigan <mpharrigan@google.com>
2023-12-07 15:01:33 -0800

    Support Adjoint And Classical Sim

--- qualtran/bloqs/and_bloq.py
+++ qualtran/bloqs/and_bloq.py
@@ -24,7 +24,7 @@ to the and of its control registers. `And` will output the result into a fresh r
 
 import itertools
 from functools import cached_property
-from typing import Any, Dict, Set, Tuple
+from typing import Any, Dict, Optional, Set, Tuple
 
 import cirq
 import numpy as np
@@ -95,12 +95,16 @@ class And(GateWithRegisters):
         dag = '†' if self.adjoint else ''
         return f'And{dag}'
 
-    def on_classical_vals(self, ctrl: NDArray[np.uint8]) -> Dict[str, NDArray[np.uint8]]:
-        if self.adjoint:
-            raise NotImplementedError("Come back later.")
+    def on_classical_vals(
+        self, *, ctrl: NDArray[np.uint8], target: Optional[int] = None
+    ) -> Dict[str, NDArray[np.uint8]]:
+        out = 1 if tuple(ctrl) == (self.cv1, self.cv2) else 0
+        if not self.adjoint:
+            return {'ctrl': ctrl, 'target': out}
 
-        target = 1 if tuple(ctrl) == (self.cv1, self.cv2) else 0
-        return {'ctrl': ctrl, 'target': target}
+        # Adjoint
+        assert target == out
+        return {'ctrl': ctrl}
 
     def add_my_tensors(
         self,
