066fddb1849145fbdb60ae4719ac7bbc07837129
Matthew Harrigan <mpharrigan@google.com>
2023-12-08 10:38:52 -0800

    Adjoint protocol (#565)
    * Adjoint protocol
    
    * get block encoding working
    
    * Address review 1
    
    * Pylint

--- qualtran/_infra/bloq.py
+++ qualtran/_infra/bloq.py
@@ -157,6 +157,20 @@ class Bloq(metaclass=abc.ABCMeta):
         bb, initial_soqs = BloqBuilder.from_signature(self.signature, add_registers_allowed=False)
         return bb.finalize(**bb.add_d(self, **initial_soqs))
 
+    def adjoint(self) -> 'Bloq':
+        """The adjoint of this bloq.
+
+        Bloq authors can override this method in certain circumstances. Otherwise, the default
+        fallback wraps this bloq in `Adjoint`.
+
+        Please see the documentation for `Adjoint` and the `Adjoint.ipynb` notebook for full
+        details.
+        """
+
+        from qualtran import Adjoint
+
+        return Adjoint(self)
+
     def on_classical_vals(self, **vals: 'ClassicalValT') -> Dict[str, 'ClassicalValT']:
         """How this bloq operates on classical data.
 
