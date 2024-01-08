7c9f3a0af0705e8bf72e0297c7cd2ce2c9b94628
Tanuj Khattar <tanujkhattar@google.com>
2024-01-09 03:17:08 +0530

    Add `HammingWeightPhasingViaPhaseGradient` Bloq (#509)
    * Add HammingWeightPhasingViaPhaseGradient Bloq
    
    * Address nits and comments
    
    * Fix docstring
    
    * Fix docstrings and add more tests

--- qualtran/bloqs/rotations/phase_gradient_test.py
+++ qualtran/bloqs/rotations/phase_gradient_test.py
@@ -15,7 +15,13 @@ import cirq
 import numpy as np
 import pytest
 
-from qualtran.bloqs.rotations.phase_gradient import PhaseGradientState, PhaseGradientUnitary
+from qualtran.bloqs.rotations.phase_gradient import (
+    AddIntoPhaseGrad,
+    AddScaledValIntoPhaseReg,
+    PhaseGradientState,
+    PhaseGradientUnitary,
+)
+from qualtran.cirq_interop.bit_tools import float_as_fixed_width_int
 from qualtran.testing import assert_valid_bloq_decomposition
 
 
@@ -48,3 +54,45 @@ def test_phase_gradient_gate(n: int, exponent, controlled):
     if controlled:
         cirq_gate = cirq_gate.controlled()
     assert np.allclose(cirq.unitary(bloq), cirq.unitary(cirq_gate))
+
+
+def test_add_into_phase_grad():
+    x_bit, phase_bit = 4, 7
+    bloq = AddIntoPhaseGrad(x_bit, phase_bit)
+    basis_map = {}
+    for x in range(2**x_bit):
+        for phase_grad in range(2**phase_bit):
+            phase_grad_out = (phase_grad + x) % 2**phase_bit
+            # Test Bloq style classical simulation.
+            assert bloq.call_classically(x=x, phase_grad=phase_grad) == (x, phase_grad_out)
+            # Prepare basis states mapping for cirq-style simulation.
+            input_state = int(f'{x:0{x_bit}b}' + f'{phase_grad:0{phase_bit}b}', 2)
+            output_state = int(f'{x:0{x_bit}b}' + f'{phase_grad_out:0{phase_bit}b}', 2)
+            basis_map[input_state] = output_state
+    # Test cirq style simulation.
+    num_bits = x_bit + phase_bit
+    assert len(basis_map) == len(set(basis_map.values()))
+    circuit = cirq.Circuit(bloq.on(*cirq.LineQubit.range(num_bits)))
+    cirq.testing.assert_equivalent_computational_basis_map(basis_map, circuit)
+
+
+def test_add_scaled_val_into_phase_reg():
+    x_bit, phase_bit, gamma, gamma_bit = 4, 7, 0.123, 6
+    bloq = AddScaledValIntoPhaseReg(x_bit, phase_bit, gamma, gamma_bit)
+    gamma_fixed_width_float = float_as_fixed_width_int(gamma, gamma_bit + 1)[1] / (2**gamma_bit)
+    gamma_int = float_as_fixed_width_int(gamma_fixed_width_float, phase_bit + 1)[1]
+    basis_map = {}
+    for x in range(2**x_bit):
+        for phase_grad in range(2**phase_bit):
+            phase_grad_out = (phase_grad + x * gamma_int) % 2**phase_bit
+            # Test Bloq style classical simulation.
+            assert bloq.call_classically(x=x, phase_grad=phase_grad) == (x, phase_grad_out)
+            # Prepare basis states mapping for cirq-style simulation.
+            input_state = int(f'{x:0{x_bit}b}' + f'{phase_grad:0{phase_bit}b}', 2)
+            output_state = int(f'{x:0{x_bit}b}' + f'{phase_grad_out:0{phase_bit}b}', 2)
+            basis_map[input_state] = output_state
+    # Test cirq style simulation.
+    num_bits = x_bit + phase_bit
+    assert len(basis_map) == len(set(basis_map.values()))
+    circuit = cirq.Circuit(bloq.on(*cirq.LineQubit.range(num_bits)))
+    cirq.testing.assert_equivalent_computational_basis_map(basis_map, circuit)
