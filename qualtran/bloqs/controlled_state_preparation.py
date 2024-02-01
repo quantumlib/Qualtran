Fixed bug in angle offset calculation

qualtran/bloqs/controlled_state_preparation.py
@@ -123,7 +123,7 @@ class ControlledRotStatePreparation(Bloq):
         """
         offset_angles = [0]*(2**self.n_qubits)
         for i in range(self.n_qubits):
-            for j in range(i+1):
+            for j in range(2**i):
                 item_range = 2**(self.n_qubits-i)
                 offset = np.pi*(1-amplitude_rom_vals[i][j]/(2**self.rot_reg_size))
                 for k in range(item_range*j, item_range*(j+1)):