5c1c6c19a22dd32b4931c9c7e55388a551b44864
Víctor Carballo <vicaraxd@gmail.com>
2024-02-05 11:18:54 +0100

    Added parameters to compile gate

--- qualtran/bloqs/chemistry/prepare_mps/compile_gate.py
+++ qualtran/bloqs/chemistry/prepare_mps/compile_gate.py
@@ -1,32 +1,22 @@
-from typing import Dict
+from typing import Dict, Tuple
 import attrs
-from __future__ import annotations
-from math import acos, sqrt
-
-import numpy as np
-from numpy.typing import ArrayLike
 
 from qualtran import Bloq, BloqBuilder, Signature, SoquetT
-from qualtran.bloqs.qrom import QROM
+from qualtran.bloqs.controlled_state_preparation import ControlledStatePreparationUsingRotations
 
 
 @attrs.frozen
-class CompileGate(Bloq):
-    n: int  # number of qubits that the gate acts on
-    k: int  # number of rows of the gate specified
+class CompileGateGivenVectors(Bloq):
+    n_qubits: int  # number of qubits that the gate acts on
+    gate_coefs: Tuple[Tuple] # tuple with the columns/rows of the gate that are specified
+    adjoint: bool = False
 
     @property
     def signature(self):
-        return Signature.build(x=self.n)
+        return Signature.build(x=self.n_qubits)
 
     def build_composite_bloq(
-        self, bb: BloqBuilder, *, x: SoquetT, cols: ArrayLike[ArrayLike]
+        self, bb: BloqBuilder, *, x: SoquetT
     ) -> Dict[str, SoquetT]:
-        for i, col in enumerate(cols):
-            i_state = np.zeros(col.shape)
-            i_state[i] = 1/np.sqrt(2)
-            w = np.concatenate(1/np.sqrt(2)*col, i_state)
             
         return {"x": x}
-
-
