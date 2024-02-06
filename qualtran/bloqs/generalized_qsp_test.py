[GQSP] fix bug in phase angle calculation (#636)
* gqsp: fix for real polynomials (now works till degree 150)

* attempt fixing phi

* simplify decomp. of SU2 gate

* small inline test for checking each QSP angle

* fix mistake in paper

* cleanup code: remove special case of real coeffs.

* accidental debug comment

* slightly larger degrees

* check symbolic till deg 10, for complex poly.

* fix incorrect matrix equality check

* rename test

* update expr. for phi to match paper more closely

qualtran/bloqs/generalized_qsp_test.py
@@ -130,10 +130,9 @@ def evaluate_polynomial_of_matrix(P: Sequence[complex], U: NDArray) -> NDArray:
     return result
 
 
-def assert_matrices_same_upto_global_phase(A: NDArray, B: NDArray):
+def assert_matrices_almost_equal(A: NDArray, B: NDArray):
     assert A.shape == B.shape
-    assert np.linalg.norm(A @ A.conj().T - B @ B.conj().T) <= 1e-5
-    assert np.linalg.norm(A.conj().T @ A - B.conj().T @ B) <= 1e-5
+    assert np.linalg.norm(A - B) <= 1e-5
 
 
 def verify_generalized_qsp(U: GateWithRegisters, P: Sequence[complex]):
@@ -144,21 +143,32 @@ def verify_generalized_qsp(U: GateWithRegisters, P: Sequence[complex]):
 
     expected_top_left = evaluate_polynomial_of_matrix(P, input_unitary)
     actual_top_left = result_unitary[:N, :N]
-    assert_matrices_same_upto_global_phase(expected_top_left, actual_top_left)
+    assert_matrices_almost_equal(expected_top_left, actual_top_left)
 
     expected_bottom_left = evaluate_polynomial_of_matrix(gqsp_U.Q, input_unitary)
     actual_bottom_left = result_unitary[N:, :N]
-    assert_matrices_same_upto_global_phase(expected_bottom_left, actual_bottom_left)
+    assert_matrices_almost_equal(expected_bottom_left, actual_bottom_left)
 
 
 @pytest.mark.parametrize("bitsize", [1, 2, 3])
-@pytest.mark.parametrize("degree", [2, 3, 4])
-def test_generalized_real_qsp_on_random_unitaries(bitsize: int, degree: int):
+@pytest.mark.parametrize("degree", [2, 3, 4, 5, 50, 100, 150, 180])
+def test_generalized_qsp_with_real_poly_on_random_unitaries(bitsize: int, degree: int):
     random_state = np.random.RandomState(42)
 
-    for _ in range(20):
+    for _ in range(10):
         U = RandomGate.create(bitsize, random_state=random_state)
         P = random_qsp_polynomial(degree, random_state=random_state, only_real_coeffs=True)
+        verify_generalized_qsp(U, P)
+
+
+@pytest.mark.parametrize("bitsize", [1, 2, 3])
+@pytest.mark.parametrize("degree", [2, 3, 4, 5, 50, 100, 120])
+def test_generalized_qsp_with_complex_poly_on_random_unitaries(bitsize: int, degree: int):
+    random_state = np.random.RandomState(42)
+
+    for _ in range(10):
+        U = RandomGate.create(bitsize, random_state=random_state)
+        P = random_qsp_polynomial(degree, random_state=random_state)
         verify_generalized_qsp(U, P)
 
 
@@ -220,10 +230,10 @@ class SymbolicGQSP:
         assert abs(error_QU) <= 1e-5
 
 
-@pytest.mark.parametrize("degree", [2, 3, 4])
+@pytest.mark.parametrize("degree", [2, 3, 4, 5, 10])
 def test_generalized_real_qsp_with_symbolic_signal_matrix(degree: int):
     random_state = np.random.RandomState(102)
 
     for _ in range(10):
-        P = random_qsp_polynomial(degree, random_state=random_state, only_real_coeffs=True)
+        P = random_qsp_polynomial(degree, random_state=random_state)
         SymbolicGQSP(P).verify()