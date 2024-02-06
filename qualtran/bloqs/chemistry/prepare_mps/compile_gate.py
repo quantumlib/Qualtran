63e7052c711fdb9922f8a28f493a79844b84c59f
Víctor Carballo <vicaraxd@gmail.com>
2024-02-06 09:44:04 +0100

    Added prepare oracle class placeholder

--- qualtran/bloqs/chemistry/prepare_mps/compile_gate.py
+++ qualtran/bloqs/chemistry/prepare_mps/compile_gate.py
@@ -1,8 +1,10 @@
 from typing import Dict, Tuple
 import attrs
 
-from qualtran import Bloq, BloqBuilder, Signature, SoquetT
+
+from qualtran import Bloq, BloqBuilder, SelectionRegister, Signature, SoquetT
 from qualtran.bloqs.controlled_state_preparation import ControlledStatePreparationUsingRotations
+from qualtran.bloqs.select_and_prepare import PrepareOracle
 
 
 @attrs.frozen
@@ -20,3 +22,20 @@ class CompileGateGivenVectors(Bloq):
     ) -> Dict[str, SoquetT]:
             
         return {"x": x}
+
+
+@attrs.frozen
+class PrepareOracleCompileGateReflection(PrepareOracle):
+    n_qubits: int # length in qubits of the state |u_i> (without the reflection ancilla!)
+    state: Tuple # state |u_i>
+    index: int # i value in |i>
+
+    @property
+    def selection_registers(self) -> Tuple[SelectionRegister, ...]:
+        return (
+            SelectionRegister(
+                "state", bitsize=self.n_qubits+1, iteration_length=self.n_qubits+1
+            ),
+        )
+    
+    
