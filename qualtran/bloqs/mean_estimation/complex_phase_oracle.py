5791bdfe99a0b4a0de891eebc8b94f2dbd74d4d8
Matthew Harrigan <mpharrigan@google.com>
2024-02-14 13:23:57 -0800

    Use functools.cached_property (instead of cirq)

--- qualtran/bloqs/mean_estimation/complex_phase_oracle.py
+++ qualtran/bloqs/mean_estimation/complex_phase_oracle.py
@@ -12,11 +12,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from functools import cached_property
 from typing import Tuple
 
 import attrs
 import cirq
-from cirq._compat import cached_property
 from numpy.typing import NDArray
 
 from qualtran import GateWithRegisters, Register, Signature
