700645744a2758a792cad05db3a116b1ea125d83
Fionn Malone <fmalone@google.com>
2024-02-23 23:44:55 +0000

    Fix lint.

--- qualtran/serialization/data_types.py
+++ qualtran/serialization/data_types.py
@@ -11,10 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import Union
-
-import sympy
-
 from qualtran import BoundedQUInt, QAny, QBit, QDType, QFxp, QInt, QIntOnesComp, QUInt
 from qualtran.protos import data_types_pb2
 from qualtran.serialization.args import int_or_sympy_from_proto, int_or_sympy_to_proto
