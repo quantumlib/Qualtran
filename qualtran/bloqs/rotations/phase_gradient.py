5e1508c115b29d0e0a09b2a3873434828e725d38
Tanuj Khattar <tanujkhattar@google.com>
2024-02-26 13:04:33 -0800

    Remove extra imports to make pylint happy

--- qualtran/bloqs/rotations/phase_gradient.py
+++ qualtran/bloqs/rotations/phase_gradient.py
@@ -12,8 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import itertools
-import math
 from functools import cached_property
 from typing import Any, Dict, Iterable, Sequence, Set, TYPE_CHECKING, Union
 
