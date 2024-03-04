3af59f1d7b76988ba2e559ad842c951afe348c1a
Fionn Malone <fmalone@google.com>
2024-03-04 23:29:45 +0000

    Add bloq to build trotterized unitary.

---
+++ qualtran/bloqs/chemistry/trotter/trotterized_unitary.py
@@ -0,0 +1,123 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Bloq for building a Trotterized unitary"""
+
+from functools import cached_property
+from typing import Dict, Iterable, Union
+
+import attrs
+import sympy
+
+from qualtran import Bloq, bloq_example, BloqBuilder, BloqDocSpec, Signature, Soquet, SoquetT
+
+
+@attrs.frozen
+class TrotterizedUnitary(Bloq):
+    r"""Implement arbitrary trotterized unitary given any Trotter splitting of the Hamiltonian.
+
+    Given an arbitrary splitting of the Hamiltonian into $m$ terms
+
+    $$
+        H = \sum_{\gamma=1}^\Gamma H_\gamma
+    $$
+
+    then the unitary $e^{-i t H}$ can be approximately implemented via a $p$-th order product
+    formula
+
+    $$
+        S_p(t) = \prod_{v=1}^{\Upsilon}\prod_{k=1}^\Gamma e^{-it a_{v,\gamma} H_{\pi_v(\gamma)}}
+    $$
+
+    where $\Upsilon$ is the number of `stages`, a_{v, \gamma} are real numbers
+    and $\pi_v(\gamma)$ is a permutation of the Hamiltonian terms.
+
+    To construct the unitary we adopt the convention from the second reference
+    which simply requires a list of the coefficients and a list of indices which
+    index the hamiltonian terms.
+
+    For example, the second order Suzuki splitting could be built like
+
+    >>> indicies = (0, 1, 0)
+    >>> coeffs = (0.5, 1, 0.5)
+    >>> suzuki = TrotterizedUnitary((U1, U2), indicies, coeffs, timestep=0.1)
+
+    where $U1 = e^{-i t H_1}$ and $U2 = e^{-i t H_2}$ are bloqs implementing the
+    individula unitaries required by the splitting method.
+
+
+    Args:
+        bloqs: A tuple of bloqs of length `m` which implement the unitaries for
+            each term in the Hamiltonian.
+        indices: A tuple of integers which specifies which bloq to apply when
+            forming the unitary as a product of unitaries.
+        coeffs: The coefficients `a` which appear in the expression for the unitary.
+        timestep: The timestep `t`.
+
+    Registers:
+        system: The system register to which to apply the unitary.
+
+    References:
+        [Theory of Trotter Error with Commutator Scaling](
+            https://journals.aps.org/prx/abstract/10.1103/PhysRevX.11.011020) Eq. 12 page 7.
+        [Trotter error with commutator scaling for the Fermi-Hubbard model](
+            https://arxiv.org/abs/2306.10603) see github repo for software to produce splittings.
+    """
+
+    bloqs: Iterable[Bloq]
+    indices: Iterable[int]
+    coeffs: Iterable[Union[float, sympy.Expr]]
+    timestep: Union[float, sympy.Expr]
+
+    def __attrs_post_init__(self):
+        ref_sig = self.bloqs[0].signature
+        for bloq in self.bloqs[1:]:
+            assert bloq.signature == ref_sig
+
+    @cached_property
+    def signature(self) -> Signature:
+        return self.bloqs[0].signature
+
+    def build_composite_bloq(self, bb: 'BloqBuilder', **soqs: SoquetT) -> Dict[str, 'Soquet']:
+        for i, c in zip(self.indices, self.coeffs):
+            soqs |= bb.add_d(attrs.evolve(self.bloqs[i], angle=2 * c * self.timestep), **soqs)
+        return soqs
+
+
+@bloq_example
+def _trott_unitary() -> TrotterizedUnitary:
+    from qualtran.bloqs.for_testing.ising import IsingXUnitary, IsingZZUnitary
+
+    nsites = 3
+    j_zz = 2
+    gamma_x = 0.1
+    dt = 0.01
+    indices = (0, 1, 0)
+    coeffs = (0.5 * gamma_x, j_zz, 0.5 * gamma_x)
+    # The angles for the Trotter bloqs will be overwritten, so these are placeholder values.
+    zz_bloq = IsingZZUnitary(nsites=nsites, angle=2 * dt * j_zz)
+    x_bloq = IsingXUnitary(nsites=nsites, angle=0.5 * 2 * dt * gamma_x)
+    trott_unitary = TrotterizedUnitary(
+        bloqs=(x_bloq, zz_bloq), indices=indices, coeffs=coeffs, timestep=dt
+    )
+    return trott_unitary
+
+
+_TROTT_UNITARY_DOC = BloqDocSpec(
+    bloq_cls=TrotterizedUnitary,
+    import_line=(
+        'from qualtran.bloqs.for_testing.ising import IsingXUnitary, IsingZZUnitary\n'
+        'from qualtran.bloqs.chemistry.trotter.trotterized_unitary import TrotterizedUnitary'
+    ),
+    examples=(_trott_unitary,),
+)
