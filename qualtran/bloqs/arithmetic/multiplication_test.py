eca0cd4544abd32fa059d3a427abc7e8881e1b61
Frankie Papa <fpapa250@gmail.com>
2024-03-12 16:37:36 -0700

    linting

--- qualtran/bloqs/arithmetic/multiplication_test.py
+++ qualtran/bloqs/arithmetic/multiplication_test.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import cirq
-import numpy as np
 import pytest
 
 from qualtran import BloqBuilder, QUInt, Register
