61453fc00a1d36843bddd2f47a0161d512f6b84e
Fionn Malone <fmalone@google.com>
2024-03-12 06:03:07 +0000

    Add bloq classification function.

---
+++ qualtran/resource_counting/classify_bloqs.py
@@ -0,0 +1,91 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import importlib
+import inspect
+import sys
+from collections import defaultdict
+from typing import Dict, Optional, Sequence, Tuple, TYPE_CHECKING, Union
+
+from qualtran import Bloq
+from qualtran.bloqs.basic_gates import CSwap, TGate
+from qualtran.bloqs.reflection import Reflection
+from qualtran.bloqs.util_bloqs import Allocate, Free, Join, Split
+from qualtran.resource_counting.t_counts_from_sigma import _get_all_rotation_types
+
+if TYPE_CHECKING:
+    from qualtran import Bloq
+    from qualtran.resource_counting import BloqCountT, GeneralizerT, SympySymbolAllocator
+
+
+def _get_all_bloqs_in_module(bloq_module: str) -> Tuple[Bloq]:
+    """Returns all classes defined in bloqs.basic_gates which have an attribute `eps`."""
+    importlib.import_module(bloq_module)
+    all_members = [v for (_, v) in inspect.getmembers(sys.modules[bloq_module], inspect.isclass)]
+    defined_in_module = tuple(member for member in all_members if bloq_module in member.__module__)
+    return defined_in_module
+
+
+def _get_basic_bloq_classification() -> Dict[str, Tuple[Bloq]]:
+    """High level classification of bloqs by the module name."""
+
+    bloq_classifier = {
+        'arithmetic': _get_all_bloqs_in_module('qualtran.bloqs.arithmetic'),
+        'rotations': _get_all_bloqs_in_module('qualtran.bloqs.rotations')
+        + _get_all_rotation_types(),
+        'state_preparation': _get_all_bloqs_in_module('qualtran.bloqs.state_preparation'),
+        'data_loading': _get_all_bloqs_in_module('qualtran.bloqs.data_loading'),
+        'mcmt': _get_all_bloqs_in_module('qualtran.bloqs.mcmt'),
+        'multiplexers': _get_all_bloqs_in_module('qualtran.bloqs.multiplexers'),
+        'swaps': _get_all_bloqs_in_module('qualtran.bloqs.swap_network') + (CSwap,),
+        'reflection': (Reflection,),
+    }
+    return bloq_classifier
+
+
+def classify_t_count_by_bloq_type(
+    bloq: Bloq,
+    generalizer: Optional[Union['GeneralizerT', Sequence['GeneralizerT']]] = None,
+    bloq_classification: Optional[Dict[str, Tuple[Bloq]]] = None,
+) -> Dict[str, int]:
+    """Classify (bin) the T count of a bloq's call graph by type of operation.
+
+    Args:
+        bloq: the bloq to classify.
+        generalizer: If provided, run this function on each (sub)bloq to replace attributes
+            that do not affect resource estimates with generic sympy symbols. If the function
+            returns `None`, the bloq is omitted from the counts graph. If a sequence of
+            generalizers is provided, each generalizer will be run in order.
+        bloq_classification: An optional dictionary mapping bloq_classifications to bloq types.
+
+    Returns
+        classified_bloqs: dictionary containing the T count for different types of bloqs.
+    """
+    classified_bloqs = defaultdict(int)
+    if bloq_classification is None:
+        bloq_classification = _get_basic_bloq_classification()
+    for bloq, num_calls in bloq.bloq_counts().items():
+        # only classify bloqs which contribute to the T count.
+        if isinstance(bloq, (Split, Join, Allocate, Free)):
+            continue
+        num_t = bloq.call_graph(generalizer=generalizer)[1].get(TGate())
+        if num_t is not None:
+            num_t = int(num_t)
+            is_classified = False
+            for k, v in bloq_classification.items():
+                if isinstance(bloq, v):
+                    classified_bloqs[k] += num_calls * num_t
+                    is_classified = True
+            if not is_classified:
+                classified_bloqs['other'] += num_calls * num_t
+    return classified_bloqs
---
+++ qualtran/resource_counting/classify_bloqs_test.py
@@ -0,0 +1,88 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from typing import Set, TYPE_CHECKING
+
+import attrs
+import pytest
+
+from qualtran import Bloq, QInt, Signature
+from qualtran.bloqs.arithmetic import Add
+from qualtran.bloqs.arithmetic.comparison import LessThanConstant
+from qualtran.bloqs.basic_gates import CSwap, TGate
+from qualtran.bloqs.data_loading.qrom import QROM
+from qualtran.bloqs.mcmt.and_bloq import And
+from qualtran.bloqs.reflection import Reflection
+from qualtran.bloqs.rotations.hamming_weight_phasing import HammingWeightPhasing
+from qualtran.resource_counting import BloqCountT
+from qualtran.resource_counting.classify_bloqs import (
+    _get_all_bloqs_in_module,
+    classify_t_count_by_bloq_type,
+)
+
+if TYPE_CHECKING:
+    from qualtran.resource_counting import BloqCountT, GeneralizerT, SympySymbolAllocator
+
+
+@attrs.frozen
+class TestBundleOfBloqs(Bloq):
+    """A fake bloq which just defines a call graph"""
+
+    bloqs: BloqCountT
+
+    def signature(self) -> 'Signature':
+        return Signature.build()
+
+    def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
+        return self.bloqs
+
+
+@pytest.mark.parametrize(
+    'bloq_count, classification',
+    (
+        (((CSwap(10), 42),), 'swaps'),
+        (((HammingWeightPhasing(10, 1.11), 11),), 'rotations'),
+        (((Add(QInt(8)), 4),), 'arithmetic'),
+        (((QROM.build([4, 10, 11, 34]), 8),), 'data_loading'),
+        (((And(), 4),), 'mcmt'),
+        (((Reflection((3, 3, 2), (0, 0, 1)), 100),), 'reflection'),
+        (((LessThanConstant(8, 3), 10),), 'arithmetic'),
+    ),
+)
+def test_default_classification(bloq_count, classification):
+    bloq = TestBundleOfBloqs(bloq_count)
+    classified_bloqs = classify_t_count_by_bloq_type(bloq)
+    bc = bloq_count[0]
+    assert classified_bloqs[classification] == bc[1] * bc[0].call_graph()[1].get(TGate())
+
+
+def test_get_all_bloqs_in_module():
+    comparitors = _get_all_bloqs_in_module('qualtran.bloqs.arithmetic.comparison')
+    assert isinstance(LessThanConstant(8, 3), comparitors)
+    arithmetic = _get_all_bloqs_in_module('qualtran.bloqs.arithmetic')
+    assert isinstance(LessThanConstant(8, 3), arithmetic)
+    rotation = _get_all_bloqs_in_module('qualtran.bloqs.rotations')
+    assert not isinstance(LessThanConstant(8, 3), rotation)
+    assert isinstance(HammingWeightPhasing(10, 1.11), rotation)
+    data_load = _get_all_bloqs_in_module('qualtran.bloqs.data_loading')
+    assert isinstance(QROM.build([4, 10, 11, 23]), data_load)
+
+
+def test_classify_bloq_counts_with_custom_bloq_classification():
+    bloq_classification = {'swaps': (CSwap,)}
+    test_bloq = TestBundleOfBloqs(((CSwap(10), 42), (Add(QInt(4)), 3)))
+    classified_bloqs = classify_t_count_by_bloq_type(
+        test_bloq, bloq_classification=bloq_classification
+    )
+    assert classified_bloqs == {'swaps': 42 * 10 * 7, 'other': 3 * 4 * (4 - 1)}
+    assert test_bloq.call_graph()[1].get(TGate()) == sum(classified_bloqs.values())
