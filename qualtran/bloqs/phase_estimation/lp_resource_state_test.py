8ef73f1c87789c2f6476ae879d5e7b99b0fd2d87
Tanuj Khattar <tanujkhattar@google.com>
2024-03-13 02:10:27 -0700

    Add LPResourceState Bloq to prepare resource state for optimal phase estimation of walk operator

---
+++ qualtran/bloqs/phase_estimation/lp_resource_state_test.py
@@ -0,0 +1,34 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import numpy as np
+import pytest
+
+from qualtran.bloqs.phase_estimation.lp_resource_state import LPResourceState
+from qualtran.cirq_interop.testing import (
+    assert_decompose_is_consistent_with_t_complexity,
+    GateHelper,
+)
+
+
+@pytest.mark.parametrize('n', [*range(1, 14, 2)])
+def test_prepares_resource_state(n):
+    bloq = LPResourceState(n)
+    state = GateHelper(bloq).circuit.final_state_vector()
+    np.testing.assert_allclose(state, bloq.state())
+
+
+@pytest.mark.parametrize('n', [*range(1, 14, 2)])
+def test_t_complexity(n):
+    bloq = LPResourceState(n)
+    assert_decompose_is_consistent_with_t_complexity(bloq)
