29111557498563b0575e7ba3793a397c63cefc08
Tanuj Khattar <tanujkhattar@google.com>
2024-03-15 12:41:28 -0700

    Fix pylint

--- qualtran/bloqs/qft/approximate_qft.py
+++ qualtran/bloqs/qft/approximate_qft.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from functools import cached_property
-from typing import Set, TYPE_CHECKING, Union
+from typing import Set, TYPE_CHECKING
 
 import attrs
 import cirq
