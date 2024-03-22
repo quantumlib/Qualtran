bdd2f648b3945ed5e1e9304d59a73b7404ebf93b
Tanuj Khattar <tanujkhattar@google.com>
2024-03-21 18:24:42 -0700

    Kitaev phase estimation Bloq

---
+++ qualtran/bloqs/phase_estimation/kitaev_qpe_text_book.py
@@ -0,0 +1,96 @@
+# Copyright 2024 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import math
+from functools import cached_property
+from typing import Tuple
+
+import attrs
+import cirq
+
+from qualtran import Bloq, GateWithRegisters, Register, Signature
+from qualtran.bloqs.basic_gates import Hadamard, OnEach
+from qualtran.bloqs.qft.qft_text_book import QFTTextBook
+
+
+@attrs.frozen
+class KitaevQPE(GateWithRegisters):
+    r"""Kitaev's Phase Estimation algorithm
+
+    Originally introduced by Kitaev in https://arxiv.org/abs/quant-ph/9511026.
+
+    Args:
+        m_bits: Bitsize of the phase register to be used during phase estimation
+        unitary: A cirq Gate representing the unitary to run the phase estimation protocol on.
+    """
+
+    unitary: Bloq
+    m_bits: int
+    state_prep: Bloq = attrs.field()
+    inverse_qft: Bloq = attrs.field()
+
+    @state_prep.default
+    def _default_state_prep(self):
+        return OnEach(self.m_bits, Hadamard())
+
+    @inverse_qft.default
+    def _default_inverse_qft(self):
+        return QFTTextBook(self.m_bits, with_reverse=True).adjoint()
+
+    def __attrs_post_init__(self):
+        assert self.state_prep.signature.n_qubits() == self.m_bits
+
+    @classmethod
+    def from_precision_and_eps(cls, unitary: Bloq, precision: int, eps: float):
+        r"""Obtain accurate estimate of $\phi$ to $precision$ bits with $1-eps$ success probability.
+
+        Uses Eq 5.35 from Neilson and Chuang to estimate the size of phase register s.t. we can
+        estimate the phase $\phi$ to $precision$ bits of accuracy with probability at least
+        $1 - eps$.
+
+        $$
+            t = n + ceil(\log(2 + \frac{1}{2\eps}))
+        $$
+
+        Args:
+            unitary: Unitary operation to obtain phase estimate of.
+            precision: Number of bits of precision
+            eps: Probability of success.
+        """
+        m_bits = precision + math.ceil(math.log(2 + 1 / (2 * eps)))
+        return KitaevQPE(m_bits=m_bits, unitary=unitary)
+
+    @cached_property
+    def target_registers(self) -> Tuple[Register, ...]:
+        return tuple(self.unitary.signature)
+
+    @cached_property
+    def phase_registers(self) -> Tuple[Register, ...]:
+        return tuple(Signature.build(qpe_reg=self.m_bits))
+
+    @cached_property
+    def signature(self) -> Signature:
+        return Signature([*self.phase_registers, *self.target_registers])
+
+    def decompose_from_registers(
+        self, context: cirq.DecompositionContext, **quregs
+    ) -> cirq.OP_TREE:
+        target_quregs = {reg.name: quregs[reg.name] for reg in self.target_registers}
+        unitary_op = self.unitary.on_registers(**target_quregs)
+
+        phase_qubits = quregs['qpe_reg']
+
+        yield self.state_prep.on(*phase_qubits)
+        for i, qbit in enumerate(phase_qubits[::-1]):
+            yield cirq.pow(unitary_op.controlled_by(qbit), 2**i)
+        yield self.inverse_qft.on(*phase_qubits)
---
+++ qualtran/bloqs/phase_estimation/kitaev_qpe_text_book_test.py
@@ -0,0 +1,83 @@
+# Copyright 2024 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cirq
+import numpy as np
+import pytest
+
+from qualtran.bloqs.basic_gates import ZPowGate
+from qualtran.bloqs.for_testing.qubitization_walk_test import get_uniform_pauli_qubitized_walk
+from qualtran.bloqs.phase_estimation.kitaev_qpe_text_book import KitaevQPE
+from qualtran.cirq_interop.testing import GateHelper
+
+
+def simulate_theta_estimate(circuit, measurement_register) -> float:
+    sim = cirq.Simulator()
+    qubit_order = cirq.QubitOrder.explicit(measurement_register, fallback=cirq.QubitOrder.DEFAULT)
+    final_state = sim.simulate(circuit, qubit_order=qubit_order).final_state_vector
+    m_bits = len(measurement_register)
+    samples = cirq.sample_state_vector(final_state, indices=[*range(m_bits)], repetitions=500)
+    counts = np.bincount(samples.dot(1 << np.arange(samples.shape[-1] - 1, -1, -1)))
+    assert len(counts) <= 2**m_bits
+    return np.argmax(counts) / 2**m_bits
+
+
+@pytest.mark.parametrize('theta', [0.234, 0.78, 0.54])
+def test_kitaev_phase_estimation_zpow_theta(theta):
+    precision, error_bound = 3, 0.1
+    gh = GateHelper(KitaevQPE(ZPowGate(exponent=2 * theta), precision))
+    circuit = cirq.Circuit(cirq.X(*gh.quregs['q']), cirq.decompose_once(gh.operation))
+    precision_register = gh.quregs['qpe_reg']
+    assert abs(simulate_theta_estimate(circuit, precision_register) - theta) < error_bound
+
+
+@pytest.mark.parametrize('num_terms', [2, 3, 4])
+def test_kitaev_phase_estimation_qubitized_walk(num_terms: int):
+    precision, eps = 6, 0.05
+    ham, walk = get_uniform_pauli_qubitized_walk(num_terms)
+
+    ham_coeff = [abs(ps.coefficient.real) for ps in ham]
+    qubitization_lambda = np.sum(ham_coeff)
+    g = GateHelper(walk)
+    L_state = np.zeros(2 ** len(g.quregs['selection']))
+    L_state[: len(ham_coeff)] = np.sqrt(ham_coeff / qubitization_lambda)
+
+    eigen_values, eigen_vectors = np.linalg.eigh(ham.matrix())
+
+    for eig_idx, eig_val in enumerate(eigen_values):
+        # Applying QPE to determine eigenvalue for walk operator W on initial state |L>|k>
+        K_state = eigen_vectors[:, eig_idx].flatten()
+        L_K = np.kron(L_state, K_state)
+        L_K /= np.linalg.norm(L_K)
+        gh = GateHelper(KitaevQPE(walk, precision))
+        qpe_reg, selection, target = (
+            gh.quregs['qpe_reg'],
+            gh.quregs['selection'],
+            gh.quregs['target'],
+        )
+        qpe = cirq.Circuit(cirq.decompose_once(gh.operation))
+        prep_L_K = cirq.Circuit(
+            cirq.StatePreparationChannel(L_K, name="PREP_L_K").on(*selection, *target)
+        )
+        qpe_with_init = prep_L_K + qpe
+        assert len(qpe_with_init.all_qubits()) < 23
+        phase = simulate_theta_estimate(qpe_with_init, qpe_reg)
+
+        expected = np.cos(phase * 2 * np.pi)
+        is_close = [
+            np.allclose(eig_val / qubitization_lambda, expected, atol=eps),
+            np.allclose(eig_val / qubitization_lambda, -expected, atol=eps),
+        ]
+        if np.abs(eig_val) > 1e-6:
+            assert np.any(is_close)
