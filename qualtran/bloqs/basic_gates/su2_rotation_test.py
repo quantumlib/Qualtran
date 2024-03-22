ff015fb42781206d8e0595336c1b835881d4eddf
Anurudh Peduri <anurudhp97@gmail.com>
2024-03-22 20:38:58 +0100

    add `from_matrix`

--- qualtran/bloqs/basic_gates/su2_rotation_test.py
+++ qualtran/bloqs/basic_gates/su2_rotation_test.py
@@ -14,7 +14,9 @@
 import cirq
 import numpy as np
 
-from . import Hadamard, TGate
+from qualtran import Bloq
+from qualtran.bloqs.basic_gates import Hadamard, TGate, XGate, YGate, ZGate
+
 from .su2_rotation import _hadamard, _su2_rotation_gate, _t_gate, SU2RotationGate
 
 
@@ -47,3 +49,21 @@ def test_su2_rotation_gates(bloq_autotester):
 def test_su2_rotation_gate_example_unitaries_match():
     np.testing.assert_allclose(_t_gate().tensor_contract(), TGate().tensor_contract())
     np.testing.assert_allclose(_hadamard().tensor_contract(), Hadamard().tensor_contract())
+
+
+def test_from_matrix_on_random_unitaries():
+    random_state = np.random.RandomState(42)
+
+    for _ in range(20):
+        mat = cirq.testing.random_unitary(2, random_state=random_state)
+        np.testing.assert_allclose(SU2RotationGate.from_matrix(mat).rotation_matrix, mat)
+
+
+def test_from_matrix_on_standard_gates():
+    gates: list[Bloq] = [TGate(), XGate(), YGate(), ZGate(), Hadamard()]
+
+    for gate in gates:
+        mat = gate.tensor_contract()
+        np.testing.assert_allclose(
+            SU2RotationGate.from_matrix(mat).rotation_matrix, mat, atol=1e-15
+        )
