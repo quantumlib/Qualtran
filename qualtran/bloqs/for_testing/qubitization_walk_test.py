2a5c2d40877a8bf7a2b10a1cf11d124368d53bc9
Tanuj Khattar <tanujkhattar@google.com>
2024-03-21 23:39:57 -0700

    Fix pylint and formatting

--- qualtran/bloqs/for_testing/qubitization_walk_test.py
+++ qualtran/bloqs/for_testing/qubitization_walk_test.py
@@ -1,27 +1,29 @@
-# Copyright 2024 The Cirq Developers
+#  Copyright 2024 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import cirq
-import attrs
-from numpy._typing import NDArray
-from typing import Tuple
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 from functools import cached_property
-from qualtran.bloqs.select_and_prepare import PrepareOracle
+from typing import Tuple
+
+import attrs
+import cirq
 import scipy
+from numpy._typing import NDArray
+
 from qualtran import Signature
-from qualtran.bloqs.state_preparation import PrepareUniformSuperposition
 from qualtran.bloqs.multiplexers.select_pauli_lcu import SelectPauliLCU
 from qualtran.bloqs.qubitization_walk_operator import QubitizationWalkOperator
+from qualtran.bloqs.select_and_prepare import PrepareOracle
+from qualtran.bloqs.state_preparation import PrepareUniformSuperposition
 
 
 @attrs.frozen
