ac2264dd1cf78e313052d3591dc46fb48ce7de4c
Tanuj Khattar <tanujkhattar@google.com>
2024-03-25 23:39:19 -0700

    Fix pylint

--- qualtran/bloqs/basic_gates/t_gate.py
+++ qualtran/bloqs/basic_gates/t_gate.py
@@ -104,8 +104,8 @@ class TGate(Bloq):
         import cirq
 
         (q,) = q
-        pow = -1 if self.is_adjoint else 1
-        return cirq.T(q) ** pow, {'q': np.array([q])}
+        p = -1 if self.is_adjoint else 1
+        return cirq.T(q) ** p, {'q': np.array([q])}
 
     def pretty_name(self) -> str:
         maybe_dag = '†' if self.is_adjoint else ''
