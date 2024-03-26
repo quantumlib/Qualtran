5c57c950dde725b3c652378906dfe8f932e0790e
Matthew Harrigan <mpharrigan@google.com>
2024-03-26 20:04:53 +0000

    Remove sneaky fredkin decomposition (#803)
    * Remove sneaky fredkin decomposition
    
    This should be a gate/bloq
    
    * lint
    
    * Port clifford=10 to qualtran
    
    * break from cirq
    
    * lint

--- qualtran/bloqs/basic_gates/swap_test.py
+++ qualtran/bloqs/basic_gates/swap_test.py
@@ -37,6 +37,7 @@ from qualtran.bloqs.basic_gates.swap import (
     _swap_matrix,
     _swap_small,
 )
+from qualtran.cirq_interop.t_complexity_protocol import t_complexity, TComplexity
 from qualtran.resource_counting.generalizers import ignore_split_join
 
 
@@ -91,6 +92,9 @@ def _set_ctrl_two_bit_swap(ctrl_bit):
 def test_two_bit_cswap():
     cswap = TwoBitCSwap()
     np.testing.assert_array_equal(cswap.tensor_contract(), cirq.unitary(cirq.CSWAP))
+    np.testing.assert_allclose(
+        cswap.decompose_bloq().tensor_contract(), cirq.unitary(cirq.CSWAP), atol=1e-8
+    )
 
     # Zero ctrl -- it's identity
     np.testing.assert_array_equal(np.eye(4), _set_ctrl_two_bit_swap(0).tensor_contract())
@@ -104,13 +108,6 @@ def test_two_bit_cswap():
     assert (ctrl, x, y) == (0, 1, 0)
     ctrl, x, y = cswap.call_classically(ctrl=1, x=1, y=0)
     assert (ctrl, x, y) == (1, 0, 1)
-
-    # cirq
-    c1 = cirq.Circuit([cirq.CSWAP(*cirq.LineQubit.range(3))]).freeze()
-    c2, _ = cswap.as_composite_bloq().to_cirq_circuit(
-        ctrl=[cirq.LineQubit(0)], x=[cirq.LineQubit(1)], y=[cirq.LineQubit(2)]
-    )
-    assert c1 == c2
 
 
 def _set_ctrl_swap(ctrl_bit, bloq: CSwap):
@@ -154,10 +151,6 @@ y1: ─────×(y)───────×───┼───
 y2: ─────×(y)───────────×───
     ''',
     )
-    expected_circuit = cirq.Circuit(
-        cswap_op, [cirq.CSWAP(*quregs['ctrl'], x, y) for (x, y) in zip(quregs['x'], quregs['y'])]
-    )
-    cirq.testing.assert_same_circuits(circuit, expected_circuit)
 
 
 def test_cswap_unitary():
@@ -194,6 +187,9 @@ def test_cswap_bloq_counts():
 
     counts2 = bloq.decompose_bloq().bloq_counts(generalizer=ignore_split_join)
     assert counts1 == counts2
+
+    assert t_complexity(CSwap(1)) == TComplexity(t=7, clifford=10)
+    assert t_complexity(TwoBitCSwap()) == TComplexity(t=7, clifford=10)
 
 
 def test_cswap_symbolic():
