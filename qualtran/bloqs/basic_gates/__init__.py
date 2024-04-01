c77e59d1a1ea4f1f94bb0d1f98e9e9e643ddce84
Anurudh Peduri <7265746+anurudhp@users.noreply.github.com>
2024-04-01 18:43:33 +0200

    move `SU2RotationGate` to `basic_gates/` (#802)
    * move `SU2RotationGate` to `basic_gates/`
    
    * add cirq_op and wire_symbol
    
    * add bloq_example
    
    * add tensor
    
    * add more bloq examples
    
    * add autotest, tensor test
    
    * support global shift, verify unitaries for examples
    
    * comment: can't use `lambda` for var name
    
    * add `from_matrix`
    
    * `_unitary_` + test
    
    * test T complexity
    
    * eps
    
    * support parameterized
    
    * regenerate notebook
    
    * use GlobalPhase bloq
    
    * 2024
    
    * fix TComplexity for `cirq.GlobalPhaseGate` and `cirq.global_phase_operation`
    
    * test call graph
    
    * code cleanup

--- qualtran/bloqs/basic_gates/__init__.py
+++ qualtran/bloqs/basic_gates/__init__.py
@@ -27,6 +27,7 @@ from .hadamard import Hadamard
 from .on_each import OnEach
 from .rotation import CZPowGate, Rx, Ry, Rz, XPowGate, YPowGate, ZPowGate
 from .s_gate import SGate
+from .su2_rotation import SU2RotationGate
 from .swap import CSwap, Swap, TwoBitCSwap, TwoBitSwap
 from .t_gate import TGate
 from .toffoli import Toffoli
