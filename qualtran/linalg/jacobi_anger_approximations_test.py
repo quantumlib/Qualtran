78421f795f6667696797aee5224d56a7be70e93d
Anurudh Peduri <anurudhp97@gmail.com>
2024-04-03 21:33:14 +0200

    more poly tests

--- qualtran/linalg/jacobi_anger_approximations_test.py
+++ qualtran/linalg/jacobi_anger_approximations_test.py
@@ -17,13 +17,14 @@ import pytest
 
 from .jacobi_anger_approximations import (
     approx_exp_cos_by_jacobi_anger,
+    approx_exp_sin_by_jacobi_anger,
     degree_jacobi_anger_approximation,
 )
 
 
 @pytest.mark.parametrize("t", [2, 3, 5, 10])
 @pytest.mark.parametrize("precision", [1e-5, 1e-7, 1e-10])
-def test_cos_approximation(t: float, precision: float):
+def test_exp_cos_approximation(t: float, precision: float):
     random_state = np.random.RandomState(42 + int(t))
 
     degree = degree_jacobi_anger_approximation(t, precision=precision)
@@ -32,4 +33,18 @@ def test_cos_approximation(t: float, precision: float):
     e_itheta = np.exp(1j * theta)
     np.testing.assert_allclose(
         P(e_itheta) * e_itheta ** (-degree), np.exp(1j * t * np.cos(theta)), rtol=precision * 2
+    )
+
+
+@pytest.mark.parametrize("t", [2, 3, 5, 10])
+@pytest.mark.parametrize("precision", [1e-5, 1e-7, 1e-10])
+def test_exp_sin_approximation(t: float, precision: float):
+    random_state = np.random.RandomState(42 + int(t))
+
+    degree = degree_jacobi_anger_approximation(t, precision=precision)
+    P = np.polynomial.Polynomial(approx_exp_sin_by_jacobi_anger(t, degree=degree))
+    theta = 2 * np.pi * random_state.random(1000)
+    e_itheta = np.exp(1j * theta)
+    np.testing.assert_allclose(
+        P(e_itheta) * e_itheta ** (-degree), np.exp(1j * t * np.sin(theta)), rtol=precision * 2
     )
