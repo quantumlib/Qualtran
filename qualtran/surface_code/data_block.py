5f1df0b33e5486b98cd9765c02c270f61168f832
Noureldin <noureldinyosri@google.com>
2024-04-03 17:31:36 -0700

    Add more data blocks (#831)

--- qualtran/surface_code/data_block.py
+++ qualtran/surface_code/data_block.py
@@ -14,10 +14,12 @@
 
 import abc
 import math
+from typing import Optional
 
-from attrs import frozen
+from attrs import field, frozen
 
 import qualtran.surface_code.quantum_error_correction_scheme_summary as qec
+from qualtran.surface_code.reference import Reference
 
 
 class DataBlock(metaclass=abc.ABCMeta):
@@ -32,7 +34,7 @@ class DataBlock(metaclass=abc.ABCMeta):
     def footprint(self, n_algo_qubits: int) -> int:
         """The number of physical qubits used by the data block.
 
-        Args:
+        Attributes:
             n_algo_qubits: The number of algorithm qubits whose data must be stored and
                 accessed.
         """
@@ -41,20 +43,27 @@ class DataBlock(metaclass=abc.ABCMeta):
     def data_error(self, n_algo_qubits: int, n_cycles: int, phys_err: float) -> float:
         """The error associated with storing data on `n_algo_qubits` for `n_cycles`."""
 
+    @abc.abstractmethod
+    def n_cycles_to_consume_a_magic_state(self) -> int:
+        """The worst case number of cycles needed to consume a magic state."""
+
 
 @frozen
 class SimpleDataBlock(DataBlock):
     """A simple data block that uses a fixed code distance and routing overhead.
 
-    Args:
+    Attributes:
         data_d: The code distance `d` for protecting the qubits in the data block.
-        routing_overhead: As an approximation, assume a number of routing or auxiliary
+        routing_overhead: As an approximation, assume some routing or auxiliary
             qubits proportional to the number of algorithm qubits.
+        qec_scheme: Underlying quantum error correction scheme.
+        reference: A description of the source of the model.
     """
 
     data_d: int
     routing_overhead: float = 0.5
     qec_scheme: qec.QuantumErrorCorrectionSchemeSummary = qec.FowlerSuperconductingQubits
+    reference: Optional[Reference] = None
 
     def n_logical_qubits(self, n_algo_qubits: int) -> int:
         """Number of logical qubits including overhead.
@@ -76,3 +85,113 @@ class SimpleDataBlock(DataBlock):
         return data_cells * self.qec_scheme.logical_error_rate(
             physical_error_rate=phys_err, code_distance=self.data_d
         )
+
+    def n_cycles_to_consume_a_magic_state(self) -> int:
+        return self.data_d
+
+
+@frozen
+class CompactDataBlock(SimpleDataBlock):
+    r"""The compact data block uses a fixed code distance and routing overhead.
+
+    The compact data block lays $n$ qubit batches in grid of shape (3, $n/2$) where
+    the data batches are lined in the first and last row with the middle row being
+    an ancilla region. This lowers the memory footprint of the block at the cost of an
+    increased number of cycles to consume a magic state.
+
+    Attributes:
+        data_d: The code distance `d` for protecting the qubits in the data block.
+        qec_scheme: Underlying quantum error correction scheme.
+        reference: A description of the source of the model.
+
+    References:
+        [A Game of Surface Codes](https://arxiv.org/abs/1808.02892)
+        page 7, figure 9
+    """
+
+    routing_overhead: float = field(default=0.5, init=False)
+    reference: Reference = field(
+        default=Reference(url='https://arxiv.org/abs/1808.02892', page=7), init=False
+    )
+
+    def n_cycles_to_consume_a_magic_state(self) -> int:
+        return 9 * self.data_d
+
+
+@frozen
+class IntermediateDataBlock(SimpleDataBlock):
+    r"""The intermediate data block uses a fixed code distance and routing overhead.
+
+    The intermediate data block lays $n$ qubit batches in grid of shape (2, $2n+2$) where
+    the data batches are lined in the first row with the second row being an ancilla region.
+
+    Attributes:
+        data_d: The code distance `d` for protecting the qubits in the data block.
+        qec_scheme: Underlying quantum error correction scheme.
+        reference: A description of the source of the model.
+
+    References:
+        [A Game of Surface Codes](https://arxiv.org/abs/1808.02892)
+        page 9, figure 13a
+    """
+
+    routing_overhead: float = field(default=1.0, init=False)
+    reference: Reference = field(
+        default=Reference(url='https://arxiv.org/abs/1808.02892', page=8), init=False
+    )
+
+    def n_cycles_to_consume_a_magic_state(self) -> int:
+        return 5 * self.data_d
+
+
+@frozen
+class FastDataBlock(DataBlock):
+    r"""The fast data block uses a fixed code distance and a square layout.
+
+    The fast data block lays $n$ qubit batches in a square grid of side length $1 + \sqrt{2n}$
+    where the bottom row is an ancilla region and the top $\sqrt{2n}x\sqrt{2n}$ region is divided
+    into alternating data and ancilla columns.
+    The increased footprint is to be able to consume magic states in a single timestep.
+
+    Attributes:
+        data_d: The code distance `d` for protecting the qubits in the data block.
+        qec_scheme: Underlying quantum error correction scheme.
+        reference: A description of the source of the model.
+
+    References:
+        [A Game of Surface Codes](https://arxiv.org/abs/1808.02892)
+        page 9, figure 13b
+    """
+
+    data_d: int
+    qec_scheme: qec.QuantumErrorCorrectionSchemeSummary = qec.FowlerSuperconductingQubits
+    reference: Reference = field(
+        default=Reference(url='https://arxiv.org/abs/1808.02892', page=9), init=False
+    )
+
+    @staticmethod
+    def grid_size(n_algo_qubits: int) -> int:
+        return math.ceil(2 * n_algo_qubits + math.sqrt(8 * n_algo_qubits) + 1)
+
+    def footprint(self, n_algo_qubits: int) -> int:
+        return FastDataBlock.grid_size(n_algo_qubits)
+
+    def data_error(self, n_algo_qubits: int, n_cycles: int, phys_err: float) -> float:
+        data_cells = self.n_logical_qubits(n_algo_qubits) * n_cycles
+        return data_cells * self.qec_scheme.logical_error_rate(
+            physical_error_rate=phys_err, code_distance=self.data_d
+        )
+
+    def n_cycles_to_consume_a_magic_state(self) -> int:
+        return self.data_d
+
+    @staticmethod
+    def from_error_budget(
+        error_budget: float,
+        n_algo_qubits: int,
+        qec_scheme: qec.QuantumErrorCorrectionSchemeSummary,
+        physical_error_rate: float,
+    ) -> 'FastDataBlock':
+        q = FastDataBlock.grid_size(n_algo_qubits)
+        d = qec_scheme.code_distance_from_budget(physical_error_rate, error_budget / q)
+        return FastDataBlock(data_d=d, qec_scheme=qec_scheme)
