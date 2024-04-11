98fbdda14f717f2955cf9ffdf7632d25db0aa0d8
Matthew Harrigan <mpharrigan@google.com>
2024-04-11 14:57:49 -0700

    revert hack

--- qualtran/cirq_interop/jupyter_tools.py
+++ qualtran/cirq_interop/jupyter_tools.py
@@ -62,7 +62,7 @@ def circuit_with_costs(circuit: 'cirq.AbstractCircuit') -> 'cirq.AbstractCircuit
     """Annotates each operation in the circuit with its T-complexity cost."""
 
     def _map_func(op: cirq.Operation, _):
-        t_cost = t_complexity_protocol.t_complexity(op.gate)
+        t_cost = t_complexity_protocol.t_complexity(op)
         return op.with_tags(f't:{t_cost.t:g},r:{t_cost.rotations:g}')
 
     return cirq.map_operations(circuit, map_func=_map_func)
