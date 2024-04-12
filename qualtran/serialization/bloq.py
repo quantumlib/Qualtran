87f87422f85bf87b9c8d7098675c9f7d7ad63161
local_linix <arren.bustamante@gmail.com>
2024-04-12 13:22:44 -0700

    Scratch test

--- qualtran/serialization/bloq.py
+++ qualtran/serialization/bloq.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+
+# Test Do not submit
 import dataclasses
 import inspect
 from typing import Any, Callable, Dict, List, Optional, Union
