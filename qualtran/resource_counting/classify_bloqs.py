6c6483593ee3bf7431113ffbf2ee0fa781c7afde
Matthew Harrigan <mpharrigan@google.com>
2024-04-24 11:56:02 -0700

    bloq counts

--- qualtran/resource_counting/classify_bloqs.py
+++ qualtran/resource_counting/classify_bloqs.py
@@ -105,3 +105,19 @@ def classify_t_count_by_bloq_type(
         classification = classify_bloq(k, bloq_classification)
         classified_bloqs[classification] += v * t_counts_from_sigma(k.call_graph()[1])
     return classified_bloqs
+
+
+def bloq_is_clifford(b: Bloq):
+    from qualtran.bloqs.basic_gates import CNOT, Hadamard, SGate, TwoBitSwap, XGate, ZGate
+    from qualtran.bloqs.mcmt.multi_control_multi_target_pauli import MultiTargetCNOT
+    from qualtran.bloqs.util_bloqs import ArbitraryClifford
+
+    if isinstance(b, Adjoint):
+        b = b.subbloq
+
+    if isinstance(
+        b, (TwoBitSwap, Hadamard, XGate, ZGate, ArbitraryClifford, CNOT, MultiTargetCNOT, SGate)
+    ):
+        return True
+
+    return False
