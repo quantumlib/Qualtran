ccdd693f3aabd969d481edc5b57e0e6a13e93228
Matthew Harrigan <mpharrigan@google.com>
2024-04-26 11:12:13 -0700

    Clean up and maintenance of checks

--- qualtran/conftest.py
+++ qualtran/conftest.py
@@ -75,7 +75,7 @@ def assert_equivalent_bloq_example_counts_for_pytest(bloq_ex: BloqExample):
         raise bce from bce
 
 
-def assert_bloq_example_serialize_for_pytest(bloq_ex: BloqExample):
+def assert_bloq_example_serializes_for_pytest(bloq_ex: BloqExample):
     if bloq_ex.name in [
         'prep_sparse',
         'thc_prep',
@@ -93,14 +93,14 @@ def assert_bloq_example_serialize_for_pytest(bloq_ex: BloqExample):
         pytest.xfail("Skipping serialization test for bloq examples that cannot yet be serialized.")
 
     try:
-        qlt_testing.assert_bloq_example_serialize(bloq_ex)
+        qlt_testing.assert_bloq_example_serializes(bloq_ex)
     except qlt_testing.BloqCheckException as bce:
         raise bce from bce
 
 
-def assert_bloq_example_typing_for_pytest(bloq_ex: BloqExample):
+def assert_bloq_example_qtyping_for_pytest(bloq_ex: BloqExample):
     try:
-        qlt_testing.assert_bloq_example_preserves_types(bloq_ex)
+        qlt_testing.assert_bloq_example_qtyping(bloq_ex)
     except qlt_testing.BloqCheckException as bce:
         if bce.check_result is qlt_testing.BloqCheckResult.NA:
             pytest.skip(bce.msg)
@@ -112,8 +112,8 @@ _TESTFUNCS = [
     ('make', assert_bloq_example_make_for_pytest),
     ('decompose', assert_bloq_example_decompose_for_pytest),
     ('counts', assert_equivalent_bloq_example_counts_for_pytest),
-    ('serialization', assert_bloq_example_serialize_for_pytest),
-    ('typing', assert_bloq_example_typing_for_pytest),
+    ('serialize', assert_bloq_example_serializes_for_pytest),
+    ('qtyping', assert_bloq_example_qtyping_for_pytest),
 ]
 
 
