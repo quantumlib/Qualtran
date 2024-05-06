993247ed040227668e6eda897c13329302f25831
Fionn Malone <fionn.malone@gmail.com>
2024-05-06 08:28:48 -0700

    Fix copyright.

--- qualtran/bloqs/chemistry/thc/select_bloq.py
+++ qualtran/bloqs/chemistry/thc/select_bloq.py
@@ -1,5 +1,4 @@
 #  Copyright 2023 Google LLC
-
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,11 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 """SELECT for the molecular tensor hypercontraction (THC) hamiltonian"""
 from functools import cached_property
 from typing import Dict, Optional, Set, Tuple, TYPE_CHECKING
 
-import cirq
 import numpy as np
 from attrs import evolve, frozen
 
