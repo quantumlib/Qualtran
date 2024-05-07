e6b44fc30b96270fc24bf9e580eed76a71577567
Matthew Harrigan <mpharrigan@google.com>
2024-05-07 10:41:28 -0700

    mark notebook test

--- qualtran/surface_code/msft_resource_estimator_interop_test.py
+++ qualtran/surface_code/msft_resource_estimator_interop_test.py
@@ -11,8 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import pytest
+
 import qualtran.testing as qlt_testing
 
 
+@pytest.mark.notebook
 def test_msft_resource_estimator_interop_notebook():
     qlt_testing.execute_notebook('msft_resource_estimator_interop')
