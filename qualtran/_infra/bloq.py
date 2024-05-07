2e94f25325b20e4edab348fdd40dfb259682bbd8
Doug Strain <dstrain@google.com>
2024-05-07 09:31:19 -0700

    Update bloq.py

--- qualtran/_infra/bloq.py
+++ qualtran/_infra/bloq.py
@@ -31,6 +31,7 @@ if TYPE_CHECKING:
         BloqBuilder,
         CompositeBloq,
         CtrlSpec,
+        GateWithRegister,
         Register,
         Signature,
         Soquet,
@@ -167,7 +168,7 @@ class Bloq(metaclass=abc.ABCMeta):
         bb, initial_soqs = BloqBuilder.from_signature(self.signature, add_registers_allowed=False)
         return bb.finalize(**bb.add_d(self, **initial_soqs))
 
-    def adjoint(self) -> 'Adjoint':
+    def adjoint(self) -> 'GateWithRegister':
         """The adjoint of this bloq.
 
         Bloq authors can override this method in certain circumstances. Otherwise, the default
