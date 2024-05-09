4fd7c7d36f0da7ef433e1689796d81668670b785
Tanuj Khattar <tanujkhattar@google.com>
2024-05-08 17:25:04 -0700

    Fix tests and mypy

--- qualtran/_infra/composite_bloq.py
+++ qualtran/_infra/composite_bloq.py
@@ -172,7 +172,7 @@ class CompositeBloq(Bloq):
         self,
         *,
         qubit_manager: Optional['cirq.QubitManager'] = None,
-        cirq_quregs: Optional[Dict[str, 'CirqQuregInT']] = None,
+        cirq_quregs: Optional[Mapping[str, 'CirqQuregInT']] = None,
     ) -> 'cirq.FrozenCircuit':
         """Convert this CompositeBloq to a `cirq.Circuit`.
 
