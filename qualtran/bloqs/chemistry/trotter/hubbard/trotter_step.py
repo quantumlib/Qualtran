0b2577cf818f65c0343078196eff2d752c400a37
Fionn Malone <fmalone@google.com>
2024-05-11 14:51:43 +0000

    Fix up notebook.

--- qualtran/bloqs/chemistry/trotter/hubbard/trotter_step.py
+++ qualtran/bloqs/chemistry/trotter/hubbard/trotter_step.py
@@ -21,13 +21,18 @@ U \approx e^{i \frac{t}{2} H_I} e^{i \frac{t}{2} H_h^p} e^{i t H_h^g}
 $$
 
 """
-from qualtran.bloqs.chemistry.trotter.hubbard.hopping import HoppingTile
-from qualtran.bloqs.chemistry.trotter.hubbard.interaction import Interaction
+from qualtran.bloqs.chemistry.trotter.hubbard.hopping import HoppingTile, HoppingTileHWP
+from qualtran.bloqs.chemistry.trotter.hubbard.interaction import Interaction, InteractionHWP
 from qualtran.bloqs.chemistry.trotter.trotterized_unitary import TrotterizedUnitary
 
 
 def build_plaq_unitary_second_order_suzuki(
-    length: int, hubb_u: float, timestep: float, hubb_t: float = 1.0, eps: float = 1e-9
+    length: int,
+    hubb_u: float,
+    timestep: float,
+    hubb_t: float = 1.0,
+    eps: float = 1e-9,
+    strip_layer: bool = False,
 ) -> TrotterizedUnitary:
     """Build second order Suzuki-Trotter unitary for the square lattice Hubbard model.
 
@@ -37,18 +42,25 @@ def build_plaq_unitary_second_order_suzuki(
         timestep: The time step for the unitary.
         hubb_t: Hubbard t. Default = 1.
         eps: The precision for single-qubit rotations.
+        strip_layer: Whether to strip one application of the interaction term
+            which is a common optimization if multiple trotter step are merged.
 
     Returns:
         unitary: The trotterized approximation to the unitary e^{-i t H}.
     """
-    # Trotter splitting parameters when H = H_I + H_h^p + H_h^g
-    indices = (0, 1, 2, 1, 0)
-    coeffs = (0.5, 0.5, 1.0, 0.5, 0.5)
     # Build the basic bloqs which make up the 2nd order PlAQ unitary.
     # The pink and gold "tiles".
     pink = HoppingTile(length=length, angle=0, eps=eps, pink=True, tau=hubb_t)
     gold = HoppingTile(length=length, angle=0, eps=eps, pink=False, tau=hubb_t)
     interaction = Interaction(length=length, angle=0, eps=eps, hubb_u=hubb_u)
+    if strip_layer:
+        # H_p H_g H_p H_I
+        indices = (1, 2, 1, 0)
+        coeffs = (0.5, 1, 0.5, 1)
+    else:
+        # Trotter splitting parameters when H = H_I + H_h^p + H_h^g
+        indices = (0, 1, 2, 1, 0)
+        coeffs = (0.5, 0.5, 1.0, 0.5, 0.5)
     unitary = TrotterizedUnitary(
         (interaction, pink, gold), indices=indices, coeffs=coeffs, timestep=timestep
     )
