570cc1d67a0c35d9a8c4bb850ce55d965e29b238
skushnir123 <samkushnir@gmail.com>
2024-05-12 13:57:08 -0400

    Update qrom_adjoint.py

--- qualtran/bloqs/data_loading/qrom_adjoint.py
+++ qualtran/bloqs/data_loading/qrom_adjoint.py
@@ -12,6 +12,7 @@ from qualtran.bloqs.data_loading import QROM
 from qualtran.bloqs.data_loading.one_hot_encoding import OneHotEncoding
 from qualtran.bloqs.data_loading.qrom import _to_tuple
 from qualtran.bloqs.multiplexers.unary_iteration_bloq import UnaryIterationGate
+from qualtran.cirq_interop.bit_tools import iter_bits
 from qualtran.resource_counting.symbolic_counting_utils import SymbolicInt, is_symbolic, log2
 
 @dataclasses.dataclass(frozen=True)
@@ -27,14 +28,27 @@ class QROMWithClassicalControls(QROM):
         bitstring = []
         x_start = 0
         for i in range(len(self.QROM_bloq.target_bitsizes)):
-            bitstring.append(self.QROM_bloq.data[x[x_start:x_start + self.QROM_bloq.target_bitsizes[i]]])
+            bitsize = self.QROM_bloq.target_bitsizes[i]
+            data = self.QROM_bloq.data[i][x[x_start:x_start + bitsize]]
+            bitstring.append(iter_bits(data, bitsize))
+        return bitstring
+
 
     def nth_operation(
         self, context: cirq.DecompositionContext, control: cirq.Qid, **kwargs
     ) -> cirq.OP_TREE:
         selection_idx = tuple(kwargs[reg.name] for reg in self.selection_registers)
         target_regs = {reg.name: kwargs[reg.name] for reg in self.target_registers}
-        yield self._load_nth_data(selection_idx, lambda q: CNOT().on(control, q), **target_regs)
+        # yield self._load_nth_data(selection_idx, lambda q: CNOT().on(control, q), **target_regs)
+        # for i, d in enumerate(self.data):
+        #     target = target_regs.get(f'target{i}_', ())
+        target = target_regs.get(f'target{0}_', ())
+        # for q, bit in zip(target, f'{int(self.data[0][selection_idx]):0{len(target)}b}'):
+        #     if int(bit):
+        #         yield gate(q)
+        N = int(log2(len(target)))
+        for i in range(len(target)):
+            x = iter_bits(selection_idx, self.selection_bitsizes[0])
 
 @attrs.frozen
 class QROMAdj():
