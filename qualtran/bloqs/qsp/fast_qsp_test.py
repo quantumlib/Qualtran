1a39a1cce8498ca4cd78c5d76deaaf9a0ffca2e1
local_linix <arren.bustamante@gmail.com>
2024-05-23 13:46:00 -0700

    Fix testing parameters.

--- qualtran/bloqs/qsp/fast_qsp_test.py
+++ qualtran/bloqs/qsp/fast_qsp_test.py
@@ -25,8 +25,7 @@ from .generalized_qsp_test import (
 )
 
 
-@pytest.mark.parametrize("degree", [4, 5])
-@pytest.mark.parametrize("precision", [1e-5, 1e-5])
+@pytest.mark.parametrize("degree, precision", [(4, 1e-5), (5, 1e-5)])
 def test_complementary_polynomial_quick(degree: int, precision: float):
     random_state = np.random.RandomState(42)
     for _ in range(2):
@@ -37,8 +36,7 @@ def test_complementary_polynomial_quick(degree: int, precision: float):
         )
 
 
-@pytest.mark.parametrize("degree", [3, 4])
-@pytest.mark.parametrize("precision", [1e-4, 1e-4])
+@pytest.mark.parametrize("degree, precision", [(3, 1e-4), (4, 1e-4)])
 def test_real_polynomial_has_real_complementary_polynomial_quick(degree: int, precision: float):
     random_state = np.random.RandomState(42)
 
@@ -53,9 +51,9 @@ def test_real_polynomial_has_real_complementary_polynomial_quick(degree: int, pr
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("degree", [5, 10, 20, 30])
-@pytest.mark.parametrize("num_tests", [20, 20, 5, 3])
-@pytest.mark.parametrize("precision", [1e-5, 1e-5, 1e-5, 1e-5])
+@pytest.mark.parametrize(
+    "degree, num_tests, precision", [(5, 20, 2e-5), (10, 20, 2e-5), (20, 5, 2e-5), (30, 3, 2e-5)]
+)
 def test_complementary_polynomial(degree: int, num_tests: int, precision: float):
     random_state = np.random.RandomState(42)
 
@@ -70,9 +68,8 @@ def test_complementary_polynomial(degree: int, num_tests: int, precision: float)
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("degree", [2, 3, 4, 5, 10])
-@pytest.mark.parametrize("num_tests", [10, 10, 10, 10, 10])
-def test_generalized_real_qsp_with_symbolic_signal_matrix(degree: int, num_tests: int):
+@pytest.mark.parametrize("degree, num_tests", [(2, 10), (3, 10), (4, 10), (5, 10), (10, 10)])
+def test_fast_real_qsp_with_symbolic_signal_matrix(degree: int, num_tests: int):
     random_state = np.random.RandomState(102)
 
     for _ in range(num_tests):
@@ -83,9 +80,10 @@ def test_generalized_real_qsp_with_symbolic_signal_matrix(degree: int, num_tests
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("degree", [2, 3, 4, 5, 10, 20])
-@pytest.mark.parametrize("num_tests", [10, 10, 10, 10, 10, 10])
-@pytest.mark.parametrize("precision", [2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2])
+@pytest.mark.parametrize(
+    "degree, num_tests, precision",
+    [(2, 10, 2e-2), (3, 10, 2e-2), (4, 10, 2e-2), (5, 10, 2e-2), (10, 10, 2e-2), (20, 10, 2e-2)],
+)
 def test_real_polynomial_has_real_complementary_polynomial(
     degree: int, num_tests: int, precision: float
 ):
