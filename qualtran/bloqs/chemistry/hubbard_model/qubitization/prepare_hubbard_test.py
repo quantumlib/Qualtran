e9cb0786c536ac626c8f60daf4a4521b94543b4b
Tanuj Khattar <tanujkhattar@google.com>
2024-05-28 19:29:17 -0400

    Octopus split and refactor

--- qualtran/bloqs/chemistry/hubbard_model/qubitization/prepare_hubbard_test.py
+++ qualtran/bloqs/chemistry/hubbard_model/qubitization/prepare_hubbard_test.py
@@ -12,36 +12,17 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import cirq
 import pytest
 
-from qualtran._infra.gate_with_registers import get_named_qubits
-from qualtran.bloqs.chemistry.hubbard_model.qubitization.hubbard_model import (
+from qualtran.bloqs.chemistry.hubbard_model.qubitization.prepare_hubbard import (
     _prep_hubb,
-    _sel_hubb,
     PrepareHubbard,
-    SelectHubbard,
 )
 from qualtran.cirq_interop.t_complexity_protocol import t_complexity
-from qualtran.testing import assert_valid_bloq_decomposition, execute_notebook
 
 
 def test_prep_hubb_auto(bloq_autotester):
     bloq_autotester(_prep_hubb)
-
-
-def test_sel_hubb_auto(bloq_autotester):
-    bloq_autotester(_sel_hubb)
-
-
-@pytest.mark.parametrize('dim', [*range(2, 10)])
-def test_select_t_complexity(dim):
-    select = SelectHubbard(x_dim=dim, y_dim=dim, control_val=1)
-    cost = t_complexity(select)
-    N = 2 * dim * dim
-    logN = 2 * (dim - 1).bit_length() + 1
-    assert cost.t == 10 * N + 14 * logN - 8
-    assert cost.rotations == 0
 
 
 @pytest.mark.parametrize('dim', [*range(3, 10)])
@@ -53,42 +34,3 @@ def test_prepare_t_complexity(dim):
     # TODO(#233): The rotation count should reduce to a constant once cost for Controlled-H
     # gates is recognized as $2$ T-gates instead of $2$ rotations.
     assert cost.rotations <= 2 * logN + 9
-
-
-def test_hubbard_model_consistent_protocols():
-    select_gate = SelectHubbard(x_dim=2, y_dim=2)
-    prepare_gate = PrepareHubbard(x_dim=2, y_dim=2, t=1, u=2)
-
-    assert_valid_bloq_decomposition(select_gate)
-    assert_valid_bloq_decomposition(prepare_gate)
-
-    # Build controlled SELECT gate
-    select_op = select_gate.on_registers(**get_named_qubits(select_gate.signature))
-    equals_tester = cirq.testing.EqualsTester()
-    equals_tester.add_equality_group(
-        select_gate.controlled(),
-        select_gate.controlled(num_controls=1),
-        select_gate.controlled(control_values=(1,)),
-        select_op.controlled_by(cirq.q("control")).gate,
-    )
-    equals_tester.add_equality_group(
-        select_gate.controlled(control_values=(0,)),
-        select_gate.controlled(num_controls=1, control_values=(0,)),
-        select_op.controlled_by(cirq.q("control"), control_values=(0,)).gate,
-    )
-
-    # Test diagrams
-    expected_symbols = ['U', 'V', 'p_x', 'p_y', 'alpha', 'q_x', 'q_y', 'beta']
-    expected_symbols += ['target'] * 8
-    expected_symbols[0] = 'SelectHubbard'
-    assert cirq.circuit_diagram_info(select_gate).wire_symbols == tuple(expected_symbols)
-
-
-@pytest.mark.notebook
-def test_hubbard_model_notebook():
-    execute_notebook('hubbard_model')
-
-
-@pytest.mark.notebook
-def test_phase_estimation_of_qubitized_hubbard_model():
-    execute_notebook('phase_estimation_of_quantum_walk')
