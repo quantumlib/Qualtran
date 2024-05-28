e5690ddcc4ab0d95983257e507ac97c6d49d48ad
Tanuj Khattar <tanujkhattar@google.com>
2024-05-28 16:25:00 -0700

    Move `test_phase_estimation_of_qubitized_hubbard_model` to `phase_estimation/` (#1004)
    Move test_phase_estimation_of_qubitized_hubbard_model

--- qualtran/bloqs/hubbard_model_test.py
+++ qualtran/bloqs/hubbard_model_test.py
@@ -82,8 +82,3 @@ def test_hubbard_model_consistent_protocols():
 @pytest.mark.notebook
 def test_hubbard_model_notebook():
     execute_notebook('hubbard_model')
-
-
-@pytest.mark.notebook
-def test_phase_estimation_of_qubitized_hubbard_model():
-    execute_notebook('phase_estimation_of_quantum_walk')
