8f2cb78576de3a032235e59523ecb254538d43c1
Matthew Harrigan <mpharrigan@google.com>
2024-07-03 17:48:02 -0700

    CY

--- qualtran/bloqs/basic_gates/y_gate_test.py
+++ qualtran/bloqs/basic_gates/y_gate_test.py
@@ -15,7 +15,17 @@ import cirq
 import numpy as np
 
 from qualtran import BloqBuilder
-from qualtran.bloqs.basic_gates import MinusState, PlusState, YGate
+from qualtran.bloqs.basic_gates import MinusState, OneEffect, OneState, PlusState, YGate
+from qualtran.bloqs.basic_gates.y_gate import _cy_gate, _y_gate, CYGate
+from qualtran.cirq_interop import cirq_gate_to_bloq
+
+
+def test_y_gate(bloq_autotester):
+    bloq_autotester(_y_gate)
+
+
+def test_cy_gate(bloq_autotester):
+    bloq_autotester(_cy_gate)
 
 
 def test_to_cirq():
@@ -38,3 +48,45 @@ def test_to_cirq():
     vec1 = cbloq.tensor_contract()
     vec2 = cirq.final_state_vector(circuit)
     np.testing.assert_allclose(vec1, vec2)
+
+
+def test_cy_vs_cirq():
+    bloq = YGate().controlled()
+    assert bloq == CYGate()
+
+    gate = cirq.Y.controlled()
+    np.testing.assert_allclose(cirq.unitary(gate), bloq.tensor_contract())
+
+
+def test_cirq_interop():
+    circuit = CYGate().as_composite_bloq().to_cirq_circuit()
+    should_be = cirq.Circuit(
+        [cirq.Moment(cirq.Y(cirq.NamedQubit('q')).controlled_by(cirq.NamedQubit('ctrl')))]
+    )
+    assert circuit == should_be
+
+    (op,) = list(should_be.all_operations())
+    assert op.gate is not None
+    assert cirq_gate_to_bloq(op.gate) == CYGate()
+
+
+def test_active_cy_is_y():
+    bb = BloqBuilder()
+    q = bb.add_register('q', 1)
+    ctrl_on = bb.add(OneState())
+    ctrl_on, q = bb.add(CYGate(), ctrl=ctrl_on, q=q)
+    bb.add(OneEffect(), q=ctrl_on)
+    cbloq = bb.finalize(q=q)
+
+    np.testing.assert_allclose(YGate().tensor_contract(), cbloq.tensor_contract())
+
+
+def test_cy_adjoint():
+    bb = BloqBuilder()
+    ctrl = bb.add_register('ctrl', 1)
+    q = bb.add_register('q', 1)
+    ctrl, q = bb.add(CYGate(), ctrl=ctrl, q=q)
+    ctrl, q = bb.add(CYGate().adjoint(), ctrl=ctrl, q=q)
+    cbloq = bb.finalize(ctrl=ctrl, q=q)
+
+    np.testing.assert_allclose(np.eye(4), cbloq.tensor_contract(), atol=1e-12)
