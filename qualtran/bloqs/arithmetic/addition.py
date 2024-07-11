9973e30d2b385cda83869ee25661055bf77a8ece
Anurudh Peduri <anurudhp97@gmail.com>
2024-07-10 23:48:21 -0700

    cleanup redundant code in `MultiControlledX`, delegate to `MultiControlledPauli`

--- qualtran/bloqs/arithmetic/addition.py
+++ qualtran/bloqs/arithmetic/addition.py
@@ -444,7 +444,7 @@ class AddK(Bloq):
             if binary_rep[i] == 1:
                 if len(self.cvs) > 0 and ctrls is not None:
                     ctrls, k_split[i] = bb.add(
-                        MultiControlX(cvs=self.cvs), ctrls=ctrls, x=k_split[i]
+                        MultiControlX(cvs=self.cvs), controls=ctrls, target=k_split[i]
                     )
                 else:
                     k_split[i] = bb.add(XGate(), q=k_split[i])
@@ -464,7 +464,7 @@ class AddK(Bloq):
             if binary_rep[i] == 1:
                 if len(self.cvs) > 0 and ctrls is not None:
                     ctrls, k_split[i] = bb.add(
-                        MultiControlX(cvs=self.cvs), ctrls=ctrls, x=k_split[i]
+                        MultiControlX(cvs=self.cvs), controls=ctrls, target=k_split[i]
                     )
                 else:
                     k_split[i] = bb.add(XGate(), q=k_split[i])
