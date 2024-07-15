eeef69864eb74b10220bf28abc9b7e3262e78530
Anurudh Peduri <anurudhp97@gmail.com>
2024-07-15 11:14:08 -0700

    add classical sim test

--- qualtran/bloqs/mcmt/ctrl_spec_activation_test.py
+++ qualtran/bloqs/mcmt/ctrl_spec_activation_test.py
@@ -11,9 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import numpy as np
 import pytest
 
-from qualtran import CtrlSpec
+from qualtran import CtrlSpec, QUInt
 from qualtran.bloqs.mcmt.ctrl_spec_activation import (
     _ctrl_on_bits,
     _ctrl_on_int,
@@ -21,6 +22,7 @@ from qualtran.bloqs.mcmt.ctrl_spec_activation import (
     _ctrl_on_nd_bits,
     CtrlSpecActivation,
 )
+from qualtran.simulation.classical_sim import get_classical_truth_table
 
 
 @pytest.mark.parametrize(
@@ -34,3 +36,18 @@ def test_examples(bloq_autotester, example):
 def test_raises_for_single_qubit_controls(ctrl_spec: CtrlSpec):
     with pytest.raises(ValueError):
         _ = CtrlSpecActivation(ctrl_spec)
+
+
+@pytest.mark.parametrize(
+    "ctrl_spec",
+    [CtrlSpec(QUInt(4), cvs=4), CtrlSpec((QUInt(4), QUInt(4)), cvs=(np.array(3), np.array(5)))],
+)
+def test_truth_table_using_classical_sim(ctrl_spec: CtrlSpec):
+    bloq = CtrlSpecActivation(ctrl_spec)
+    _, _, tt = get_classical_truth_table(bloq)
+    for in_vals, out_vals in tt:
+        # check: control values not modified
+        assert all(ctrl_in == ctrl_out for ctrl_in, ctrl_out in zip(in_vals, out_vals))
+
+        # check: target bit (last output value) matches `is_active`
+        assert out_vals[-1] == ctrl_spec.is_active(*in_vals)
