7286e32995dc7cc0f21614c843244ccf05764759
Anurudh Peduri <anurudhp97@gmail.com>
2024-07-16 14:16:35 -0700

    add csim test

--- qualtran/bloqs/arithmetic/negate_test.py
+++ qualtran/bloqs/arithmetic/negate_test.py
@@ -11,3 +11,29 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import pytest
+
+from qualtran import QInt, QUInt
+from qualtran.bloqs.arithmetic.negate import Negate
+
+
+@pytest.mark.parametrize("bitsize", [1, 2, 3, 4, 8])
+def test_negate_classical_sim(bitsize: int):
+    # TODO use QInt once classical sim is fixed.
+    #      classical sim currently only supports unsigned.
+    def _uint_to_int(val: int) -> int:
+        return QInt(bitsize).from_bits(QUInt(bitsize).to_bits(val))
+
+    dtype = QUInt(bitsize)
+
+    bloq = Negate(dtype)
+    for x_unsigned in dtype.get_classical_domain():
+        (neg_x_unsigned,) = bloq.call_classically(x=x_unsigned)
+        x = _uint_to_int(x_unsigned)
+        neg_x = _uint_to_int(neg_x_unsigned)
+        if x == -(2 ** (bitsize - 1)):
+            # twos complement negate(-2**(n - 1)) == -2**(n - 1)
+            assert neg_x == x
+        else:
+            # all other values
+            assert neg_x == -x
