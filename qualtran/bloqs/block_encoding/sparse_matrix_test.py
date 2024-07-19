5b97bf72db6993776d24503a482b421d1010e112
Charles Yuan <charlesyuan@google.com>
2024-07-19 15:09:28 -0700

    Add `ExplicitEntryOracle` to block encode a matrix

--- qualtran/bloqs/block_encoding/sparse_matrix_test.py
+++ qualtran/bloqs/block_encoding/sparse_matrix_test.py
@@ -15,10 +15,16 @@
 from typing import cast
 
 import numpy as np
+import pytest
 
 from qualtran import BloqBuilder, QAny, Register, Signature, Soquet
 from qualtran.bloqs.basic_gates import IntEffect, IntState
-from qualtran.bloqs.block_encoding.sparse_matrix import _sparse_matrix_block_encoding
+from qualtran.bloqs.block_encoding.sparse_matrix import (
+    _sparse_matrix_block_encoding,
+    ExplicitEntryOracle,
+    FullRowColumnOracle,
+    SparseMatrix,
+)
 
 
 def test_sparse_matrix(bloq_autotester):
@@ -54,3 +60,34 @@ def test_sparse_matrix_tensors():
     from_gate = np.full((4, 4), 0.3)
     from_tensors = bloq.tensor_contract() * alpha
     np.testing.assert_allclose(from_gate, from_tensors)
+
+
+rs = np.random.RandomState(1234)
+
+
+def gen_test():
+    n = rs.randint(1, 3)
+    N = 2**n
+    data = rs.rand(N, N)
+    return n, data
+
+
+@pytest.mark.parametrize(
+    "n,data", [(1, [[0.0, 0.25], [1 / 3, 0.467]])] + [gen_test() for _ in range(10)]
+)
+def test_explicit_entry_oracle(n, data):
+    row_oracle = FullRowColumnOracle(n)
+    col_oracle = FullRowColumnOracle(n)
+    entry_oracle = ExplicitEntryOracle(n, data=data, entry_bitsize=10)
+    bloq = SparseMatrix(row_oracle, col_oracle, entry_oracle, eps=0)
+
+    alpha = bloq.alpha
+    bb = BloqBuilder()
+    system = bb.add_register("system", n)
+    ancilla = cast(Soquet, bb.add(IntState(0, n + 1)))
+    system, ancilla = bb.add_t(bloq, system=system, ancilla=ancilla)
+    bb.add(IntEffect(0, n + 1), val=ancilla)
+    bloq = bb.finalize(system=system)
+
+    from_tensors = bloq.tensor_contract() * alpha
+    np.testing.assert_allclose(data, from_tensors, atol=0.003)
