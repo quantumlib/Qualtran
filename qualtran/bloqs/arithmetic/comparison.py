0b6c2ee2db33b659032ca5a3b5596e289cbfd378
Fionn Malone <fmalone@google.com>
2024-07-24 10:49:56 -0700

    Update QPIC \otimes and ^ (#1186)
    * Add otimes replacement.
    
    * Fix some latex translations for qpic.
    
    ---------
    
    Co-authored-by: Matthew Harrigan <mpharrigan@google.com>

--- qualtran/bloqs/arithmetic/comparison.py
+++ qualtran/bloqs/arithmetic/comparison.py
@@ -81,7 +81,7 @@ class LessThanConstant(GateWithRegisters, cirq.ArithmeticGate):  # type: ignore[
         if reg.name == 'x':
             return TextBox("x")
         if reg.name == 'target':
-            return TextBox("z^(x<a)")
+            return TextBox("z∧(x<a)")
         raise ValueError(f'Unknown register name {reg.name}')
 
     def registers(self) -> Sequence[Union[int, Sequence[int]]]:
@@ -468,7 +468,7 @@ class LessThanEqual(GateWithRegisters, cirq.ArithmeticGate):  # type: ignore[mis
         if reg.name == "y":
             return TextBox('y')
         if reg.name == "target":
-            return TextBox('z^(x<=y)')
+            return TextBox('z∧(x<=y)')
         raise ValueError(f'Unknown register name {reg.name}')
 
     def on_classical_vals(self, *, x: int, y: int, target: int) -> Dict[str, 'ClassicalValT']:
