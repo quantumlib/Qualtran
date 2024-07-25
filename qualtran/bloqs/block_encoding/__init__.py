9e47d4d02b60d307842d82c7dcdb9e2e3999187b
Fionn Malone <fmalone@google.com>
2024-07-25 17:08:16 +0000

    Fix import.

--- qualtran/bloqs/block_encoding/__init__.py
+++ qualtran/bloqs/block_encoding/__init__.py
@@ -14,8 +14,7 @@
 r"""High level bloqs for defining bloq encodings and operations on block encodings."""
 
 from qualtran.bloqs.block_encoding.block_encoding_base import BlockEncoding
-
-# from qualtran.bloqs.block_encoding.chebyshev_polynomial import ChebyshevPolynomial
+from qualtran.bloqs.block_encoding.chebyshev_polynomial import ChebyshevPolynomial
 from qualtran.bloqs.block_encoding.lcu_block_encoding import (
     BlackBoxPrepare,
     BlackBoxSelect,
