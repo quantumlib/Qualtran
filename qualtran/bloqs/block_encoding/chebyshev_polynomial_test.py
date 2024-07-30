38fd2d482c269755f94df9edc1cc4fc090003b4f
Charles Yuan <charlesyuan@google.com>
2024-07-30 11:18:01 -0700

    Revamp `ChebyshevPolynomial`

--- qualtran/bloqs/block_encoding/chebyshev_polynomial_test.py
+++ qualtran/bloqs/block_encoding/chebyshev_polynomial_test.py
@@ -11,22 +11,63 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from qualtran.bloqs.basic_gates import TGate
+
+import numpy as np
+
+from qualtran import BloqBuilder
+from qualtran.bloqs.basic_gates import Hadamard, Identity, IntEffect, IntState, XGate
 from qualtran.bloqs.block_encoding.chebyshev_polynomial import (
-    _black_box_chebyshev_poly,
-    _chebyshev_poly,
+    _chebyshev_poly_even,
+    _chebyshev_poly_odd,
 )
+from qualtran.testing import assert_equivalent_bloq_example_counts
 
 
-def test_chebyshev(bloq_autotester):
-    bloq_autotester(_chebyshev_poly)
+def test_chebyshev_poly_even(bloq_autotester):
+    bloq_autotester(_chebyshev_poly_even)
 
 
-def test_black_box_chebyshev(bloq_autotester):
-    bloq_autotester(_black_box_chebyshev_poly)
+def test_chebyshev_poly_even_counts():
+    assert_equivalent_bloq_example_counts(_chebyshev_poly_even)
 
 
-def test_chebyshev_t_counts():
-    counts = _chebyshev_poly().call_graph()[1]
-    counts_decomp = _chebyshev_poly().decompose_bloq().call_graph()[1]
-    assert counts[TGate()] == counts_decomp[TGate()]
+def test_chebyshev_poly_odd(bloq_autotester):
+    bloq_autotester(_chebyshev_poly_odd)
+
+
+def test_chebyshev_poly_odd_counts():
+    assert_equivalent_bloq_example_counts(_chebyshev_poly_odd)
+
+
+def test_chebyshev_poly_even_tensors():
+    def t4(x):
+        return 8 * np.linalg.matrix_power(x, 4) - 8 * np.linalg.matrix_power(x, 2) + np.eye(2)
+
+    from_gate = t4((XGate().tensor_contract() + Hadamard().tensor_contract()) / 2.0)
+
+    bloq = _chebyshev_poly_even()
+    bb = BloqBuilder()
+    system = bb.add_register("system", bloq.system_bitsize)
+    ancilla = bb.add(IntState(0, bloq.ancilla_bitsize))
+    resource = bb.add(IntState(0, bloq.resource_bitsize))
+    system, ancilla, resource = bb.add(bloq, system=system, ancilla=ancilla, resource=resource)
+    bb.add(IntEffect(0, bloq.ancilla_bitsize), val=ancilla)
+    bb.add(IntEffect(0, bloq.resource_bitsize), val=resource)
+    bloq = bb.finalize(system=system)
+    from_tensors = bloq.tensor_contract()
+    np.testing.assert_allclose(from_gate, from_tensors, atol=1e-14)
+
+
+def test_chebyshev_poly_odd_tensors():
+    def t5(x):
+        return 16 * np.linalg.matrix_power(x, 5) - 20 * np.linalg.matrix_power(x, 3) + 5 * x
+
+    from_gate = t5(Hadamard().tensor_contract())
+
+    bloq = _chebyshev_poly_odd()
+    bb = BloqBuilder()
+    system = bb.add_register("system", bloq.system_bitsize)
+    system = bb.add(bloq, system=system)
+    bloq = bb.finalize(system=system)
+    from_tensors = bloq.tensor_contract()
+    np.testing.assert_allclose(from_gate, from_tensors, atol=1e-14)
