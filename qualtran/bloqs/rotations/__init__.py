ee8af74466e7cb6ef465e2cd6884c030f7e22e48
Anurudh Peduri <anurudhp97@gmail.com>
2024-08-02 10:29:21 -0700

    Make via phase gradient a factory constructor instead of new class

--- qualtran/bloqs/rotations/__init__.py
+++ qualtran/bloqs/rotations/__init__.py
@@ -12,10 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from qualtran.bloqs.rotations.hamming_weight_phasing import (
-    HammingWeightPhasing,
-    HammingWeightPhasingViaPhaseGradient,
-)
+from qualtran.bloqs.rotations.hamming_weight_phasing import HammingWeightPhasing
 from qualtran.bloqs.rotations.phase_gradient import (
     AddIntoPhaseGrad,
     AddScaledValIntoPhaseReg,
