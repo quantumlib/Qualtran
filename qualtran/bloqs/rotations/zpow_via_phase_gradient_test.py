833520438cbf3a44cfdf734d1928a523c2495bf7
anurudhp <anurudhp97@gmail.com>
2024-08-19 14:31:17 -0700

    add unitary test

--- qualtran/bloqs/rotations/zpow_via_phase_gradient_test.py
+++ qualtran/bloqs/rotations/zpow_via_phase_gradient_test.py
@@ -12,12 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import pytest
+from attrs import frozen
 
+from qualtran import Bloq, BloqBuilder, Signature, Soquet, SoquetT
+from qualtran.bloqs.basic_gates import ZPowGate
+from qualtran.bloqs.rotations.phase_gradient import PhaseGradientState
 from qualtran.bloqs.rotations.zpow_via_phase_gradient import (
     _zpow_const_via_phase_grad,
     _zpow_const_via_phase_grad_symb_angle,
     _zpow_const_via_phase_grad_symb_prec,
+    ZPowConstViaPhaseGradient,
 )
+from qualtran.linalg.testing import assert_unitaries_equivalent_upto_global_phase
 from qualtran.resource_counting import GateCounts, get_cost_value, QECGatesCost
 
 
@@ -58,3 +64,34 @@ def test_cost_symbolic_prec():
     costs = get_cost_value(bloq, QECGatesCost())
     b_grad = bloq.phase_grad_bitsize
     assert costs == GateCounts(toffoli=b_grad - 2, clifford=2 * b_grad)
+
+
+@frozen
+class TestZPowUsingPhaseGradient(Bloq):
+    exponent: float
+    phase_grad_bitsize: int
+
+    @property
+    def signature(self) -> 'Signature':
+        return Signature.build(q=1)
+
+    def build_composite_bloq(self, bb: 'BloqBuilder', q: 'Soquet') -> dict[str, 'SoquetT']:
+        phase_grad = bb.add(PhaseGradientState(self.phase_grad_bitsize))
+        q, phase_grad = bb.add(
+            ZPowConstViaPhaseGradient(self.exponent, self.phase_grad_bitsize),
+            q=q,
+            phase_grad=phase_grad,
+        )
+        bb.add(PhaseGradientState(self.phase_grad_bitsize).adjoint(), phase_grad=phase_grad)
+        return {'q': q}
+
+
+@pytest.mark.parametrize(
+    ("exponent", "phase_grad_bitsize"),
+    [(3 / 4, 3), (3 / 4, 4), pytest.param(0.175, 7, marks=pytest.mark.slow)],
+)
+def test_unitary(exponent: float, phase_grad_bitsize: int):
+    bloq = TestZPowUsingPhaseGradient(exponent, phase_grad_bitsize)
+    actual = bloq.tensor_contract()
+    expected = ZPowGate(exponent).tensor_contract()
+    assert_unitaries_equivalent_upto_global_phase(actual, expected, atol=2**-phase_grad_bitsize)
