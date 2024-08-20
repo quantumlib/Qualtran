f3fa66eb45db34a01c9974951d04168c7da3c0b9
Nour Yosri <noureldinyosri@gmail.com>
2024-08-20 09:12:17 -0700

    fix conflicts

--- qualtran/bloqs/mod_arithmetic/__init__.py
+++ qualtran/bloqs/mod_arithmetic/__init__.py
@@ -12,6 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from ._shims import CModAdd, CModSub, ModDbl, ModInv, ModMul, ModSub
+from ._shims import CModSub, ModDbl, ModInv, ModMul, ModSub
 from .mod_addition import CModAdd, CModAddK, CtrlScaleModAdd, ModAdd, ModAddK
 from .mod_subtraction import CModNeg, ModNeg
