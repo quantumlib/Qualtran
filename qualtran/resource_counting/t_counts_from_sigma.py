5199028eb3f16bd4c765dbae471fdd1999e00a5b
Anurudh Peduri <anurudhp97@gmail.com>
2024-08-20 14:32:36 -0700

    remove `_get_all_rotation_types`

--- qualtran/resource_counting/t_counts_from_sigma.py
+++ qualtran/resource_counting/t_counts_from_sigma.py
@@ -11,9 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import inspect
-import sys
-from typing import cast, Mapping, Optional, Protocol, runtime_checkable, Tuple, Type, TYPE_CHECKING
+from typing import Mapping, TYPE_CHECKING
 
 import cirq
 
@@ -23,38 +21,14 @@ if TYPE_CHECKING:
     from qualtran import Bloq
 
 
-@runtime_checkable
-class _HasEps(Protocol):
-    """Protocol for typing `RotationBloq` base class mixin that has accuracy specified as eps."""
-
-    eps: float
-
-
-def _get_all_rotation_types() -> Tuple[Type['_HasEps'], ...]:
-    """Returns all classes defined in bloqs.basic_gates which have an attribute `eps`."""
-    from qualtran.bloqs.basic_gates import GlobalPhase
-
-    bloqs_to_exclude = [GlobalPhase]
-
-    return tuple(
-        cast(Type['_HasEps'], v)  # Can't use `issubclass` with protocols with attributes.
-        for (_, v) in inspect.getmembers(sys.modules['qualtran.bloqs.basic_gates'], inspect.isclass)
-        if isinstance(v, _HasEps) and v not in bloqs_to_exclude
-    )
-
-
-def t_counts_from_sigma(
-    sigma: Mapping['Bloq', SymbolicInt],
-    rotation_types: Optional[Tuple[Type['_HasEps'], ...]] = None,
-) -> SymbolicInt:
+def t_counts_from_sigma(sigma: Mapping['Bloq', SymbolicInt]) -> SymbolicInt:
     """Aggregates T-counts from a sigma dictionary by summing T-costs for all rotation bloqs."""
     from qualtran.bloqs.basic_gates import TGate
     from qualtran.cirq_interop.t_complexity_protocol import TComplexity
+    from qualtran.resource_counting.classify_bloqs import bloq_is_rotation
 
-    if rotation_types is None:
-        rotation_types = _get_all_rotation_types()
     ret = sigma.get(TGate(), 0) + sigma.get(TGate().adjoint(), 0)
     for bloq, counts in sigma.items():
-        if isinstance(bloq, rotation_types) and not cirq.has_stabilizer_effect(bloq):
+        if bloq_is_rotation(bloq) and not cirq.has_stabilizer_effect(bloq):
             ret += ceil(TComplexity.rotation_cost(bloq.eps)) * counts
     return ret
