dcdb65570aa9c8a480c2aef56a6c1b4b94be4adb
Tanuj Khattar <tanujkhattar@google.com>
2024-08-27 15:34:35 -0700

    Initial attempt to update ReflectionUsingPrepare with lots of failing tests

--- qualtran/bloqs/reflections/reflection_using_prepare_test.py
+++ qualtran/bloqs/reflections/reflection_using_prepare_test.py
@@ -13,17 +13,19 @@
 #  limitations under the License.
 
 import itertools
+from typing import *
 from typing import Optional
 
+import attrs
 import cirq
 import numpy as np
 import pytest
 from numpy.typing import NDArray
 
-from qualtran import Adjoint, Bloq
+from qualtran import Adjoint, Bloq, BloqBuilder, Side, Signature
 from qualtran._infra.gate_with_registers import get_named_qubits
 from qualtran.bloqs.arithmetic import LessThanConstant, LessThanEqual
-from qualtran.bloqs.basic_gates import ZPowGate
+from qualtran.bloqs.basic_gates import Hadamard, OnEach, ZeroState, ZPowGate
 from qualtran.bloqs.basic_gates.swap import CSwap
 from qualtran.bloqs.mcmt import MultiControlPauli, MultiTargetCNOT
 from qualtran.bloqs.mcmt.and_bloq import And
@@ -33,7 +35,10 @@ from qualtran.bloqs.reflections.reflection_using_prepare import (
     _refl_using_prep,
     ReflectionUsingPrepare,
 )
-from qualtran.bloqs.state_preparation import StatePreparationAliasSampling
+from qualtran.bloqs.state_preparation import (
+    PrepareUniformSuperposition,
+    StatePreparationAliasSampling,
+)
 from qualtran.cirq_interop import BloqAsCirqGate
 from qualtran.cirq_interop.testing import GateHelper
 from qualtran.resource_counting.generalizers import (
@@ -112,32 +117,116 @@ def get_3q_uniform_dirac_notation(signs, global_phase: complex = 1):
     return ret
 
 
-@pytest.mark.slow
-@pytest.mark.parametrize('num_ones', [5])
-@pytest.mark.parametrize('eps', [0.05])
-@pytest.mark.parametrize('global_phase', [+1, -1j])
-def test_reflection_using_prepare(num_ones, eps, global_phase):
-    data = [1] * num_ones
-    prepare_gate = StatePreparationAliasSampling.from_probabilities(data, precision=eps)
-
-    gate = ReflectionUsingPrepare(prepare_gate, global_phase=global_phase)
-    assert_valid_bloq_decomposition(gate)
-
-    g, qubit_order, decomposed_circuit = construct_gate_helper_and_qubit_order(gate)
-
-    initial_state_prep = cirq.Circuit(cirq.H.on_each(*g.quregs['selection']))
-    initial_state = cirq.dirac_notation(initial_state_prep.final_state_vector())
-    assert initial_state == get_3q_uniform_dirac_notation('++++++++')
-    result = cirq.Simulator(dtype=np.complex64).simulate(
-        initial_state_prep + decomposed_circuit, qubit_order=qubit_order
-    )
-    selection = g.quregs['selection']
-    prepared_state = result.final_state_vector.reshape(2 ** len(selection), -1).sum(axis=1)
-    if np.sign(global_phase) == 1:
-        signs = '-' * num_ones + '+' * (9 - num_ones)
-    elif np.sign(global_phase) == -1:
-        signs = '+' * num_ones + '-' * (9 - num_ones)
-    assert cirq.dirac_notation(prepared_state) == get_3q_uniform_dirac_notation(signs, global_phase)
+# def _add_classical_kets(bb: BloqBuilder, registers: Iterable['Register']) -> Dict[str, 'SoquetT']:
+#     """Use `bb` to add `IntState(0)` for all the `vals`."""
+#
+#     from qualtran.bloqs.basic_gates import IntState, PlusState, OnEach, ZeroState
+#
+#     soqs: Dict[str, 'SoquetT'] = {}
+#     for reg in registers:
+#         # add_state = PlusState if reg.name == 'selection' else ZeroState
+#         add_state = ZeroState
+#         if reg.shape:
+#             soq = np.empty(reg.shape, dtype=object)
+#             for idx in reg.all_idxs():
+#                 soq[idx] = bb.join(
+#                     np.array([bb.add(add_state()) for _ in range(reg.bitsize)]), dtype=reg.dtype
+#                 )
+#         else:
+#             soq = bb.join(
+#                 np.array([bb.add(add_state()) for _ in range(reg.bitsize)]), dtype=reg.dtype
+#             )
+#
+#         soqs[reg.name] = soq
+#     return soqs
+#
+#
+# def initialize_from_zero(bloq: Bloq):
+#     """Take `bloq` and compose it with initial zero states for each left register.
+#
+#     This can be contracted to a state vector for a given unitary.
+#     """
+#     bb = BloqBuilder()
+#
+#     # Add the initial 'kets' according to the provided values.
+#     in_soqs = _add_classical_kets(bb, bloq.signature.lefts())
+#
+#     in_soqs['selection'] = bb.add(PrepareUniformSuperposition(5), target=in_soqs['selection'])
+#
+#     # Add the bloq itself
+#     out_soqs = bb.add_d(bloq, **in_soqs)
+#     return bb.finalize(**out_soqs)
+#
+#
+# @pytest.mark.slow
+# @pytest.mark.parametrize('num_ones', [5])
+# @pytest.mark.parametrize('eps', [0.05])
+# @pytest.mark.parametrize('global_phase', [+1, -1j])
+# def test_reflection_using_prepare(num_ones, eps, global_phase):
+#     data = [1] * num_ones
+#     prepare_gate = StatePreparationAliasSampling.from_probabilities(data, precision=eps)
+#     print(prepare_gate.signature)
+#     prepared_state = initialize_from_zero(prepare_gate.adjoint()).tensor_contract()
+#     print(cirq.dirac_notation(prepared_state))
+#     L, logL = len(data), 3
+#     state_vector = prepared_state.reshape(2**logL, len(prepared_state) // 2**logL)
+#     prepared_state = np.linalg.norm(state_vector, axis=1)
+#     print(cirq.dirac_notation(prepared_state))
+#
+#     gate = ReflectionUsingPrepare(prepare_gate, global_phase=global_phase)
+#     sel_reg = gate.selection_registers[0]
+#     bb, soqs = BloqBuilder.from_signature(
+#         Signature([attrs.evolve(reg, side=Side.RIGHT) for reg in gate.signature])
+#     )
+#     for reg in gate.signature:
+#         soqs[reg.name] = bb.join(
+#             np.array([bb.add(ZeroState()) for _ in range(reg.bitsize)]), dtype=reg.dtype
+#         )
+#     soqs['selection'] = bb.add(
+#         OnEach(
+#             prepare_gate.selection_bitsize,
+#             Hadamard(),
+#             target_dtype=prepare_gate.selection_registers[0].dtype,
+#         ),
+#         q=soqs['selection'],
+#     )
+#     # for reg in gate.signature:
+#     #     if reg.name != 'selection':
+#     #         soqs[reg.name] = bb.allocate(dtype=reg.dtype)
+#     soqs = bb.add_d(gate, **soqs)
+#     # for reg in gate.signature:
+#     #     if reg.name != 'selection':
+#     #         bb.free(soqs.pop(reg.name))
+#     cbloq = bb.finalize(**soqs)
+#     print(cbloq.flatten_once().to_cirq_circuit())
+#     assert_valid_bloq_decomposition(gate)
+#     prepared_state = cbloq.tensor_contract()
+#     print(prepared_state.shape)
+#     print(cirq.dirac_notation(prepared_state))
+#     L, logL = len(data), 3
+#     state_vector = prepared_state.reshape(2**logL, len(prepared_state) // 2**logL)
+#     prepared_state = np.linalg.norm(state_vector, axis=1)
+#     print(cirq.dirac_notation(prepared_state))
+#     # print(gate.signature)
+#     # print(gate._multi_ctrl_z.signature)
+#     #
+#     # g, qubit_order, decomposed_circuit = construct_gate_helper_and_qubit_order(gate)
+#     # print(decomposed_circuit)
+#     #
+#     # initial_state_prep = cirq.Circuit(cirq.H.on_each(*g.quregs['selection']))
+#     # initial_state = cirq.dirac_notation(initial_state_prep.final_state_vector())
+#     # assert initial_state == get_3q_uniform_dirac_notation('++++++++')
+#     # result = cirq.Simulator(dtype=np.complex128).simulate(
+#     #     initial_state_prep + decomposed_circuit, qubit_order=qubit_order
+#     # )
+#     # selection = g.quregs['selection']
+#     # prepared_state = result.final_state_vector.reshape(2 ** len(selection), -1).sum(axis=1)
+#     if np.sign(global_phase) == 1:
+#         signs = '-' * num_ones + '+' * (9 - num_ones)
+#     elif np.sign(global_phase) == -1:
+#         signs = '+' * num_ones + '-' * (9 - num_ones)
+#     assert cirq.dirac_notation(prepared_state) == get_3q_uniform_dirac_notation(signs, global_phase)
+#
 
 
 def test_reflection_using_prepare_diagram():
@@ -151,25 +240,27 @@ def test_reflection_using_prepare_diagram():
     cirq.testing.assert_has_diagram(
         circuit,
         '''
-ancilla_0: ────X───────────────────────────────Z──────X───────────────────────────────
-                                               │
-ancilla_1: ────sigma_mu────────────────────────┼──────sigma_mu────────────────────────
-               │                               │      │
-ancilla_2: ────alt─────────────────────────────┼──────alt─────────────────────────────
-               │                               │      │
-ancilla_3: ────alt─────────────────────────────┼──────alt─────────────────────────────
-               │                               │      │
-ancilla_4: ────alt─────────────────────────────┼──────alt─────────────────────────────
-               │                               │      │
-ancilla_5: ────keep────────────────────────────┼──────keep────────────────────────────
-               │                               │      │
-ancilla_6: ────less_than_equal─────────────────┼──────less_than_equal─────────────────
-               │                               │      │
-selection0: ───StatePreparationAliasSampling───@(0)───StatePreparationAliasSampling───
-               │                               │      │
-selection1: ───selection───────────────────────@(0)───selection───────────────────────
-               │                               │      │
-selection2: ───selection^-1────────────────────@(0)───selection───────────────────────
+                    ┌──────────────────────────────┐            ┌──────────────────────────────┐
+alt0: ───────────────alt─────────────────────────────────────────alt───────────────────────────────
+                     │                                           │
+alt1: ───────────────alt─────────────────────────────────────────alt───────────────────────────────
+                     │                                           │
+alt2: ───────────────alt─────────────────────────────────────────alt───────────────────────────────
+                     │                                           │
+ancilla_0: ──────────┼────────────────────────────X────target────┼────────────────────────────X────
+                     │                                 │         │
+keep: ───────────────keep──────────────────────────────┼─────────keep──────────────────────────────
+                     │                                 │         │
+less_than_equal: ────less_than_equal───────────────────┼─────────less_than_equal───────────────────
+                     │                                 │         │
+selection0: ─────────StatePreparationAliasSampling─────ctrl0_────StatePreparationAliasSampling─────
+                     │                                 │         │
+selection1: ─────────selection─────────────────────────ctrl0_────selection─────────────────────────
+                     │                                 │         │
+selection2: ─────────selection─────────────────────────ctrl0_────selection─────────────────────────
+                     │                                           │
+sigma_mu: ───────────sigma_mu^-1─────────────────────────────────sigma_mu──────────────────────────
+                    └──────────────────────────────┘            └──────────────────────────────┘
 ''',
     )
 
@@ -179,25 +270,25 @@ selection2: ───selection^-1───────────────�
     cirq.testing.assert_has_diagram(
         circuit,
         '''
-ancilla_0: ────sigma_mu───────────────────────────────sigma_mu────────────────────────
-               │                                      │
-ancilla_1: ────alt────────────────────────────────────alt─────────────────────────────
-               │                                      │
-ancilla_2: ────alt────────────────────────────────────alt─────────────────────────────
-               │                                      │
-ancilla_3: ────alt────────────────────────────────────alt─────────────────────────────
-               │                                      │
-ancilla_4: ────keep───────────────────────────────────keep────────────────────────────
-               │                                      │
-ancilla_5: ────less_than_equal────────────────────────less_than_equal─────────────────
-               │                                      │
-control: ──────┼───────────────────────────────Z──────┼───────────────────────────────
-               │                               │      │
-selection0: ───StatePreparationAliasSampling───@(0)───StatePreparationAliasSampling───
-               │                               │      │
-selection1: ───selection───────────────────────@(0)───selection───────────────────────
-               │                               │      │
-selection2: ───selection^-1────────────────────@(0)───selection───────────────────────
+alt0: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+alt1: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+alt2: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+control: ───────────┼───────────────────────────────target───┼───────────────────────────────
+                    │                               │        │
+keep: ──────────────keep────────────────────────────┼────────keep────────────────────────────
+                    │                               │        │
+less_than_equal: ───less_than_equal─────────────────┼────────less_than_equal─────────────────
+                    │                               │        │
+selection0: ────────StatePreparationAliasSampling───ctrl0_───StatePreparationAliasSampling───
+                    │                               │        │
+selection1: ────────selection───────────────────────ctrl0_───selection───────────────────────
+                    │                               │        │
+selection2: ────────selection───────────────────────ctrl0_───selection───────────────────────
+                    │                                        │
+sigma_mu: ──────────sigma_mu^-1──────────────────────────────sigma_mu────────────────────────
 ''',
     )
 
@@ -207,27 +298,25 @@ selection2: ───selection^-1───────────────�
     cirq.testing.assert_has_diagram(
         circuit,
         '''
-               ┌──────────────────────────────┐          ┌──────────────────────────────┐
-ancilla_0: ─────sigma_mu───────────────────────────────────sigma_mu─────────────────────────
-                │                                          │
-ancilla_1: ─────alt────────────────────────────────────────alt──────────────────────────────
-                │                                          │
-ancilla_2: ─────alt────────────────────────────────────────alt──────────────────────────────
-                │                                          │
-ancilla_3: ─────alt────────────────────────────────────────alt──────────────────────────────
-                │                                          │
-ancilla_4: ─────keep───────────────────────────────────────keep─────────────────────────────
-                │                                          │
-ancilla_5: ─────less_than_equal────────────────────────────less_than_equal──────────────────
-                │                                          │
-control: ───────┼────────────────────────────X────Z───────X┼────────────────────────────────
-                │                                 │        │
-selection0: ────StatePreparationAliasSampling─────@(0)─────StatePreparationAliasSampling────
-                │                                 │        │
-selection1: ────selection─────────────────────────@(0)─────selection────────────────────────
-                │                                 │        │
-selection2: ────selection^-1──────────────────────@(0)─────selection────────────────────────
-               └──────────────────────────────┘          └──────────────────────────────┘
+alt0: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+alt1: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+alt2: ──────────────alt──────────────────────────────────────alt─────────────────────────────
+                    │                                        │
+control: ───────────┼───────────────────────────────target───┼───────────────────────────────
+                    │                               │        │
+keep: ──────────────keep────────────────────────────┼────────keep────────────────────────────
+                    │                               │        │
+less_than_equal: ───less_than_equal─────────────────┼────────less_than_equal─────────────────
+                    │                               │        │
+selection0: ────────StatePreparationAliasSampling───ctrl0_───StatePreparationAliasSampling───
+                    │                               │        │
+selection1: ────────selection───────────────────────ctrl0_───selection───────────────────────
+                    │                               │        │
+selection2: ────────selection───────────────────────ctrl0_───selection───────────────────────
+                    │                                        │
+sigma_mu: ──────────sigma_mu^-1──────────────────────────────sigma_mu────────────────────────
 ''',
     )
 
