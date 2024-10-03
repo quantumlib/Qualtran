468bcf01f2c64bc230df15028e0ee2f4bf7cd7d0
Matthew Harrigan <mpharrigan@google.com>
2024-10-03 10:31:11 -0700

    debugging ci - no parallel nb

--- dev_tools/qualtran_dev_tools/notebook_execution.py
+++ dev_tools/qualtran_dev_tools/notebook_execution.py
@@ -222,8 +222,12 @@ def execute_and_export_notebooks(
         output_html=output_html,
         only_out_of_date=only_out_of_date,
     )
-    with multiprocessing.Pool() as pool:
-        results = pool.map(func, nb_rel_paths)
+    parallel = False
+    if parallel:
+        with multiprocessing.Pool() as pool:
+            results = pool.map(func, nb_rel_paths)
+    else:
+        results = [func(nb_rel_path) for nb_rel_path in nb_rel_paths]
     bad_nbs = [nbname for nbname, err in results if err is not None]
 
     if len(bad_nbs) > 0:
