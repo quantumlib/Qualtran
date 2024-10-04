6784fba6c40c76c565a7263abe2e7014edc53ec7
Tanuj Khattar <tanujkhattar@google.com>
2024-10-04 12:45:25 -0700

    Fix formatting

--- qualtran/bloqs/gf_arithmetic/gf2_multiplication.py
+++ qualtran/bloqs/gf_arithmetic/gf2_multiplication.py
@@ -30,7 +30,7 @@ from qualtran import (
     Signature,
 )
 from qualtran.bloqs.basic_gates import CNOT, Toffoli
-from qualtran.symbolics import is_symbolic, log2, Shaped, SymbolicInt, ceil
+from qualtran.symbolics import ceil, is_symbolic, log2, Shaped, SymbolicInt
 
 if TYPE_CHECKING:
     from qualtran import BloqBuilder, Soquet
