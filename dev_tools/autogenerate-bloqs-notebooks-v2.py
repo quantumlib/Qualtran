97aae937ad9a91808dca9190a0cdef73426155de
Tanuj Khattar <tanujkhattar@google.com>
2024-10-04 14:34:33 -0700

    Add GF2Add bloq for addition over GF(2^m)

--- dev_tools/autogenerate-bloqs-notebooks-v2.py
+++ dev_tools/autogenerate-bloqs-notebooks-v2.py
@@ -105,6 +105,7 @@ import qualtran.bloqs.data_loading.qrom_base
 import qualtran.bloqs.data_loading.select_swap_qrom
 import qualtran.bloqs.factoring.ecc
 import qualtran.bloqs.factoring.mod_exp
+import qualtran.bloqs.gf_arithmetic.gf2_addition
 import qualtran.bloqs.gf_arithmetic.gf2_multiplication
 import qualtran.bloqs.hamiltonian_simulation.hamiltonian_simulation_by_gqsp
 import qualtran.bloqs.mcmt.and_bloq
@@ -560,7 +561,12 @@ GF_ARITHMETIC = [
         title='GF($2^m$) Multiplication',
         module=qualtran.bloqs.gf_arithmetic.gf2_multiplication,
         bloq_specs=[qualtran.bloqs.gf_arithmetic.gf2_multiplication._GF2_MULTIPLICATION_DOC],
-    )
+    ),
+    NotebookSpecV2(
+        title='GF($2^m$) Addition',
+        module=qualtran.bloqs.gf_arithmetic.gf2_addition,
+        bloq_specs=[qualtran.bloqs.gf_arithmetic.gf2_addition._GF2_ADDITION_DOC],
+    ),
 ]
 
 
