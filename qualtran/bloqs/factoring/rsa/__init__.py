19b8cd7dd9ee672b1324aabcd885a67af926ce4b
Frankie Papa <fpapa250@gmail.com>
2024-10-15 23:11:55 -0700

    Better documentation init

--- qualtran/bloqs/factoring/rsa/__init__.py
+++ qualtran/bloqs/factoring/rsa/__init__.py
@@ -19,15 +19,18 @@ r"""Bloqs for breaking RSA cryptography systems via integer factorization.
 RSA cryptography is a form of public key cryptography based on the difficulty of
 factoring the product of two large prime numbers.
 
-Using RSA, the cryptographic scheme chooses two large prime numbers p, q, the product
-N, λ(n) = lcm(p - 1, q - 1), an integer e such that 1 < e < λ(n),
-and finally d as d ≡ e^-1 (mod λ(n)). The public key consists of the modulus N and the
-public (or encryption) exponent e. The private key consists of the private (or decryption)
-exponent d, which must be kept secret. p, q, and λ(n) must also be kept secret because
-they can be used to calculate d.
+Using RSA, the cryptographic scheme chooses two large prime numbers p, q, their product n,
+λ(n) = lcm(p - 1, q - 1) where λ is Carmichael's totient function, an integer e such that
+1 < e < λ(n), and finally d as d ≡ e^-1 (mod λ(n)). The public key consists of the modulus n and
+the public (or encryption) exponent e. The private key consists of the private (or decryption)
+exponent d, which must be kept secret. p, q, and λ(n) must also be kept secret because they can be
+used to calculate d.
 
-Using Shor's algorithm for factoring, we can find p and q (the factors of N) in polynomial time
+Using Shor's algorithm for factoring, we can find p and q (the factors of n) in polynomial time
 with a quantum algorithm.
+
+References:
+    [https://en.wikipedia.org/wiki/RSA_(cryptosystem)](RSA (cryptosystem)).
 """
 
 from .rsa_phase_estimate import RSAPhaseEstimate
