e3aeee0d5e8f72304b2c8a1124196954e764ce0a
Frankie Papa <fpapa250@gmail.com>
2024-10-23 14:27:30 -0700

    Add ECAdd() Bloq (#1425)
    * Initial commit of ec add waiting on equals to be merged.
    
    * Working on tests for ECAdd
    
    * ECAdd implementation and tests
    
    * remove modmul typo
    
    * Fix mypy errors
    
    * Better bugfix for ModAdd
    
    * Change mod inv classical impl to use monttgomery inv
    
    * Fix pytest error
    
    * Fix some comments
    
    * ECAdd lots of testing
    
    * Add comments about bugs to be fixed
    
    * Reduce complexity by keeping intermediate values mod p
    
    * Stash qmontgomery tests
    
    * Address comments
    
    * Fix montgomery prod/inv calculations + pylint/mypy
    
    ---------
    
    Co-authored-by: Noureldin <noureldinyosri@google.com>
    Co-authored-by: Matthew Harrigan <mpharrigan@google.com>

--- qualtran/serialization/resolver_dict.py
+++ qualtran/serialization/resolver_dict.py
@@ -98,6 +98,7 @@ import qualtran.bloqs.data_loading.qroam_clean
 import qualtran.bloqs.data_loading.qrom
 import qualtran.bloqs.data_loading.select_swap_qrom
 import qualtran.bloqs.factoring._factoring_shims
+import qualtran.bloqs.factoring.ecc.ec_add
 import qualtran.bloqs.factoring.rsa
 import qualtran.bloqs.for_testing.atom
 import qualtran.bloqs.for_testing.casting
@@ -348,6 +349,13 @@ RESOLVER_DICT = {
     "qualtran.bloqs.mod_arithmetic.mod_multiplication.DirtyOutOfPlaceMontgomeryModMul": qualtran.bloqs.mod_arithmetic.mod_multiplication.DirtyOutOfPlaceMontgomeryModMul,
     "qualtran.bloqs.mod_arithmetic.mod_multiplication.SingleWindowModMul": qualtran.bloqs.mod_arithmetic.mod_multiplication.SingleWindowModMul,
     "qualtran.bloqs.factoring._factoring_shims.MeasureQFT": qualtran.bloqs.factoring._factoring_shims.MeasureQFT,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepOne": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepOne,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepTwo": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepTwo,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepThree": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepThree,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepFour": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepFour,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepFive": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepFive,
+    "qualtran.bloqs.factoring.ecc.ec_add._ECAddStepSix": qualtran.bloqs.factoring.ecc.ec_add._ECAddStepSix,
+    "qualtran.bloqs.factoring.ecc.ec_add.ECAdd": qualtran.bloqs.factoring.ecc.ec_add.ECAdd,
     "qualtran.bloqs.factoring.rsa.rsa_phase_estimate.RSAPhaseEstimate": qualtran.bloqs.factoring.rsa.rsa_phase_estimate.RSAPhaseEstimate,
     "qualtran.bloqs.factoring.rsa.rsa_mod_exp.ModExp": qualtran.bloqs.factoring.rsa.rsa_mod_exp.ModExp,
     "qualtran.bloqs.for_testing.atom.TestAtom": qualtran.bloqs.for_testing.atom.TestAtom,
