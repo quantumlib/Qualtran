1cfe07c01891d79d31e769e9712c3a6e593fad95
Fionn Malone <fmalone@google.com>
2024-12-05 23:38:36 +0000

    First quantization

--- qualtran/bloqs/chemistry/pbc/first_quantization/prepare_t_test.py
+++ qualtran/bloqs/chemistry/pbc/first_quantization/prepare_t_test.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from qualtran.bloqs.basic_gates import Toffoli
 from qualtran.bloqs.chemistry.pbc.first_quantization.prepare import (
     UniformSuperpostionIJFirstQuantization,
 )
@@ -20,6 +19,7 @@ from qualtran.bloqs.chemistry.pbc.first_quantization.prepare_t import (
     _prepare_t,
     PrepareTFirstQuantization,
 )
+from qualtran.resource_counting import get_cost_value, QECGatesCost
 
 
 def test_prepare_t(bloq_autotester):
@@ -33,15 +33,19 @@ def test_prepare_kinetic_t_counts():
     n_eta = (eta - 1).bit_length()
     expected_cost = (14 * n_eta + 8 * b_r - 36) + 2 * (2 * num_bits_p + 9)
     uni = UniformSuperpostionIJFirstQuantization(eta, num_bits_rot_aa=b_r)
-    _, counts = uni.call_graph()
-    qual_cost = counts[Toffoli()]
+    counts = get_cost_value(uni, QECGatesCost())
+    cost_dict = counts.total_t_and_ccz_count()
+    qual_cost = cost_dict['n_ccz']
     uni = UniformSuperpostionIJFirstQuantization(eta, num_bits_rot_aa=b_r).adjoint()
-    _, counts = uni.call_graph()
-    qual_cost += counts[Toffoli()]
+    counts = get_cost_value(uni, QECGatesCost())
+    cost_dict = counts.total_t_and_ccz_count()
+    qual_cost += cost_dict['n_ccz']
     prep = PrepareTFirstQuantization(num_bits_p, eta, num_bits_rot_aa=b_r)
-    _, counts = prep.call_graph()
-    qual_cost += counts[Toffoli()]
+    counts = get_cost_value(prep, QECGatesCost())
+    cost_dict = counts.total_t_and_ccz_count()
+    qual_cost += cost_dict['n_ccz']
     prep = PrepareTFirstQuantization(num_bits_p, eta, num_bits_rot_aa=b_r).adjoint()
-    _, counts = prep.call_graph()
-    qual_cost += counts[Toffoli()]
+    counts = get_cost_value(prep, QECGatesCost())
+    cost_dict = counts.total_t_and_ccz_count()
+    qual_cost += cost_dict['n_ccz']
     assert qual_cost == expected_cost
