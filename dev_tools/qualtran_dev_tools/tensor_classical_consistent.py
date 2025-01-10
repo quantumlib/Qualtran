775a70f83e3a8bdfcecba3e6ceeb7a804a6e8aff
Anurudh Peduri <anurudhp97@gmail.com>
2025-01-10 13:36:09 +0100

    cleanup

--- dev_tools/qualtran_dev_tools/tensor_classical_consistent.py
+++ dev_tools/qualtran_dev_tools/tensor_classical_consistent.py
@@ -11,8 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import Callable
-
 import numpy as np
 import pytest
 
@@ -20,27 +18,22 @@ from qualtran.simulation.tensor.tensor_from_classical import tensor_from_classic
 from qualtran.symbolics import is_symbolic
 
 from .bloq_finder import get_bloq_examples
-from .tensor_report_card import ExecuteWithTimeout
 
 
-def exec_with_timeout(fn: Callable, *, timeout: float = 10.0):
-    def _run_fn(f, cxn):
-        res, err = None, None
-        try:
-            res = f()
-        except Exception as e:  # pylint: disable=broad-exception-caught
-            err = str(e)
-        cxn.send((res, err))
-
-    runner = ExecuteWithTimeout(timeout=timeout, max_workers=1)
-    runner.submit(_run_fn, {'f': fn})
-    _, output = runner.next_result()
-    return output
+def _run_fn(fn):
+    res, err = None, None
+    try:
+        res = fn()
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        res = None
+        err = str(e)
+    return res, err
 
 
 @pytest.mark.parametrize("be", get_bloq_examples(), ids=lambda be: be.name)
+@pytest.mark.timeout(2)
 def test_classical_consistent_with_tensor(be):
-    LIM = 30
+    LIM = 15
 
     bloq = be.make()
 
@@ -50,17 +43,11 @@ def test_classical_consistent_with_tensor(be):
     if n > LIM:
         pytest.skip(f'too many qubits: {n=} > {LIM}')
 
-    result = exec_with_timeout(bloq.tensor_contract)
-    if result is None:
-        pytest.skip('timeout: tensor')
-    tensor_direct, err = result
+    tensor_direct, err = _run_fn(lambda: bloq.tensor_contract())
     if err is not None:
         pytest.skip(f'no tensor: {err}')
 
-    result = exec_with_timeout(lambda: tensor_from_classical_sim(bloq))
-    if result is None:
-        pytest.skip('timeout: tensor from classical')
-    tensor_classical, err = result
+    tensor_classical, err = _run_fn(lambda: tensor_from_classical_sim(bloq))
     if err is not None:
         pytest.skip(f'no classical action: {err}')
 
