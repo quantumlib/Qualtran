7dbe3cf19aa783b7ae6a2fe1f82ec0477f2074f9
Nour Yosri <noureldinyosri@gmail.com>
2025-02-05 11:42:45 -0800

    Add a modulus parameter to QMontgomeryUInt

--- qualtran/_infra/data_types_test.py
+++ qualtran/_infra/data_types_test.py
@@ -138,16 +138,14 @@ def test_qmontgomeryuint():
 @pytest.mark.parametrize('p', [13, 17, 29])
 @pytest.mark.parametrize('val', [1, 5, 7, 9])
 def test_qmontgomeryuint_operations(val, p):
-    qmontgomeryuint_8 = QMontgomeryUInt(8)
+    qmontgomeryuint_8 = QMontgomeryUInt(8, p)
     # Convert value to montgomery form and get the modular inverse.
-    val_m = qmontgomeryuint_8.uint_to_montgomery(val, p)
-    mod_inv = qmontgomeryuint_8.montgomery_inverse(val_m, p)
+    val_m = qmontgomeryuint_8.uint_to_montgomery(val)
+    mod_inv = qmontgomeryuint_8.montgomery_inverse(val_m)
 
     # Calculate the product in montgomery form and convert back to normal form for assertion.
     assert (
-        qmontgomeryuint_8.montgomery_to_uint(
-            qmontgomeryuint_8.montgomery_product(val_m, mod_inv, p), p
-        )
+        qmontgomeryuint_8.montgomery_to_uint(qmontgomeryuint_8.montgomery_product(val_m, mod_inv))
         == 1
     )
 
@@ -155,10 +153,8 @@ def test_qmontgomeryuint_operations(val, p):
 @pytest.mark.parametrize('p', [13, 17, 29])
 @pytest.mark.parametrize('val', [1, 5, 7, 9])
 def test_qmontgomeryuint_conversions(val, p):
-    qmontgomeryuint_8 = QMontgomeryUInt(8)
-    assert val == qmontgomeryuint_8.montgomery_to_uint(
-        qmontgomeryuint_8.uint_to_montgomery(val, p), p
-    )
+    qmontgomeryuint_8 = QMontgomeryUInt(8, p)
+    assert val == qmontgomeryuint_8.montgomery_to_uint(qmontgomeryuint_8.uint_to_montgomery(val))
 
 
 def test_qgf():
