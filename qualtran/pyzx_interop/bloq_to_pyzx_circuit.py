8c5764263d691b3d1e87018a34c44ba3dfc41a82
Anurudh Peduri <anurudhp97@gmail.com>
2025-02-18 16:59:32 +0100

    docs

--- qualtran/pyzx_interop/bloq_to_pyzx_circuit.py
+++ qualtran/pyzx_interop/bloq_to_pyzx_circuit.py
@@ -26,21 +26,52 @@ ZXQubitMap = dict[str, NDArray[np.integer]]
 
 @define
 class ZXAncillaManager:
+    """A simple ancilla qubit manager for generating pyzx Circuits.
+
+    Args:
+        n: number of existing qubits in the starting circuit.
+    """
+
     n: int
 
     def allocate(self) -> int:
+        """Allocate an uninitialized ancilla qubit.
+
+        This returns the index of a new ancilla qubit for use.
+        Note that it must be manually initialized, e.g. using the `InitAncilla` gate.
+        """
         idx = self.n
         self.n += 1
         return idx
 
+    def free(self, q: int):
+        """Free an ancilla qubit.
+
+        Discard an ancilla qubit. For now, this operation does nothing.
+        """
+        pass
+
 
 def _empty_qubit_map_from_registers(registers: Iterable[Register]) -> ZXQubitMap:
+    """For each register, creates an empty NDArray of the appropriate shape to store the zx qubits."""
     return {reg.name: np.empty(reg.shape + (reg.bitsize,), dtype=int) for reg in registers}
 
 
 def _initalize_zx_circuit_from_signature(
     signature: Signature,
 ) -> tuple[zx.Circuit, ZXQubitMap, ZXAncillaManager]:
+    """Initalize a pyzx circuit from a bloq signature.
+
+    This enumerates the qubits in the same order as the registers in the signature.
+
+    Args:
+        signature: the signature of the bloq.
+
+    Returns:
+        A tuple of the pyzx circuit, the mapping from register names to qubits, and an
+        ancilla manager for the circuit.
+    """
+
     n_qubits: int = 0
     qubit_d: ZXQubitMap = {}
 
@@ -58,6 +89,17 @@ def _initalize_zx_circuit_from_signature(
 def _add_bloq_to_pyzx_circuit(
     circ: zx.Circuit, bloq: Bloq, ancilla_manager: ZXAncillaManager, in_qubits: ZXQubitMap
 ) -> ZXQubitMap:
+    """Add a single bloq acting on the given input qubits to a pyzx circuit.
+
+    Args:
+        circ: the pyzx circuit.
+        bloq: the bloq to add.
+        ancilla_manager: the ancilla manager for `circ`.
+        in_qubits: the input qubits to the bloq.
+
+    Returns:
+        A mapping of output register names to output qubits.
+    """
     try:
         gates, out_qubits = bloq.as_zx_gates(ancilla_manager, **in_qubits)
         for gate in gates:
@@ -73,6 +115,21 @@ def _add_bloq_to_pyzx_circuit(
 def _add_cbloq_to_pyzx_circuit(
     circ: zx.Circuit, cbloq: CompositeBloq, ancilla_manager: ZXAncillaManager, in_qubits: ZXQubitMap
 ) -> ZXQubitMap:
+    """Add a composite bloq acting on the given input qubits to a pyzx circuit.
+
+    This iterates through the `cbloq` graph in topologically-sorted order, and adds each
+    bloq instance.
+
+    Args:
+        circ: the pyzx circuit.
+        cbloq: the composite bloq to add.
+        ancilla_manager: the ancilla manager for `circ`.
+        in_qubits: the input qubits to the bloq.
+
+    Returns:
+        A mapping of output register names to output qubits.
+    """
+    # initialize the soquets corresponding to the `cbloq` inputs
     soq_map: dict[Soquet, NDArray[np.integer]] = {
         Soquet(binst=LeftDangle, reg=reg, idx=idx): in_qubits[reg.name][idx]
         for reg in cbloq.signature.lefts()
@@ -110,6 +167,14 @@ def _add_cbloq_to_pyzx_circuit(
 
 
 def bloq_to_pyzx_circuit(bloq: Bloq) -> zx.Circuit:
+    """Build a pyzx circuit of a bloq.
+
+    Args:
+        bloq: the bloq to convert.
+
+    Returns:
+        A pyzx circuit corresponding to `bloq`.
+    """
     circ, in_qubits, ancilla_manager = _initalize_zx_circuit_from_signature(bloq.signature)
     _ = _add_bloq_to_pyzx_circuit(circ, bloq, ancilla_manager, in_qubits)
     return circ
