d668e62a5b6c85cdb2057ffcf5bcfdb6555097c9
Matthew Harrigan <mpharrigan@google.com>
2025-03-04 16:35:41 -0800

    init

--- qualtran/__init__.py
+++ qualtran/__init__.py
@@ -51,16 +51,21 @@ from ._infra.composite_bloq import (
 )
 
 from ._infra.data_types import (
+    QCDType,
+    CDType,
     QDType,
-    QInt,
-    QBit,
     QAny,
-    QFxp,
+    QBit,
+    CBit,
+    QInt,
     QIntOnesComp,
     QUInt,
     BQUInt,
+    QFxp,
     QMontgomeryUInt,
     QGF,
+    QDTypeCheckingSeverity,
+    check_dtypes_consistent,
 )
 
 # Internal imports: none
@@ -83,7 +88,13 @@ from ._infra.gate_with_registers import GateWithRegisters
 
 from ._infra.adjoint import Adjoint
 
-from ._infra.controlled import Controlled, CtrlSpec, AddControlledT
+from ._infra.controlled import (
+    Controlled,
+    CtrlSpec,
+    AddControlledT,
+    _ControlledBase,
+    make_ctrl_system_with_correct_metabloq,
+)
 
 from ._infra.bloq_example import BloqExample, bloq_example, BloqDocSpec
 
