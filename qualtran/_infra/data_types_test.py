30bb6c8037fd2f476c41bf98af44d09f79949167
Matthew Harrigan <mpharrigan@google.com>
2025-09-23 14:48:17 -0700

    test for backwards compatibility

--- qualtran/_infra/data_types_test.py
+++ qualtran/_infra/data_types_test.py
@@ -13,8 +13,9 @@
 #  limitations under the License.
 import math
 import random
-from typing import Any, Sequence, Union
+from typing import Any, Iterable, List, Sequence, Union
 
+import attrs
 import galois
 import numpy as np
 import pytest
@@ -593,3 +594,64 @@ def test_qgf_with_default_poly_is_compatible():
     qgf_two = QGF(2, 4, irreducible_poly=qgf_one.gf_type.irreducible_poly)
 
     assert qgf_one == qgf_two
+
+
+@attrs.frozen
+class LegacyBQUInt(QDType):
+    """For testing: this doesn't use a `BitEncoding`, so it will go via `_BitEncodingShim`"""
+
+    bitsize: int
+    iteration_length: int = attrs.field()
+
+    @iteration_length.default
+    def _default_iteration_length(self):
+        return 2**self.bitsize
+
+    def is_symbolic(self) -> bool:
+        return is_symbolic(self.bitsize, self.iteration_length)
+
+    @property
+    def num_qubits(self):
+        return self.bitsize
+
+    def get_classical_domain(self) -> Iterable[Any]:
+        if isinstance(self.iteration_length, int):
+            return range(0, self.iteration_length)
+        raise ValueError(f'Classical Domain not defined for expression: {self.iteration_length}')
+
+    def assert_valid_classical_val(self, val: int, debug_str: str = 'val'):
+        if not isinstance(val, (int, np.integer)):
+            raise ValueError(f"{debug_str} should be an integer, not {val!r}")
+        if val < 0:
+            raise ValueError(f"Negative classical value encountered in {debug_str}")
+        if val >= self.iteration_length:
+            raise ValueError(f"Too-large classical value encountered in {debug_str}")
+
+    def to_bits(self, x: int) -> List[int]:
+        """Yields individual bits corresponding to binary representation of x"""
+        self.assert_valid_classical_val(x, debug_str='val')
+        return QUInt(self.bitsize).to_bits(x)
+
+    def from_bits(self, bits: Sequence[int]) -> int:
+        """Combine individual bits to form x"""
+        return QUInt(self.bitsize).from_bits(bits)
+
+    def assert_valid_classical_val_array(
+        self, val_array: NDArray[np.integer], debug_str: str = 'val'
+    ):
+        if np.any(val_array < 0):
+            raise ValueError(f"Negative classical values encountered in {debug_str}")
+        if np.any(val_array >= self.iteration_length):
+            raise ValueError(f"Too-large classical values encountered in {debug_str}")
+
+    def __str__(self):
+        return f'BQUInt({self.bitsize}, {self.iteration_length})'
+
+
+def test_legacy_qcdtype():
+    t = LegacyBQUInt(8, 230)
+    assert str(t) == 'BQUInt(8, 230)'
+    assert t._bit_encoding.to_bits(5) == QUInt(8).to_bits(5)
+    t.assert_valid_classical_val(229)
+    with pytest.raises(ValueError):
+        t.assert_valid_classical_val(230)
