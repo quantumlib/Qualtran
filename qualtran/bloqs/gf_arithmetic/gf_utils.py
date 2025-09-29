6b8997f562816e768d8db6a0bc524049f4fd09d2
Nour Yosri <noureldinyosri@google.com>
2025-09-29 13:03:06 -0700

    lint

--- qualtran/bloqs/gf_arithmetic/gf_utils.py
+++ qualtran/bloqs/gf_arithmetic/gf_utils.py
@@ -1,8 +1,23 @@
-from typing import Union, Sequence
-from qualtran.symbolics import is_symbolic, SymbolicInt
-from qualtran import QGF
+#  Copyright 2025 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from typing import Sequence, Union
 
 from galois import Poly
+
+from qualtran import QGF
+from qualtran.symbolics import is_symbolic, SymbolicInt
+
 
 def qgf_converter(x: Union[QGF, int, Poly, SymbolicInt, Sequence[int]]) -> QGF:
     if isinstance(x, QGF):
@@ -15,4 +30,3 @@ def qgf_converter(x: Union[QGF, int, Poly, SymbolicInt, Sequence[int]]) -> QGF:
         return QGF(2, x.degree, x)
     p = Poly.Degrees(x)
     return QGF(2, p.degree, p)
-
