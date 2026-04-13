01838bc8d22116f7da9dd1fad2e3288d8dea4615
William J. Huggins <whuggins@google.com>
2026-04-13 23:36:19 +0000

    tests: run notebooks in temp dir to avoid polluting CWD

--- qualtran/surface_code/flasq/examples/notebooks_test.py
+++ qualtran/surface_code/flasq/examples/notebooks_test.py
@@ -13,23 +13,33 @@
 #  limitations under the License.
 """Tests for FLASQ example notebooks."""
 import os
+import tempfile
 import pytest
 from qualtran.testing import execute_notebook
+
+def _execute_notebook_in_temp_dir(name: str):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        old_cwd = os.getcwd()
+        os.chdir(tmpdir)
+        try:
+            execute_notebook(name)
+        finally:
+            os.chdir(old_cwd)
 
 @pytest.mark.notebook
 def test_ising_notebook():
     os.environ['FLASQ_FAST_MODE_OVERRIDE'] = 'True'
     os.environ['MPLBACKEND'] = 'Agg'
-    execute_notebook('ising_notebook')
+    _execute_notebook_in_temp_dir('ising_notebook')
 
 @pytest.mark.notebook
 def test_hwp_notebook():
     os.environ['FLASQ_FAST_MODE_OVERRIDE'] = 'True'
     os.environ['MPLBACKEND'] = 'Agg'
-    execute_notebook('hwp_notebook')
+    _execute_notebook_in_temp_dir('hwp_notebook')
 
 @pytest.mark.notebook
 def test_gf2_multiplier_notebook():
     os.environ['FLASQ_FAST_MODE_OVERRIDE'] = 'True'
     os.environ['MPLBACKEND'] = 'Agg'
-    execute_notebook('gf2_multiplier_example_notebook')
+    _execute_notebook_in_temp_dir('gf2_multiplier_example_notebook')
