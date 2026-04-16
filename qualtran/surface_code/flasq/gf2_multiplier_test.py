36d162a7a29afc4af5fb0a0ed57b5558910cf90c
William J. Huggins <whuggins@google.com>
2026-04-16 17:01:11 +0000

    docs: remove meta-comment TODO in gf2_multiplier_test.py

--- qualtran/surface_code/flasq/gf2_multiplier_test.py
+++ qualtran/surface_code/flasq/gf2_multiplier_test.py
@@ -12,8 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# TODO: Remove comments about coding process.
-# This file is newly created based on our plan.
+
 
 import cirq
 import pytest
