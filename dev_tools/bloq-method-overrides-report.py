111e3789fee9ae4c18506d5005d357f22849e616
mhucka <mhucka@google.com>
2026-06-13 03:59:33 +0000

    Run through check/format-incremental --apply

--- dev_tools/bloq-method-overrides-report.py
+++ dev_tools/bloq-method-overrides-report.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import TYPE_CHECKING, Set
+from typing import Set, TYPE_CHECKING
 
 from qualtran_dev_tools.bloq_finder import get_bloq_classes
 
