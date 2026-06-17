eac6d6a257793db5339e1f57f3dc54dde5b48c91
Matthew Harrigan <mpharrigan@google.com>
2026-06-17 14:39:45 -0700

    for_testing

--- qualtran/bloqs/for_testing/__init__.py
+++ qualtran/bloqs/for_testing/__init__.py
@@ -12,8 +12,16 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .atom import TestAtom, TestTwoBitOp
+from .atom import TestAtom, TestTwoBitOp, TestGWRAtom
 from .casting import TestCastToFrom
 from .many_registers import TestMultiRegister
 from .with_call_graph import TestBloqWithCallGraph
 from .with_decomposition import TestParallelCombo, TestSerialCombo
+from .costing import CostingBloq
+from .interior_alloc import InteriorAlloc
+from .large_bloq import LargeBloq
+from .many_registers import TestBoundedQUInt, TestMultiTypedRegister, TestQFxp
+from .matrix_gate import MatrixGate
+from .qubit_count_many_alloc import TestManyAllocAbstracted, TestManyAllocMany, TestManyAllocOnce
+from .random_select_and_prepare import TestPauliSelectOracle, TestPrepareOracle
+from .with_decomposition import TestIndependentParallelCombo
